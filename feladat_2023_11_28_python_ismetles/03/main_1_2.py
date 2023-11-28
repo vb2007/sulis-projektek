bfee857e036ecac27965dce24f30c712dcca814c
VB2007 <vagvolgyi.balazs.vb@gmail.com>
2023-11-28 11:56:34 +0100

    python feladatok (felkesz)

---
+++ feladat_2023_11_28_python_ismetles/03/main_1_2.py
@@ -0,0 +1,8 @@
+#1. & 2. Feladat
+szam_1 = int(input("Adj meg egy számot: "))
+szam_2 = int(input("Mégegyet: "))
+
+print("Összegük: ", szam_1, " + ", szam_2, " = ", szam_1 + szam_2)
+print("Különbségük: ", szam_1, " - ", szam_2,  " = ", szam_1 - szam_2)
+print("Szorzatuk: ", szam_1, " * ", szam_2, " = ", szam_1 * szam_2)
+print("Hányadosuk maradékkal: ", round(szam_1 / szam_2, 2), "Maradék: ", szam_1 % szam_2)
---
+++ feladat_2023_11_28_python_ismetles/03/main_5.py
@@ -0,0 +1,8 @@
+#5. Feladat
+hossz1 = int(input("Téglalap egyik oldalának hossza: "))
+hossz2 = int(input("Téglalap másik oldalának hossza: "))
+
+print("Kerülete: ", 2*(hossz1 + hossz2))
+print("Területe: ", hossz1 * hossz2)
+
+sugar = int(input("Add meg egy körnek a sugarát: "))
