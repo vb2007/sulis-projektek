5de339f1e0d8198efa1372318b61b99341f9eed2
VB2007 <vagvolgyi.balazs.vb@gmail.com>
2023-12-12 11:12:04 +0100

    piton
    fúj

---
+++ feladat_2023_12_12_while_for/02_while/csillag.py
@@ -0,0 +1,9 @@
+hanyszor = int(input("Adj meg egy számot: "))
+hanyszor2 = hanyszor
+
+while(hanyszor > 0):
+    print("*")
+    hanyszor -= 1
+
+print("\n")
+print("*" * hanyszor2)
---
+++ feladat_2023_12_12_while_for/02_while/sokszor.py
@@ -0,0 +1,6 @@
+szoveg = str(input("Adj meg egy szót: "))
+ismetlesSzam = int(input("Adj meg egy számot: "))
+
+while(ismetlesSzam > 0):
+    print(szoveg, end="\n")
+    ismetlesSzam -= 1
