29f3fc8d41ab7801550ba54924cb3a0b21f04385
vb2007 <vagvolgyi.balazs.vb@gmail.com>
2024-01-23 09:40:22 +0100

    commit

---
+++ feladat_2024_01_16_szovegfuggvenyek/2_ketszo.py
@@ -0,0 +1,12 @@
+szo1 = str(input("Adj meg egy szót: "))
+szo2 = str(input("Adj meg mégegyet: "))
+
+#megvizsgálja valamelyik tartralmazza-e a másikat
+if szo1 == szo2:
+    print(f"{szo1} tartalmazza {szo2}-t.")
+
+#vizsgáld meg, hogy az első szó
+#...eleje a másik szóval kezdődik-e?
+if szo1[1] == szo2[:-1]:
+    print("Az első szó eleje a másik szó első betűjével kezdődik.")
+    
