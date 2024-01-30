b3405e1fcb309b31af906d3ebd753499eb6c6020
vb2007 <vagvolgyi.balazs.vb@gmail.com>
2024-01-30 10:40:54 +0000

    mégegy fix, továbbra sem tudom értelmezni a feladatot

--- feladat_2024_01_30_fuggvenyek/3.py
+++ feladat_2024_01_30_fuggvenyek/3.py
@@ -1,11 +1,11 @@
 def pozitiv(a,b,c):
     if (a >= 0) and (b >= 0) and (c >= 0):
-        print(f"Mind a három szám ({a},{b},{c}) pozitív.")
+        return True
     else:
-        print("Nem pozitív mind a három szám.")
+        return False
 
 a = int(input("Adj meg egy számot: "))
 b = int(input("Adj meg mégegyet: "))
 c = int(input("Adj meg mégegyet: "))
 
-pozitiv(a,b,c)
+print(pozitiv(a,b,c))
