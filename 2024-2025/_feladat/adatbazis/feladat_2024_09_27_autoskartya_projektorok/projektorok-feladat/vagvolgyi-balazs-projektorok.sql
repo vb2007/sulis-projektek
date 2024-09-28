4cae4b7c30c4c11b2c9730eb5122e42bb8d57ebd
VB2007 <vagvolgyi.balazs.vb@gmail.com>
2024-09-28 18:57:55 +0200

    finished task projektorok

--- 2024-2025/_feladat/adatbazis/feladat_2024_09_27_autoskartya_projektorok/projektorok-feladat/vagvolgyi-balazs-projektorok.sql
+++ 2024-2025/_feladat/adatbazis/feladat_2024_09_27_autoskartya_projektorok/projektorok-feladat/vagvolgyi-balazs-projektorok.sql
@@ -30,19 +30,58 @@ FROM `projektorok`
 WHERE `terem` IS NULL;
 
 -- 9. feladat
+SELECT `nev`
+FROM `projektorok`
+WHERE `hangszoro` IS NULL
+	AND `nev` IS NOT NULL
+ORDER BY `nev` ASC;
 
 -- 10. feladat
+SELECT `nev`
+FROM `projektorok`
+WHERE `atalakito` IS NOT NULL
+ORDER BY `nev` ASC;
 
 -- 11. feladat
+SELECT DISTINCT `nev`
+FROM `projektorok`
+WHERE `projektor` IN (1, 3, 4)
+ORDER BY `nev` ASC;
 
 -- 12. feladat
+SELECT `nev`, COALESCE(`terem`, "ismeretlen")
+FROM `projektorok`
+WHERE `visszahozta` IS NULL
+	AND `nev` IS NOT NULL
+ORDER BY `nev` ASC;
 
 -- 13. feladat
+SELECT `terem`, `elvitte`
+FROM `projektorok`
+ORDER BY `elvitte` DESC
+LIMIT 1;
 
 -- 14. feladat
+SELECT `nev`
+FROM `projektorok`
+WHERE `terem` BETWEEN 15 AND 25;
 
 -- 15. feladat
+SELECT `nev`
+FROM `projektorok`
+WHERE `terem` = 100
+	AND `hangszoro` IS NOT NULL;
 
 -- 16. feladat
+SELECT `projektor`, `elvitte`
+FROM `projektorok`
+WHERE `nev` IS NULL
+	AND `visszahozta` IS NULL;
 
 -- 17. feladat
+SELECT `nev`, `visszahozta`
+FROM `projektorok`
+WHERE `nev` IS NOT NULL
+	AND `visszahozta` IS NOT NULL
+ORDER BY `visszahozta` ASC
+LIMIT 1;
