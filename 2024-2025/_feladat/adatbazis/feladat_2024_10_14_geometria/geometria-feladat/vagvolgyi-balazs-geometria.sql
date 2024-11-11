41aa92df1d8f93bcb1f8e862e8db1b7e481e23ac
VB2007 <vagvolgyi.balazs.vb@gmail.com>
2024-11-11 22:47:54 +0100

    completed geometry task until task 10

--- 2024-2025/_feladat/adatbazis/feladat_2024_10_14_geometria/geometria-feladat/vagvolgyi-balazs-geometria.sql
+++ 2024-2025/_feladat/adatbazis/feladat_2024_10_14_geometria/geometria-feladat/vagvolgyi-balazs-geometria.sql
@@ -1,18 +1,45 @@
 -- 3. feladat
+SELECT *
+FROM `teglalapok`;
 
 -- 4. feladat
+SELECT `szin`, `a`, `b`, (a * b) AS `terulet`
+FROM `teglalapok`;
 
 -- 5. feladat
+SELECT
+	(a * b) AS `terulet`,
+    (2 * (a + b)) AS `kerulet`
+FROM `teglalapok`
+WHERE `szin` = "sárga";
 
 -- 6. feladat
+SELECT
+	`szin`,
+	(a * b) AS `terulet`,
+    (2 * (a + b)) AS `kerulet`
+FROM `teglalapok`
+WHERE (a * b) >= 4;
 
 -- 7. feladat
+SELECT *
+FROM `haromszogek`;
 
 -- 8. feladat
+SELECT
+    `szin`,
+    (a + b + c) as `kerulet`
+FROM `haromszogek`;
 
 -- 9. feladat
+SELECT CONCAT((a + b + c), " cm") as `kerulet`
+FROM `haromszogek`
+WHERE `szin` = "sárga";
 
 -- 10. feladat
+SELECT ROUND((a + b + c), 1) as `kerulet`
+FROM `haromszogek`
+WHERE `szin` = "piros";
 
 -- 11. feladat
 
