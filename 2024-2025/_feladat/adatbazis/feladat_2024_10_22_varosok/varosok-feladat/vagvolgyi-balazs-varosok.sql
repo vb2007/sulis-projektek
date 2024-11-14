5ff0ec8e1d9c3b2a5e5c3861282b33a8386170f5
VB2007 <vagvolgyi.balazs.vb@gmail.com>
2024-11-14 23:01:37 +0100

    completed til task 11

--- 2024-2025/_feladat/adatbazis/feladat_2024_10_22_varosok/varosok-feladat/vagvolgyi-balazs-varosok.sql
+++ 2024-2025/_feladat/adatbazis/feladat_2024_10_22_varosok/varosok-feladat/vagvolgyi-balazs-varosok.sql
@@ -17,14 +17,32 @@ SELECT COUNT(DISTINCT `tipus`) AS `tipus_db`
 FROM `magyarorszag`;
 
 -- 7. feladat
+SELECT COUNT(`id`) AS `db`
+FROM `magyarorszag`
+WHERE `megye` = "Csongrád-Csanád";
 
 -- 8. feladat
+SELECT COUNT(`id`) AS `db`
+FROM `magyarorszag`
+WHERE LENGTH(`nev`) = 4;
 
 -- 9. feladat
+SELECT COUNT(`id`) AS `db`
+FROM `magyarorszag`
+WHERE `megye` = "Békés"
+    OR `megye` = "Fejér";
 
 -- 10. feladat
+SELECT `megye`, COUNT(*) AS `db`
+FROM `magyarorszag`
+GROUP BY `megye`
+ORDER BY `db` DESC;
 
 -- 11. feladat
+SELECT `varos_cim_elnyerese` AS `ev`
+FROM `magyarorszag`
+ORDER BY `ev` DESC
+LIMIT 1;
 
 -- 12. feladat
 
