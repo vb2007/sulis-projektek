3dbb8b5d026c7d76193887a34c8e156381acfaec
VB2007 <vagvolgyi.balazs.vb@gmail.com>
2024-11-14 22:15:02 +0100

    uploaded varosok task

---
+++ 2024-2025/_feladat/adatbazis/feladat_2024_10_22_varosok/varosok-feladat/megoldas-ures.sql
@@ -0,0 +1,45 @@
+-- 3. feladat
+
+-- 4. feladat
+
+-- 5. feladat
+
+-- 6. feladat
+
+-- 7. feladat
+
+-- 8. feladat
+
+-- 9. feladat
+
+-- 10. feladat
+
+-- 11. feladat
+
+-- 12. feladat
+
+-- 13. feladat
+
+-- 14. feladat
+
+-- 15. feladat
+
+-- 16. feladat
+
+-- 17. feladat
+
+-- 18. feladat
+
+-- 19. feladat
+
+-- 20. feladat
+
+-- 21. feladat
+
+-- 22. feladat
+
+-- 23. feladat
+
+-- 24. feladat
+
+-- 25. feladat
