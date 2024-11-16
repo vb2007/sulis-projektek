1b863d0a567618adf05b1e8325af198df596310c
VB2007 <vagvolgyi.balazs.vb@gmail.com>
2024-11-17 00:20:42 +0100

    finished tasks

--- 2024-2025/_feladat/adatbazis/feladat_2024_11_09_koltsegvetes_felhokarcolok/csaladi-koltsegvetes-2-feladat/vagvolgyi-balazs-csaladi-koltsegvetes-2.sql
+++ 2024-2025/_feladat/adatbazis/feladat_2024_11_09_koltsegvetes_felhokarcolok/csaladi-koltsegvetes-2-feladat/vagvolgyi-balazs-csaladi-koltsegvetes-2.sql
@@ -49,11 +49,32 @@ GROUP BY `nev`
 ORDER BY `nev` DESC;
 
 -- 11. feladat
+SELECT `nap`, `kategoria`, SUM(`mennyiseg`) AS `mennyiseg`
+FROM `kiadasok`
+GROUP BY `nap`, `kategoria`
+ORDER BY `mennyiseg` DESC;
 
 -- 12. feladat
+SELECT `nev`, `kategoria`, SUM(`egysegar`) AS `koltes`
+FROM `kiadasok`
+GROUP BY `nev`, `kategoria`;
 
 -- 13. feladat
+SELECT `kategoria`, SUM(`egysegar` * `mennyiseg`) AS `koltes`
+FROM `kiadasok`
+WHERE `nev` = "Bence"
+GROUP BY `kategoria`
+ORDER BY `koltes` ASC;
 
 -- 14. feladat
+SELECT `nev`, CONCAT(SUM(`egysegar` * `mennyiseg`), " Ft") AS `koltes`
+FROM `kiadasok`
+WHERE `kategoria` = "élelmiszer"
+GROUP BY `nev`;
 
 -- 15. feladat
+SELECT `nev`
+FROM `kiadasok`
+WHERE `kategoria` = "közlekedés"
+ORDER BY SUM(`egysegar` * `mennyiseg`) DESC
+LIMIT 1;
