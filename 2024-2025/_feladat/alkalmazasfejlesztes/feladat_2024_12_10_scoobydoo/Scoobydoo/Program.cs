a4b000bbf96e349681846f70a0da766d927ef60f
VB2007 <admin@vb2007.hu>
2024-12-10 15:24:15 +0100

    added task scoobydoo

---
+++ 2024-2025/_feladat/alkalmazasfejlesztes/feladat_2024_12_10_scoobydoo/Scoobydoo/Program.cs
@@ -0,0 +1,35 @@
+﻿//1. Feladat
+string[] lines = File.ReadAllLines("nyomozas.txt");
+int size = lines.Length;
+
+string[] nevek = new string[size];
+int[] szamok = new int[size];
+
+for (int i = 0; i < size; i++)
+{
+    string[] reszletek = lines[i].Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+    string nev = string.Join(" ", reszletek.Take(reszletek.Length - 1));
+    int szam = int.Parse(reszletek.Last());
+
+    nevek[i] = nev;
+    szamok[i] = szam;
+}
+
+//2. Feladat
+Console.WriteLine("Az eltárolt adatok kötőjellel elválasztva: ");
+
+for (int i = 0; i < size; i++)
+{
+    Console.WriteLine($"{nevek[i]} - {szamok[i]}");
+}
+
+//3. Feladat
+Console.WriteLine($"Összesen {size} db nyomozáson vettek részt.");
+
+//4. Feladat
+int snackAtlag = 0;
+for (int i = 0; i < size; i++)
+{
+
+}
