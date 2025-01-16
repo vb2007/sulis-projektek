f56c114259cf199b515648b579533f5541c25563
VB2007 <admin@vb2007.hu>
2025-01-16 21:57:17 +0100

    added task cegauto

---
+++ 2024-2025/_feladat/adatbazis/feladat_2024_11_22_cegauto_vitorlasberles/cegauto-feladat/megoldas-ures.sql
@@ -0,0 +1,26 @@
+-- 3. feladat
+
+-- 4. feladat
+
+-- 5. feladat
+
+-- 6. feladat
+
+-- 7. feladat
+
+-- 8. feladat
+
+-- 9. feladat
+
+-- 10. feladat
+
+-- 11. feladat
+
+-- 12. feladat
+
+-- 13. feladat
+
+-- 14. feladat
+
+-- 15. feladat
+
