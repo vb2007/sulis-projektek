892c23d16f3b7114de4b08c6e7511a82863313c4
Balázs Vágvölgyi <balazs.vagvolgyi@linistry.com>
2025-01-17 10:27:33 +0100

    completed cegauto til task 5

---
+++ 2024-2025/_feladat/adatbazis/feladat_2024_11_22_cegauto_vitorlasberles/cegauto-feladat/vagvolgyi-balazs-cegauto.sql
@@ -0,0 +1,44 @@
+-- 3. feladat
+SELECT COUNT(DISTINCT `tipus`) AS `toyotadb`
+FROM `auto`
+WHERE `gyarto` = "Toyota";
+
+-- 4. feladat
+SELECT `szin`
+FROM `auto`
+GROUP BY `szin`
+ORDER BY COUNT(*) DESC
+LIMIT 1;
+
+-- 5. feladat
+SELECT
+    `munkatars`.`nem`,
+    COUNT(*) AS `db`
+FROM `munkatars`
+    INNER JOIN `auto`
+        ON `munkatars`.`id` = `auto`.`munkatars_id`
+WHERE
+    `auto`.`automata` = 1
+GROUP BY
+    `munkatars`.`nem`;
+
+-- 6. feladat
+
+-- 7. feladat
+
+-- 8. feladat
+
+-- 9. feladat
+
+-- 10. feladat
+
+-- 11. feladat
+
+-- 12. feladat
+
+-- 13. feladat
+
+-- 14. feladat
+
+-- 15. feladat
+
