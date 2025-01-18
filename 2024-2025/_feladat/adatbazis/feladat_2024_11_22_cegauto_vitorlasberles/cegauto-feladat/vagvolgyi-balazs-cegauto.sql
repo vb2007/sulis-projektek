247c088b5503cfeccb895d71a75f78aa6cae2c1d
VB2007 <admin@vb2007.hu>
2025-01-18 14:51:36 +0100

    completed task cegauto

--- 2024-2025/_feladat/adatbazis/feladat_2024_11_22_cegauto_vitorlasberles/cegauto-feladat/vagvolgyi-balazs-cegauto.sql
+++ 2024-2025/_feladat/adatbazis/feladat_2024_11_22_cegauto_vitorlasberles/cegauto-feladat/vagvolgyi-balazs-cegauto.sql
@@ -64,12 +64,52 @@ WHERE `munkatars`.`varos` = "Budapest"
     AND `auto`.`gyarto` = "Toyota";
 
 -- 11. feladat
+SELECT 
+    `munkatars`.`vnev`, 
+    `munkatars`.`knev`, 
+    `auto`.`rendszam`
+FROM  `munkatars`
+    INNER JOIN `auto` 
+        ON `munkatars`.`id` = `auto`.`munkatars_id`
+WHERE `munkatars`.`varos` = "Budapest"
+    AND (`auto`.`uzemanyag` = "dízel" OR `auto`.`kivitel` = "ferdehátú")
+ORDER BY `auto`.`rendszam` DESC;
 
 -- 12. feladat
+SELECT 
+    `auto`.`rendszam`, 
+    `auto`.`gyarto`, 
+    `auto`.`tipus`, 
+    `munkatars`.`knev`
+FROM `auto`
+    INNER JOIN `munkatars` 
+        ON `auto`.`munkatars_id` = `munkatars`.`id`
+WHERE  `auto`.`kor` BETWEEN 3 AND 5
+    AND (`auto`.`gyarto` = "Opel" OR `auto`.`gyarto` = "Skoda")
+ORDER BY  `auto`.`rendszam` DESC;
 
 -- 13. feladat
+SELECT SUM(`auto`.`csomagter`) AS `osszliter`
+FROM `auto`
+    INNER JOIN `munkatars` 
+        ON `auto`.`munkatars_id` = `munkatars`.`id`
+WHERE `munkatars`.`nem` = 'férfi';
 
 -- 14. feladat
+SELECT 
+    `auto`.`gyarto`, 
+    COUNT(*) AS `db`
+FROM `auto`
+    INNER JOIN `munkatars` 
+        ON `auto`.`munkatars_id` = `munkatars`.`id`
+WHERE `munkatars`.`nem` = 'nő'
+GROUP BY `auto`.`gyarto`;
 
 -- 15. feladat
-
+SELECT 
+    `gyarto`, 
+    AVG(`csomagter`) AS `csomagter_atlag`, 
+    MAX(`csomagter`) AS `legnagyobb_csomagter`
+FROM `auto`
+GROUP BY `gyarto`
+ORDER BY `legnagyobb_csomagter` ASC; --habár nem kérték, de kell, ha azt akarjuk, hogy úgy nézzen ki, mint a mintában
