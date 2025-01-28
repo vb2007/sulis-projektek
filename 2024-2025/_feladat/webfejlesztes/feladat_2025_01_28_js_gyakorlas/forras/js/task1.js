a73d8e0e7f1440048276ace0d5dcec98c343cbf0
Balázs Vágvölgyi <balazs.vagvolgyi@linistry.com>
2025-01-28 12:02:21 +0100

    completed 3rd task

--- 2024-2025/_feladat/webfejlesztes/feladat_2025_01_28_js_gyakorlas/forras/js/task1.js
+++ 2024-2025/_feladat/webfejlesztes/feladat_2025_01_28_js_gyakorlas/forras/js/task1.js
@@ -42,3 +42,21 @@ let arAFAval = b2 + b2 * VAT;
 console.log(`A(z) ${a2} ára ÁFÁ-val: ${arAFAval} forint.`);
 
 //3. Feladat
+let a3 = [];
+
+while (true) {
+    let temp = parseFloat(prompt("Add meg az árucikk árát."));
+    if (isNaN(temp)) {
+        break;
+    }
+    else {
+        a3.push(temp);
+    }
+}
+
+let sum = a3.reduce((acc, curr) => acc + curr, 0);
+console.log(`A számok összege: ${sum}`);
+
+let avg = sum / a3.length;
+console.log(`A számok átlaga: ${avg.toFixed(2)}`);
+
