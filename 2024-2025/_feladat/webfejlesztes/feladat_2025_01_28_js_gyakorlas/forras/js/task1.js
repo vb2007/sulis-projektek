34b88fdf39b2fd893bc03aac0f844c618f79ccc3
Balázs Vágvölgyi <balazs.vagvolgyi@linistry.com>
2025-01-28 12:04:35 +0100

    moved tasks to correct places

--- 2024-2025/_feladat/webfejlesztes/feladat_2025_01_28_js_gyakorlas/forras/js/task1.js
+++ 2024-2025/_feladat/webfejlesztes/feladat_2025_01_28_js_gyakorlas/forras/js/task1.js
@@ -1,6 +1,5 @@
 "use strict"
 
-//1. Feladat
 function random(alsohatar = 1, felsohatar = 10) {
     return Math.floor(Math.random() * (felsohatar - alsohatar + 1)) + alsohatar;
 }
@@ -20,43 +19,3 @@ while (true) {
 }
 
 alert(`A random szám: ${random(a1, b1)}`);
-
-//2. Feladat
-const VAT = 0.27;
-
-let a2 = prompt("Add meg az árucikk nevét.");
-let b2;
-
-while (true) {
-    b2 = parseFloat(prompt("Add meg az árucikk árát."));
-    if (isNaN(b2)) {
-        alert("Ez nem egy érvényes szám.");
-    }
-    else {
-        break;
-    }
-}
-
-let arAFAval = b2 + b2 * VAT;
-
-console.log(`A(z) ${a2} ára ÁFÁ-val: ${arAFAval} forint.`);
-
-//3. Feladat
-let a3 = [];
-
-while (true) {
-    let temp = parseFloat(prompt("Add meg az árucikk árát."));
-    if (isNaN(temp)) {
-        break;
-    }
-    else {
-        a3.push(temp);
-    }
-}
-
-let sum = a3.reduce((acc, curr) => acc + curr, 0);
-console.log(`A számok összege: ${sum}`);
-
-let avg = sum / a3.length;
-console.log(`A számok átlaga: ${avg.toFixed(2)}`);
-
