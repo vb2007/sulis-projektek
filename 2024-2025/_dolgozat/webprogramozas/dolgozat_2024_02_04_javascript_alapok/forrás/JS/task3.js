e383942cd86d7db6dd5decfa10df997a01d7536d
Balázs Vágvölgyi <vagvolgyi.balazs.vb@gmail.com>
2025-02-04 13:11:10 +0100

    added javascript test files and description

---
+++ 2024-2025/_dolgozat/webprogramozas/dolgozat_2024_02_04_javascript_alapok/forrás/JS/task3.js
@@ -0,0 +1,45 @@
+"use strict";
+
+function sortArr(arr=[2, 5, 3, 7, 4]){
+    return arr.sort(function(a, b){return a-b});
+}
+// nézze meg mit ad vissza a meghívás: console.log(sortArr())
+//Innentől lefelé írjon
+
+console.log(sortArr());
+
+let lista = [];
+let temp;
+
+while (true) {
+    temp = prompt("Adj meg egy számot.");
+    if (isNaN(temp)) {
+        break;
+    }
+    else {
+        lista.push(parseInt(temp));
+    }
+}
+
+console.log(lista);
+
+let szorzat;
+console.log(lista.length);
+for (let i = 0; i < lista.length - 1; i++) {
+    if (i == 0) {
+        szorzat = lista[i] * lista[i + 1];
+    }
+    else {
+        szorzat = szorzat * lista[i + 1];
+    }
+}
+
+console.log(`A számok szorzata: ${szorzat}`);
+
+let szortirozott = sortArr(lista);
+let median;
+if (szortirozott % 2 != 0) {
+    median = szortirozott[szortirozott.length / 2];
+}
+
+console.log(`A számok mediánja: ${median}`);
