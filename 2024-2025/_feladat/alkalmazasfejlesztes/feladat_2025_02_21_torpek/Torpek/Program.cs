1469f6ba58783ca5a774b5fd0baf939ec25154ef
VB2007 <admin@vb2007.hu>
2025-02-21 21:26:38 +0100

    completed required tasks

--- 2024-2025/_feladat/alkalmazasfejlesztes/feladat_2025_02_21_torpek/Torpek/Program.cs
+++ 2024-2025/_feladat/alkalmazasfejlesztes/feladat_2025_02_21_torpek/Torpek/Program.cs
@@ -1,28 +1,28 @@
 ﻿//1. Feladat
-string filePath = "torpek.txt";
-List<Torpe> torpek = new List<Torpe>();
+List<Gnome> gnomes = new List<Gnome>();
 
+string filePath = "torpek.txt";
 string[] lines = File.ReadAllLines(filePath);
 for (int i = 1; i < lines.Length; i++)
 {
     string[] parts = lines[i].Split(';');
-    Torpe torpe = new Torpe(parts[0], parts[1], parts[2][0], int.Parse(parts[3]), int.Parse(parts[4]));
-    torpek.Add(torpe);
+    Gnome gnome = new Gnome(parts[0], parts[1], parts[2][0], int.Parse(parts[3]), int.Parse(parts[4]));
+    gnomes.Add(gnome);
 }
 
 //foreach (var torpe in torpek)
 //{
-//    Console.WriteLine($"{torpe.Nev}, {torpe.Klan}, {torpe.Nem}, {torpe.Weight} kg, {torpe.Magassag} cm");
+//    Console.WriteLine($"{torpe.Name}, {torpe.Clan}, {torpe.Gender}, {torpe.Weight} kg, {torpe.Height} cm");
 //}
 
 //2. Feladat
-Console.WriteLine($"2. Feladat: Az állományban található törpék száma: {torpek.Count} db");
+Console.WriteLine($"2. Feladat: Az állományban található törpék száma: {gnomes.Count} db");
 
 //3. Feladat
-Console.WriteLine($"3. Feladat: A törpék átlagos súlya: {Math.Round(torpek.Average(x => x.Weight), 1)} kg");
+Console.WriteLine($"3. Feladat: A törpék átlagos súlya: {Math.Round(gnomes.Average(x => x.Weight), 1)} kg");
 
 //4. Feladat
-Torpe legmagasabbTorpe = torpek.MaxBy(x => x.Height);
+Gnome legmagasabbTorpe = gnomes.MaxBy(x => x.Height);
 Console.WriteLine($"4. Feladat: A legmagasabb törpe adatai:");
 Console.WriteLine($"\tNeve: {legmagasabbTorpe.Name}");
 Console.WriteLine($"\tKlán: {legmagasabbTorpe.Clan}");
@@ -34,19 +34,43 @@ Console.WriteLine($"\tMagasság: {legmagasabbTorpe.Height} cm");
 Console.Write("5. feladat: A klán neve: ");
 string clanInput = Console.ReadLine()!;
 string loweredClanInput = clanInput.ToLower();
-bool isClanFound = torpek.Exists(x => x.Clan.ToLower() == loweredClanInput);
+bool isClanFound = gnomes.Exists(x => x.Clan.ToLower() == loweredClanInput);
 Console.WriteLine($"\t{(isClanFound ? "Van" : "Nincs")} {clanInput} nevű klánba tartozó törpe.");
 
 //6. Feladat
-Console.WriteLine("6. Feladat: A törpék testtömeg indexe (BMI):");
-foreach (var torpe in torpek)
+double smallestBMIValue = Math.Round(gnomes.Min(torpe =>
 {
     double heightInMeters = torpe.Height / 100.0;
-    double bmi = torpe.Weight / (heightInMeters * heightInMeters);
-    Console.WriteLine($"\t{torpe.Name} BMI-je: {Math.Round(bmi, 2)}");
-}
+    return torpe.Weight / (heightInMeters * heightInMeters);
+}), 1);
+
+Console.WriteLine($"6. feladat: A legkisebb TTI érték: {smallestBMIValue}");
 
 //7. Feladat
-Console.WriteLine($"Nőnemű törpék: {torpek.FindAll(x => x.Gender == 'N')}");
+List<Gnome> femaleGnomes = gnomes.FindAll(x => x.Gender == 'N');
+Console.WriteLine($"7. feladat: Nőnemű törpék: {string.Join(", ", femaleGnomes.Select(t => $"{t.Name} ({t.Clan})"))}");
 
-public record Torpe(string Name, string Clan, char Gender, int Weight, int Height);
+//8. Feladat
+Console.WriteLine($"8. feladat: Résztvevők: ");
+foreach (Gnome gnome in gnomes.OrderBy(g => g.Clan).ThenBy(g => g.Name))
+{
+    Console.WriteLine($"{gnome.Name} ({gnome.Clan})");
+}
+
+//9. Feladat
+IEnumerable<string> uniqueClans = gnomes.Select(g => g.Clan).Distinct().OrderBy(clan => clan);
+Console.WriteLine($"9. feladat: Résztvevő klánok: {string.Join(", ", uniqueClans)}");
+
+//10. Feladat
+var top3GnomesByBMI = gnomes
+    .Select(g => new { Gnome = g, BMI = g.Weight / Math.Pow(g.Height / 100.0, 2) })
+    .OrderByDescending(g => g.BMI)
+    .Take(3);
+
+Console.WriteLine("10. feladat: A 3 legnagyobb TTI-vel rendelkező törpe:");
+foreach (var g in top3GnomesByBMI)
+{
+    Console.WriteLine($"{g.Gnome.Name} ({g.Gnome.Clan})");
+}
+
+public record Gnome(string Name, string Clan, char Gender, int Weight, int Height);
