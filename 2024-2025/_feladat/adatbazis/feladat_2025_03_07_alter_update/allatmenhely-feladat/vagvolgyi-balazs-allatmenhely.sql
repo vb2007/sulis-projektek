48ce8e8589e8d0eca9989f63ce8358b553894d00
VB2007 <admin@vb2007.hu>
2025-03-08 15:49:52 +0100

    completed task 8 - 14

--- 2024-2025/_feladat/adatbazis/feladat_2025_03_07_alter_update/allatmenhely-feladat/vagvolgyi-balazs-allatmenhely.sql
+++ 2024-2025/_feladat/adatbazis/feladat_2025_03_07_alter_update/allatmenhely-feladat/vagvolgyi-balazs-allatmenhely.sql
@@ -43,22 +43,45 @@ SELECT `id` FROM `menhelyek` WHERE nev = "Rex Kutyaotthon";
 -- 7. feladat
 INSERT INTO `allatok` (`nev`, `kor`, `tipus`, `fajta`, `ivartalanitott`, `nem`, `behozva`, `menhely_id`)
 VALUES
-("Csoki", 4, "kutya", "keverék", TRUE, "kan", "2016-04-01", (SELECT id FROM menhelyek WHERE nev = "Rex Kutyaotthon")),
-("Picasso", 7, "kutya", "keverék", FALSE, "kan", "2018-01-12", (SELECT id FROM menhelyek WHERE nev = "Rex Kutyaotthon"));
+("Csoki", 4, "kutya", "keverék", TRUE, "kan", "2016-04-01", (SELECT `id` FROM `menhelyek` WHERE `nev` = "Rex Kutyaotthon")),
+("Picasso", 7, "kutya", "keverék", FALSE, "kan", "2018-01-12", (SELECT `id` FROM `menhelyek` WHERE `nev` = "Rex Kutyaotthon"));
 
 -- 8. feladat
+INSERT INTO `menhelyek` (`nev`, `cim`, `web`, `adoszam`, `bankszamlaszam`)
+VALUES
+("Noé állatotthon", "Budapest XVII.ker. Csordakút utca", "http://www.noeallatotthon.hu/", "18169696-1-42", "11710002-20083777");
 
 -- 9. feladat
+INSERT INTO `allatok` (`nev`, `kor`, `tipus`, `fajta`, `ivartalanitott`, `nem`, `behozva`, `menhely_id`)
+VALUES
+("Bíbor", 2, "cica", "perzsai", TRUE, "nőstény", "2017-05-17", (SELECT `id` FROM `menhelyek` WHERE `nev` = "Noé állatotthon")),
+("Bubbly", 0.5, "cica", NULL, FALSE, "nőstény", "2018-01-02", (SELECT `id` FROM `menhelyek` WHERE `nev` = "Noé állatotthon")),
+("Csoki", 2, "kutya", "keverék", FALSE, "nőstény", "2020-01-12", (SELECT `id` FROM `menhelyek` WHERE `nev` = "Noé állatotthon")),
+("Nixxxon", 2.5, "kutya", "golden retviver", TRUE, "kan", NOW(), (SELECT `id` FROM `menhelyek` WHERE `nev` = "Noé állatotthon"));
 
 -- 10. feladat
+DELETE FROM `allatok`
+WHERE `nev` = "Bubbly";
 
 -- 11. feladat
+UPDATE `allatok`
+SET `nev` = "Nixon"
+WHERE `nev` = "Nixxxon";
 
 -- 12. feladat
+UPDATE `allatok`
+SET `nem` = "nőstény"
+WHERE `nev` = "Picasso";
 
 -- 13. feladat
+UPDATE `allatok`
+SET `kor` = 3
+WHERE `nev` = "Csoki";
 
 -- 14. feladat
+SELECT AVG(`kor`) AS `atlag_eletkor`
+FROM `allatok`
+WHERE `tipus` = 'kutya';
 
 -- 15. feladat
 
