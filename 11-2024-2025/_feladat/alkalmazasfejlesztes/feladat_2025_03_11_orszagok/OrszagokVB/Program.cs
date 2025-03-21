63d41c9bb32bd6d0fd3830b185016077e6aab258
VB2007 <admin@vb2007.hu>
2025-03-21 18:37:35 +0100

    fixed cw texts and overflow error

--- 11-2024-2025/_feladat/alkalmazasfejlesztes/feladat_2025_03_11_orszagok/OrszagokVB/Program.cs
+++ 11-2024-2025/_feladat/alkalmazasfejlesztes/feladat_2025_03_11_orszagok/OrszagokVB/Program.cs
@@ -4,6 +4,7 @@
     {
         static void Main(string[] args)
         {
+            //6. Feladat
             List<Orszagok> orszagok = new();
 
             foreach (var item in File.ReadAllLines("orszagok.csv").Skip(1))
@@ -17,23 +18,22 @@
                     int.Parse(darabolt[4])));
             }
 
-            Console.WriteLine("7. Feladat");
             foreach (var item in orszagok)
             {
                 Console.WriteLine(item.Info() + "\n");
             }
 
-            Console.WriteLine("8. Feladat");
+            Console.WriteLine("7. Feladat:");
             Console.WriteLine($"{orszagok.Count} ország adatait tároltuk");
 
-            Console.WriteLine("9. Feladat");
-            Console.WriteLine($"Összterület:  {orszagok.Sum(x => x.Terulet)}\nÖsszlakosság: {orszagok.Sum(x => x.Lakossag)}");
+            Console.WriteLine("8. Feladat:");
+            Console.WriteLine($"Országok összterület:  {orszagok.Sum(x => (long)x.Terulet)}\nOrszágok összlakossága: {orszagok.Sum(x => (long)x.Lakossag)}");
 
-            Console.WriteLine("10. Feladat");
-            Console.WriteLine($"Legnagyobb {orszagok.MaxBy(x => x.Terulet).Orszag}");
+            Console.WriteLine("10. Feladat:");
+            Console.WriteLine($"Legnagyobb területtel rendelkező ország: {orszagok.MaxBy(x => x.Terulet).Orszag}");
 
-            Console.WriteLine("13. Feladat");
-            Console.WriteLine($"\"A\" betűvel kezdődik: {orszagok.Count(x => x.Orszag.StartsWith('A'))}");
+            Console.WriteLine("13. Feladat:");
+            Console.WriteLine($"\"A\" betűvel kezdődő országok száma: {orszagok.Count(x => x.Orszag.StartsWith('A'))}");
 
             Console.WriteLine("");
             Console.WriteLine("Adja meg egy főváros nevét: ");
