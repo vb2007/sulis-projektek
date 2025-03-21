902c0bd7d10b5f19fb52a7c6eede220a6ee17eea
VB2007 <admin@vb2007.hu>
2025-03-21 18:29:27 +0100

    added task description and solution orszagok

---
+++ 11-2024-2025/_feladat/alkalmazasfejlesztes/feladat_2025_03_11_orszagok/OrszagokVB/Orszagok.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrszagokVB
+{
+    internal class Orszagok
+    {
+        public string Orszag { get; set; }
+        private int terulet;
+        public int Terulet
+        {
+            get { return terulet; }
+            set
+            {
+                if (value > 0)
+                {
+                    terulet = value;
+                }
+            }
+        }
+
+        private int lakossag;
+        public int Lakossag
+        {
+            get { return lakossag; }
+            set
+            {
+                if (value > 20000)
+                {
+                    lakossag = value;
+                }
+            }
+        }
+
+        public string Fovaros { get; set; }
+
+        private int tavolsag;
+        public int Tavolsag
+        {
+            get { return tavolsag; }
+            init
+            {
+                if (value >= 200 && value <= 25000)
+                {
+                    tavolsag = value;
+                }
+            }
+        }
+
+        public Orszagok()
+        {
+            Orszag = "Ismertetlen";
+            Fovaros = "Ismertetlen";
+            terulet = 1;
+            lakossag = 20001;
+            tavolsag = 200;
+        }
+
+        //Ország;Terület;Lakosság;Főváros;Távolság
+        public Orszagok(string orszag, int terulet, int lakossag, string fovaros, int tavolsag)
+        {
+            Orszag = orszag;
+            Terulet = terulet;
+            Lakossag = lakossag;
+            Fovaros = fovaros;
+            Tavolsag = tavolsag;
+        }
+
+        public string Info()
+        {
+            return $"{Orszag} Területe: {Terulet} km²\n Lakossága: {Lakossag} fő\n Dővárosa: {Fovaros}\n Távolság: {Tavolsag} km";
+        }
+    }
+}
---
+++ 11-2024-2025/_feladat/alkalmazasfejlesztes/feladat_2025_03_11_orszagok/OrszagokVB/Program.cs
@@ -0,0 +1,70 @@
+﻿namespace OrszagokVB
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Orszagok> orszagok = new();
+
+            foreach (var item in File.ReadAllLines("orszagok.csv").Skip(1))
+            {
+                string[] darabolt = item.Split(';');
+                orszagok.Add(new Orszagok(
+                    darabolt[0],
+                    int.Parse(darabolt[1]),
+                    int.Parse(darabolt[2]),
+                    darabolt[3],
+                    int.Parse(darabolt[4])));
+            }
+
+            Console.WriteLine("7. Feladat");
+            foreach (var item in orszagok)
+            {
+                Console.WriteLine(item.Info() + "\n");
+            }
+
+            Console.WriteLine("8. Feladat");
+            Console.WriteLine($"{orszagok.Count} ország adatait tároltuk");
+
+            Console.WriteLine("9. Feladat");
+            Console.WriteLine($"Összterület:  {orszagok.Sum(x => x.Terulet)}\nÖsszlakosság: {orszagok.Sum(x => x.Lakossag)}");
+
+            Console.WriteLine("10. Feladat");
+            Console.WriteLine($"Legnagyobb {orszagok.MaxBy(x => x.Terulet).Orszag}");
+
+            Console.WriteLine("13. Feladat");
+            Console.WriteLine($"\"A\" betűvel kezdődik: {orszagok.Count(x => x.Orszag.StartsWith('A'))}");
+
+            Console.WriteLine("");
+            Console.WriteLine("Adja meg egy főváros nevét: ");
+            string beolvasottFovaros = Console.ReadLine()!;
+
+            if (orszagok.Exists(x => x.Fovaros == beolvasottFovaros))
+            {
+                Console.WriteLine(orszagok.Find(x => x.Fovaros == beolvasottFovaros).Info());
+            }
+            else
+            {
+                Console.WriteLine("Nincs ilyen főváros");
+            }
+
+            Console.WriteLine("Mekkora területnél kisebbek: ");
+            int beolvasottTerulet = int.Parse(Console.ReadLine()!);
+
+            foreach (var item in orszagok.Where(x => x.Terulet < beolvasottTerulet))
+            {
+                Console.WriteLine(item.Orszag);
+            }
+
+            int magyarorszagTerulete = 93000;
+            List<Orszagok> kissebbMONal = orszagok.Where(x => x.Terulet < magyarorszagTerulete).ToList();
+
+            StreamWriter sw = new StreamWriter("kisebbMO.txt");
+            foreach (var item in kissebbMONal)
+            {
+                sw.WriteLine(item.Info() + "\n");
+            }
+            sw.Close();
+        }
+    }
+}
