22c4da698ccab645f4df514b07ce7a1dfbbc6dba
VB2007 <admin@vb2007.hu>
2025-04-03 23:29:44 +0200

    added missing task hoember

---
+++ 11-2024-2025/_feladat/alkalmazasfejlesztes/feladat_2025_03_14_hoember/Hoember/HoEmber.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hoember
+{
+    class HoEmber
+    {
+        private const int LOPAS = 66;
+        private int gombocokSzama;
+        private int meret;
+        private int fagyott;
+
+        public bool VanSepru { get; set; }
+        public int GombocokSzama
+        {
+            get { return gombocokSzama; }
+            private set
+            {
+                if (value == 2 || value == 3)
+                {
+                    gombocokSzama = value;
+                }
+            }
+        }
+        public int Meret
+        {
+            get { return meret; }
+            set
+            {
+                if (value < 0)
+                {
+                    meret = 0;
+                }
+                else if (value > 100)
+                {
+                    meret = 100;
+                }
+                else
+                {
+                    meret = value;
+                }
+            }
+        }
+        public int Fagyott
+        {
+            get { return fagyott; }
+            set
+            {
+                if (value < 0)
+                {
+                    fagyott = 0;
+                }
+                else if (value > 100)
+                {
+                    fagyott = 100;
+                }
+                else
+                {
+                    fagyott = value;
+                }
+            }
+        }
+        public HoEmber()
+        {
+            GombocokSzama = Random.Shared.Next(2, 4);
+            meret = 100;
+            fagyott = 100;
+            VanSepru = true;
+        }
+        public HoEmber(int gomboc, bool sepru)
+        {
+            GombocokSzama = gomboc;
+            meret = 100;
+            fagyott = 100;
+            VanSepru = sepru;
+        }
+        public HoEmber(string sor)
+        {
+            string[] darabolt = sor.Split(';');
+            GombocokSzama = int.Parse(darabolt[0]);
+            meret = int.Parse(darabolt[1]);
+            fagyott = int.Parse(darabolt[1]);
+            VanSepru = darabolt[3] != "0";
+        }
+        public string Info()
+        {
+            return $"Gombóc: {gombocokSzama}, méret: {meret}, fagyott: {fagyott}, seprű: {VanSepru}";
+        }
+        public void Olvad(int ertek)
+        {
+            Fagyott -= ertek;
+            Meret -= ertek;
+        }
+        public void Visszafagy(int ertek)
+        {
+            Fagyott += ertek;
+        }
+        public bool SepruLopas(int ertek)
+        {
+            int esely = ertek + (100 - Fagyott);
+            if (esely > LOPAS)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ 11-2024-2025/_feladat/alkalmazasfejlesztes/feladat_2025_03_14_hoember/Hoember/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Hoember
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HoEmber ho1 = new HoEmber();
+            Console.WriteLine(ho1.Info());
+
+            HoEmber ho2 = new HoEmber();
+            ho2.Olvad(30);
+            Console.WriteLine(ho2.Info());
+            ho2.Visszafagy(10);
+            Console.WriteLine(ho2.Info());
+
+
+            List<HoEmber> list = new List<HoEmber>();
+
+            foreach (var item in File.ReadLines("ho.csv").Skip(1))
+            {
+                list.Add(new HoEmber(item));
+            }
+
+            foreach (var item in list)
+            {
+                Console.WriteLine();
+                Console.WriteLine(item.Info());
+            }
+        }
+    }
+}
