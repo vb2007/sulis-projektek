0d8d23471f8c1801231dfaa3fc2fbd5cd0db0893
VB2007 <admin@vb2007.hu>
2025-04-03 23:22:51 +0200

    added groupby and groupby lib projects

---
+++ 11-2024-2025/_feladat/alkalmazasfejlesztes/feladat_2025_04_01_groupby/GroupByLib/Diak.cs
@@ -0,0 +1,18 @@
+﻿namespace GroupByLib
+{
+    public class Diak
+    {
+        public string Osztaly { get; init; }
+        public string Nev { get; init; }
+        public string Szint { get; init; }
+        public int Pont { get; init; }
+
+        public Diak(string osztaly, string nev, string szint, int pont)
+        {
+            Osztaly = osztaly;
+            Nev = nev;
+            Szint = szint;
+            Pont = pont;
+        }
+    }
+}
---
+++ 11-2024-2025/_feladat/alkalmazasfejlesztes/feladat_2025_04_01_groupby/GroupByLib/Diakok.cs
@@ -0,0 +1,21 @@
+﻿namespace GroupByLib
+{
+    public class Diakok
+    {
+        readonly List<Diak> diakok = new();
+        public Diakok(IEnumerable<string> adatSorok)
+        {
+            foreach (var item in adatSorok)
+            {
+                string[] darabolt = item.Split(';');
+                diakok.Add(new Diak(darabolt[0], darabolt[1], darabolt[2], int.Parse(darabolt[3])));
+            }
+        }
+
+        public IEnumerable<Diak> Lista => diakok;
+        public Dictionary<string, int> OsztalyLetszam =>
+            diakok.GroupBy(x => x.Osztaly).ToDictionary(x => x.Key, x => x.Count());
+        //public Dictionary<string, int> Osztalyszint =>
+            //diakok.GroupBy(x => new()).ToDictionary(x => x.Key, x => x.Atlag());
+    }
+}
