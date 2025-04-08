fc496b2512361762de6f075333834cca9f6b1ab1
VB2007 <admin@vb2007.hu>
2025-04-08 21:20:09 +0200

    added half complete task hegyek

---
+++ 11-2024-2025/_feladat/alkalmazasfejlesztes/feladat_2025_04_08_magyarorszagi_varosok/Hegyek_Lib/Hegyek.cs
@@ -0,0 +1,23 @@
+﻿namespace Hegyek_Lib
+{
+    public class Hegyek
+    {
+        private readonly List<Hegy> hegyek = new List<Hegy>();
+
+        public Hegyek(IEnumerable<string> fajlSorok)
+        {
+            foreach (var item in fajlSorok)
+            {
+                hegyek.Add(new Hegy(item));
+            }
+        }
+
+        public int HegyekSzama => hegyek.Count;
+        public Hegy Legalacsonyabb => hegyek.MinBy(h => h.Magassag);
+        public double AtlagMagassag => Math.Round(hegyek.Average(h => h.Magassag), 1);
+        public int HegysegbenHegycsucs(string hegysegNeve) => hegyek.Count(x => x.Hegyseg == hegysegNeve);
+
+        public int ErteknelMagasabbakSzama(double magassag) => hegyek.Count(x => x.LabraValtas > magassag);
+        public IEnumerable<Hegy> SorbaRendezett => hegyek.OrderByDescending(x => x.Magassag);
+    }
+}
---
+++ 11-2024-2025/_feladat/alkalmazasfejlesztes/feladat_2025_04_08_magyarorszagi_varosok/Hegyek_VB/Program.cs
@@ -0,0 +1,37 @@
+﻿using Hegyek_Lib;
+
+namespace Hegyek_VB
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Hegyek hegyek = new(File.ReadLines("hegyekMo.txt").Skip(1));
+
+            Console.WriteLine($"2. feladat: {hegyek} db hegy adatai találhatók a fájlban.");
+
+            Hegy legalacsonyabb = hegyek.Legalacsonyabb;
+            Console.WriteLine($"3. feladat: A legalacsonyabb hegy: {legalacsonyabb.Hegycsucs} ({legalacsonyabb.Hegyseg}), {legalacsonyabb.Magassag} m");
+
+            Console.WriteLine($"4. feladat: A hegycsúcsok átlagos magassága: {hegyek.AtlagMagassag} m");
+
+            Console.WriteLine($"5. feladat: A Mátrában {hegyek.HegysegbenHegycsucs("Mátra")} db hegycsúcs található.");
+
+            Console.WriteLine($"8. feladat: A 3000 lábnál magasabb hegyek száma: {hegyek.ErteknelMagasabbakSzama(3000)} db");
+
+            Fajlbairas(3);
+
+            void Fajlbairas(int darabszam)
+            {
+                List<string> kigyujtott = new List<string>();
+
+                foreach (var item in hegyek.SorbaRendezett.Take(darabszam))
+                {
+                    kigyujtott.Add(item.ToString());
+                }
+
+                File.WriteAllLines("haromlegmagasabb.txt", kigyujtott);
+            }
+        }
+    }
+}
