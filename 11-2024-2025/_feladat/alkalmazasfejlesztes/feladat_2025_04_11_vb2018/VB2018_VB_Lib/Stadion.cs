ae8c175c9ae8add4e379ffd6c9a0d3f29286f3dd
VB2007 <admin@vb2007.hu>
2025-04-12 15:29:46 +0200

    made stadion class

--- 11-2024-2025/_feladat/alkalmazasfejlesztes/feladat_2025_04_11_vb2018/VB2018_VB_Lib/Stadion.cs
+++ 11-2024-2025/_feladat/alkalmazasfejlesztes/feladat_2025_04_11_vb2018/VB2018_VB_Lib/Stadion.cs
@@ -2,6 +2,19 @@
 {
     public class Stadion
     {
+        public string Varos { get; init; }
+        public string Nev1 { get; init; }
+        public string Nev2 { get; init; }
+        public int Ferohely { get; init; }
 
+        public Stadion(string dataLines)
+        {
+            string[] splitted = dataLines.Split(';');
+
+            Varos = splitted[0];
+            Nev1 = splitted[1];
+            Nev2 = splitted[2];
+            Ferohely = int.Parse(splitted[3]);
+        }
     }
 }
