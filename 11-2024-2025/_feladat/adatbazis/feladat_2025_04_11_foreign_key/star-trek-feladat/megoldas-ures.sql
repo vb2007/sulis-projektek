3fb3c29929127c317eb7df339818cc90e4a45a60
VB2007 <admin@vb2007.hu>
2025-04-12 18:35:11 +0200

    added base files

---
+++ 11-2024-2025/_feladat/adatbazis/feladat_2025_04_11_foreign_key/star-trek-feladat/megoldas-ures.sql
@@ -0,0 +1,31 @@
+-- 3. feladat
+
+-- 4. feladat
+
+-- 6. feladat
+
+-- 7. feladat
+
+-- 8. feladat
+
+-- 9. feladat
+
+-- 10. feladat
+
+-- 11. feladat
+
+-- 12. feladat
+
+-- 13. feladat
+
+-- 14. feladat
+
+-- 15. feladat
+
+-- 16. feladat
+
+-- 17. feladat
+
+-- 18. feladat
+
+-- 19. feladat
---
+++ 11-2024-2025/_feladat/adatbazis/feladat_2025_04_11_foreign_key/star-trek-feladat/st-fajok-szerepek.sql
@@ -0,0 +1,54 @@
+INSERT INTO fajok
+VALUES
+(15, 'Andoriai'),
+(18, 'Bajori'),
+(5, 'Borg'),
+(13, 'Breen'),
+(12, 'Cardassiai'),
+(9, 'Egyesült Földi Csillagflotta'),
+(16, 'Ferengi'),
+(1, 'Föderáció'),
+(10, 'Gorn'),
+(14, 'Harogin'),
+(4, 'Jem\'Hadar'),
+(2, 'Klingon'),
+(11, 'Reman'),
+(3, 'Romulán'),
+(6, 'Talaxiai'),
+(8, 'Terrai'),
+(17, 'Tholian'),
+(7, 'Vulkáni')
+;
+
+
+INSERT INTO `hajo_szerepek`
+VALUES
+(17, ' '),
+(1, 'cirkáló'),
+(11, 'csatacirkáló'),
+(10, 'csatahajó'),
+(8, 'dreadnaught'),
+(22, 'elfogó'),
+(16, 'felderítő hajó'),
+(14, 'felfedező hajó'),
+(9, 'fregatt'),
+(18, 'futárhajó'),
+(21, 'holohajó'),
+(2, 'kísérőhajó'),
+(23, 'közepes cirkáló'),
+(7, 'kutatóhajó'),
+(13, 'nehéz harcimadár'),
+(15, 'nehéz romboló'),
+(4, 'ragadozómadár'),
+(26, 'romboló'),
+(5, 'szarkofág'),
+(24, 'személyszállító'),
+(19, 'támogató hajó'),
+(3, 'teherhajó'),
+(6, 'űrkomp'),
+(12, 'vadászgép'),
+(20, 'vadászhajó'),
+(27, 'könnyű cirkáló'),
+(28, 'nehéz cirkáló'),
+(29, 'nehéz felderítő')
+;
