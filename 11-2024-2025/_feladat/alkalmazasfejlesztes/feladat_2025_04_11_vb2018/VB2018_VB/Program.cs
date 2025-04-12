31e282be3a07a35df60839cbcdacde22de029f36
VB2007 <admin@vb2007.hu>
2025-04-12 15:37:19 +0200

    read data to class

--- 11-2024-2025/_feladat/alkalmazasfejlesztes/feladat_2025_04_11_vb2018/VB2018_VB/Program.cs
+++ 11-2024-2025/_feladat/alkalmazasfejlesztes/feladat_2025_04_11_vb2018/VB2018_VB/Program.cs
@@ -1,10 +1,12 @@
-﻿namespace VB2018_VB
+﻿using VB2018_VB_Lib;
+
+namespace VB2018_VB
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            Stadions stadions = new(File.ReadLines("vb2018.txt").Skip(1));
         }
     }
 }
