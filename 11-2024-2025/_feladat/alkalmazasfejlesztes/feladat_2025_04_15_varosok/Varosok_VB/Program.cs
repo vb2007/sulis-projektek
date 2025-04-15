869de9ecdc6d8bac109ef3584d06346ea2d06e5b
Balázs Vágvölgyi <balazs.vagvolgyi@linistry.com>
2025-04-15 12:02:06 +0200

    updates cities class and solved task 2

--- 11-2024-2025/_feladat/alkalmazasfejlesztes/feladat_2025_04_15_varosok/Varosok_VB/Program.cs
+++ 11-2024-2025/_feladat/alkalmazasfejlesztes/feladat_2025_04_15_varosok/Varosok_VB/Program.cs
@@ -1,7 +1,20 @@
-﻿internal class Program
+﻿using Varosok_VB_Lib;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Cities cities = new(File.ReadAllLines("varosok.csv").Skip(1));
+
+        //foreach (City city in cities.List)
+        //{
+        //    Console.WriteLine(city);
+        //}
+
+        Console.WriteLine($"2. feladat: Városok száma évenként:");
+        foreach (var item in cities.NumberOfCitiesByYear)
+        {
+            Console.WriteLine($"\t{item.Key} - {item.Value}");
+        }
     }
 }
--- 11-2024-2025/_feladat/alkalmazasfejlesztes/feladat_2025_04_15_varosok/Varosok_VB_Lib/Cities.cs
+++ 11-2024-2025/_feladat/alkalmazasfejlesztes/feladat_2025_04_15_varosok/Varosok_VB_Lib/Cities.cs
@@ -2,6 +2,21 @@
 {
     public class Cities
     {
+        private readonly List<City> citiesList = new();
 
+        public Cities(IEnumerable<string> dataLines)
+        {
+            foreach (var item in dataLines)
+            {
+                citiesList.Add(new City(item));
+            }
+        }
+
+        public IEnumerable<City> List => citiesList;
+
+        public Dictionary<int, int> NumberOfCitiesByYear =>
+            citiesList.GroupBy(x => x.Date.Year)
+            .ToDictionary(x => x.Key, x => x.Count())
+            .OrderBy(x => x.Key).ToDictionary();
     }
 }
