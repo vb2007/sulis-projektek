bab8e96308b5eb421f8f80de8e583856cecb0bff
Balázs Vágvölgyi <balazs.vagvolgyi@linistry.com>
2025-04-15 08:43:21 +0200

    added minecraft base task

---
+++ 11-2024-2025/_feladat/webprogramozas/feladat_2025_04_15_egy_minecraft_film/forras/src/js/main.js
@@ -0,0 +1,53 @@
+"use strict"
+
+const quotes = [
+    {
+        hu: 'Én, Steve vagyok!',
+        en: 'I am Steve!',
+        image: 'quotes/i_am_steve.png',
+    },
+    {
+        hu: 'Csirke Zsoké!',
+        en: 'Chicken Jockey!',
+        image: 'quotes/chicken_jockey.png',
+    },
+    {
+        hu: 'Tűz szerszám!',
+        en: 'Flint and steel!',
+        image: 'quotes/flint_and_steel.png',
+    },
+    {
+        hu: 'Az Alvilág!',
+        en: 'The Nether!',
+        image: 'quotes/the_nether.png',
+    },
+    {
+        hu: 'Ez egy barkácsasztal!',
+        en: 'This is a crafting table!',
+        image: 'quotes/crafting_table.png',
+    },
+    {
+        hu: 'Forrá láva és csirke!',
+        en: 'Hot lava and chicken!',
+        image: 'quotes/hot_lava_and_chicken.png',
+    },
+    {
+        hu: 'Gyémánt páncél, teljes szett!',
+        en: 'Diamond armor, full set!',
+        image: 'quotes/diamond_armor.png',
+    },
+    {
+        hu: 'Kiengedés!',
+        en: 'Release!',
+        image: 'quotes/release.png',
+    },
+    {
+        hu: 'Forrón jövök be!',
+        en: 'Coming in hot!',
+        image: 'quotes/coming_in_hot.png',
+    },
+];
+
+const ticketsSold = [56, 34, 23, 45, 67, 89, 12, 34, 56, 78,];
+
+const selectableLanguages = ['en', 'hu'];
