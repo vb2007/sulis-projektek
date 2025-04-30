3a448e4567e0a72eb9076ea672b986e65b121e84
VB2007 <admin@vb2007.hu>
2025-04-30 23:41:20 +0200

    added vehicle class

--- 11-2024-2025/_feladat/alkalmazasfejlesztes/feladat_2025_04_29_gepkocsik/Gepkocsi_VB_Lib/Vehicle.cs
+++ 11-2024-2025/_feladat/alkalmazasfejlesztes/feladat_2025_04_29_gepkocsik/Gepkocsi_VB_Lib/Vehicle.cs
@@ -2,6 +2,24 @@
 {
     public class Vehicle
     {
+        //rendszam;tnev;cim;gyartmany;tipus;hurtart
+        public string LicensePlate { get; init; }
+        public string OwnerName { get; init; }
+        public string Address { get; init; }
+        public string Manufacturer { get; init; }
+        public string Model { get; init; }
+        public int CylinderCapacity { get; init; }
 
+        public Vehicle(string line)
+        {
+            string[] splitted = line.Split(';');
+
+            LicensePlate = splitted[0];
+            OwnerName = splitted[1];
+            Address = splitted[2];
+            Manufacturer = splitted[3];
+            Model = splitted[4];
+            CylinderCapacity = int.Parse(splitted[5]);
+        }
     }
 }
