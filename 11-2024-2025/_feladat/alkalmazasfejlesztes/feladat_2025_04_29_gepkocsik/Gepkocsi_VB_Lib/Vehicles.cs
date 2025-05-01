32b8e3533b2b9581652d15819bcb32528ee3d328
VB2007 <admin@vb2007.hu>
2025-05-01 20:27:15 +0200

    added proper variables to vehicles class

--- 11-2024-2025/_feladat/alkalmazasfejlesztes/feladat_2025_04_29_gepkocsik/Gepkocsi_VB_Lib/Vehicles.cs
+++ 11-2024-2025/_feladat/alkalmazasfejlesztes/feladat_2025_04_29_gepkocsik/Gepkocsi_VB_Lib/Vehicles.cs
@@ -2,6 +2,16 @@
 {
     public class Vehicles
     {
-        
+        private readonly List<Vehicle> vehiclesList = new();
+
+        public Vehicles(IEnumerable<string> dataLines)
+        {
+            foreach (var item in dataLines)
+            {
+                vehiclesList.Add(new (item));
+            }
+        }
+
+        public IEnumerable<Vehicle> List => vehiclesList;
     }
 }
