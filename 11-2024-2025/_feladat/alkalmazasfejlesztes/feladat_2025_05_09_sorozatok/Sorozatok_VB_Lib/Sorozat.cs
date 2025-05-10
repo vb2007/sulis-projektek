9808737a08143dc61fb9569f0d989bab419115f8
VB2007 <admin@vb2007.hu>
2025-05-10 02:58:33 +0200

    added sorozat class

--- 11-2024-2025/_feladat/alkalmazasfejlesztes/feladat_2025_05_09_sorozatok/Sorozatok_VB_Lib/Sorozat.cs
+++ 11-2024-2025/_feladat/alkalmazasfejlesztes/feladat_2025_05_09_sorozatok/Sorozatok_VB_Lib/Sorozat.cs
@@ -2,6 +2,24 @@
 {
     public class Sorozat
     {
+        //cim;rendezo;szarmazas;kezdesEve;kategoria;evadok
+        public string Cim { get; init; }
+        public string Rendezo { get; init; }
+        public string Szarmazas { get; init; }
+        public int KezdesEve { get; init; }
+        public string Kategoria { get; init; }
+        public int Evadok { get; init; }
 
+        public Sorozat(string sor)
+        {
+            string[] adatok = sor.Split(';');
+
+            Cim = adatok[0];
+            Rendezo = adatok[1];
+            Szarmazas = adatok[2];
+            KezdesEve = int.Parse(adatok[3]);
+            Kategoria = adatok[4];
+            Evadok = int.Parse(adatok[5]);
+        }
     }
 }
