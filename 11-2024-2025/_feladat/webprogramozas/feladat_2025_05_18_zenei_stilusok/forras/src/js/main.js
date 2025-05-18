2a515acc91b08e7e431facca8d8f5d2b94f1e22f
VB2007 <admin@vb2007.hu>
2025-05-18 19:14:15 +0200

    completed fucking dogshit js

--- 11-2024-2025/_feladat/webprogramozas/feladat_2025_05_18_zenei_stilusok/forras/src/js/main.js
+++ 11-2024-2025/_feladat/webprogramozas/feladat_2025_05_18_zenei_stilusok/forras/src/js/main.js
@@ -1,4 +1,4 @@
-"use strict"
+"use strict";
 
 const artists = [
     { name: "Akkezdet Phiai", style: "rap-hiphop" },
@@ -19,4 +19,56 @@ const artists = [
     { name: "Halott Pénz", style: "other" }
 ];
 
+const getStyleNameByValue = (styleValue) => {
+    switch(styleValue) {
+        case "rap-hiphop":
+            return "Rap / Hip-Hop";
+        case "electronic-experimental": 
+            return "Elektronikus / Kísérleti";
+        case "rock-alternative":
+            return "Rock / Alternatív";
+        case "other":
+            return "Egyéb / Vegyes";
+    }
+}
 
+const filterArtists = (style) => {
+    return artists.filter(artist => artist.style === style);
+};
+
+const renderCard = (username, style) => {
+    const card = document.getElementById("card");
+    card.classList.add("active");
+    card.style.display = "block";
+    card.style.marginLeft = "auto";
+    card.style.marginRight = "auto";
+
+    const h3 = card.querySelector("h3");
+    h3.textContent = `${username} zenei stílusa: ${getStyleNameByValue(style)}`;
+
+    const cardDiv = card.querySelector("div");
+    cardDiv.innerHTML = "";
+
+    const img = document.createElement("img");
+    img.src = `./src/img/${style}.jpg`;
+
+    const ul = document.createElement("ul");
+    let artists = filterArtists(style);
+
+    artists.forEach(artist => {
+        const li = document.createElement("li");
+        li.textContent = artist.name;
+        ul.append(li);
+    });
+
+    cardDiv.append(img, ul);
+};
+
+document.querySelector("button").addEventListener("click", (event) => {
+    event.preventDefault();
+
+    const name = document.getElementById("name").value;
+    const style = document.getElementById("style").value;
+
+    renderCard(name, style);
+});
