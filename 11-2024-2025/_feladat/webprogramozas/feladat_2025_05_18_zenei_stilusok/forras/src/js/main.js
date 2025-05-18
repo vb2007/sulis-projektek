51809746ab9dcfdde199b4016611f289d94b263e
VB2007 <admin@vb2007.hu>
2025-05-18 13:17:46 +0200

    added source

---
+++ 11-2024-2025/_feladat/webprogramozas/feladat_2025_05_18_zenei_stilusok/forras/src/js/main.js
@@ -0,0 +1,22 @@
+"use strict"
+
+const artists = [
+    { name: "Akkezdet Phiai", style: "rap-hiphop" },
+    { name: "Cserihanna", style: "rap-hiphop" },
+    { name: "Eminem", style: "rap-hiphop" },
+    { name: "Krúbi", style: "rap-hiphop" },
+    { name: "NKS", style: "rap-hiphop" },
+
+    { name: "Fehér Krisztián", style: "electronic-experimental" },
+    { name: "Keygen Church", style: "electronic-experimental" },
+    { name: "VØJ", style: "electronic-experimental" },
+
+    { name: "Gorillaz", style: "rock-alternative" },
+    { name: "Ronnie Radke", style: "rock-alternative" },
+
+    { name: "Alexandrov Kórus", style: "other" },
+    { name: "Andrius Klimka", style: "other" },
+    { name: "Halott Pénz", style: "other" }
+];
+
+
