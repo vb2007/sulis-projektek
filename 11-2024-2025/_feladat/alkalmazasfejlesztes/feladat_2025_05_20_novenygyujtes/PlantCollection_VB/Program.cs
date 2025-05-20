e74ac74233632f1be8194218e31b1794f0fc5982
Balázs Vágvölgyi <balazs.vagvolgyi@linistry.com>
2025-05-20 11:37:57 +0200

    created project, added reference, added interface

---
+++ 11-2024-2025/_feladat/alkalmazasfejlesztes/feladat_2025_05_20_novenygyujtes/PlantCollection_VB/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PlantCollection_VB
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ 11-2024-2025/_feladat/alkalmazasfejlesztes/feladat_2025_05_20_novenygyujtes/PlantCollection_VB_Lib/Virag.cs
@@ -0,0 +1,7 @@
+﻿namespace PlantCollection_VB_Lib
+{
+    public class Virag
+    {
+
+    }
+}
