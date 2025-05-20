e5aae443bf8bfa2da81dea0cc7406d9a5bdcc10b
VB2007 <admin@vb2007.hu>
2025-05-20 12:02:31 +0200

    updated plant factory with matrix

--- 11-2024-2025/_feladat/alkalmazasfejlesztes/feladat_2025_05_20_novenygyujtes/PlantCollection_VB_Lib/PlantFactory.cs
+++ 11-2024-2025/_feladat/alkalmazasfejlesztes/feladat_2025_05_20_novenygyujtes/PlantCollection_VB_Lib/PlantFactory.cs
@@ -5,9 +5,36 @@
         private readonly List<IPlant> plants = new();
         private static readonly Random random = new();
 
+        public PlantFactory(int matrixSize)
+        {
+            char[,] matrix = new char[matrixSize, matrixSize];
+            for (int i = 0; i < matrixSize; i++)
+            {
+                for (int j = 0; j < matrixSize; j++)
+                {
+                    matrix[i, j] = Create();
+                }
+            }
+        }
+
         public IPlant Create()
         {
+            int plantType = random.Next(3); //3 types: 0, 1, 2
 
+            switch (plantType)
+            {
+                case 0:
+                    plants.Add(new Flover());
+                    break;
+                case 1:
+                    plants.Add(new Herb());
+                    break;
+                case 2:
+                    plants.Add(new Mushroom());
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(plantType), "Érvénytelen növénytípus.");
+            }
         }
     }
 }
