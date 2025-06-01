c83711eed3ba0e21860a9992f8e90fd1d836efae
VB2007 <admin@vb2007.hu>
2025-06-01 15:55:48 +0200

    added import & file reading + count

--- 11-2024-2025/_feladat/alkalmazasfejlesztes/feladat_2025_05_23_footgolf/Footgolf_VB/Program.cs
+++ 11-2024-2025/_feladat/alkalmazasfejlesztes/feladat_2025_05_23_footgolf/Footgolf_VB/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace Footgolf_VB
+﻿using Footgolf_VB_Lib;
+
+namespace Footgolf_VB
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Competitors competitors = new(File.ReadAllLines("fob2016.txt"));
+
+            Console.WriteLine($"3. feladat: Versenyzők száma: {competitors.Count}");
         }
     }
 }
--- 11-2024-2025/_feladat/alkalmazasfejlesztes/feladat_2025_05_23_footgolf/Footgolf_VB_Lib/Competitors.cs
+++ 11-2024-2025/_feladat/alkalmazasfejlesztes/feladat_2025_05_23_footgolf/Footgolf_VB_Lib/Competitors.cs
@@ -2,6 +2,16 @@
 {
     public class Competitors
     {
+        private readonly List<Competitor> competitors = new();
 
+        public Competitors(IEnumerable<string> dataLines)
+        {
+            foreach (var competitor in dataLines)
+            {
+                competitors.Add(new(competitor));
+            }
+        }
+
+        public int Count => competitors.Count;
     }
 }
