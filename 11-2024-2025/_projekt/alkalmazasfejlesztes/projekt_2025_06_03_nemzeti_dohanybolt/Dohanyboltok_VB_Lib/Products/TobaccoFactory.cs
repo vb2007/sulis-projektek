afed3f86fab800fa9b536be0c8bb748048eb30fd
VB2007 <admin@vb2007.hu>
2025-06-10 00:50:46 +0200

    added random generation login to tobacco factory

--- 11-2024-2025/_projekt/alkalmazasfejlesztes/projekt_2025_06_03_nemzeti_dohanybolt/Dohanyboltok_VB_Lib/Products/TobaccoFactory.cs
+++ 11-2024-2025/_projekt/alkalmazasfejlesztes/projekt_2025_06_03_nemzeti_dohanybolt/Dohanyboltok_VB_Lib/Products/TobaccoFactory.cs
@@ -8,9 +8,33 @@
         private readonly List<string> CigaretteNames = new List<string> { "Marlboro Gold", "Yellow Camel", "Marlboro Blue Ice", "Pall Mall Gold", "Marlboro Red" };
         private readonly List<string> CigarNames = new List<string> { "Djarum Black Ruby", "Amigos Mini Filter", "Davidoff Demi Tasse", "Djarum Black Emerald", "Marlboro Leaf Beyond Blue" };
 
-        public TobaccoFactory()
+        public TobaccoFactory(int amount)
         {
+            for (int i = 0; i < amount; i++)
+            {
+                products.Add(Create());
+            }
+        }
 
+        private IProduct Create()
+        {
+            int cigaretteOrCigar = rnd.Next(0, 2);
+            int price = rnd.Next(700, 2501); // 700-2500 Ft
+
+            if (cigaretteOrCigar == 0) //cigarette
+            {
+                int nicotineContent = rnd.Next(0, 21); // 0-20 mg
+                string name = CigaretteNames[rnd.Next(CigaretteNames.Count)];
+
+                return new Cigarette(name, price, nicotineContent);
+            }
+            else //cigar
+            {
+                int length = rnd.Next(5, 31); // 5-30 cm
+                string name = CigarNames[rnd.Next(CigarNames.Count)];
+
+                return new Cigar(name, price, length);
+            }
         }
     }
 }
