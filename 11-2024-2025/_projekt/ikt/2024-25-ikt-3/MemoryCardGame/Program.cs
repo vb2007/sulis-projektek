e6c83933940a0c532625d47f7d53c0f0c1647311
VB2007 <admin@vb2007.hu>
2025-09-01 22:36:44 +0200

    backed up 2024-25-ikt-3

---
+++ 11-2024-2025/_projekt/ikt/2024-25-ikt-3/MemoryCardGame/Program.cs
@@ -0,0 +1,15 @@
+﻿using MemoryCardGame.Core;
+using MemoryCardGame.Other;
+
+namespace MemoryCardGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Memóriakártya Játék";
+            Game game = new Game();
+            game.Start();
+        }
+    }
+}
