beeea99f2bdd235b6d25a846e8ae5105d188c5c8
VB2007 <admin@vb2007.hu>
2025-09-02 20:18:48 +0200

    backed up feladat_2023_03_13_bevezeto

---
+++ 9-2022-2023/_feladat/python/feladat_2023_03_13_bevezeto/orai_munka.py
@@ -0,0 +1,26 @@
+nev = "asd";
+nev = "Asd";
+print(nev);
+
+'''_,_= "asd";
+print(X%);'''
+
+alma,fa = 4,5;
+print(alma);
+print(fa);
+
+x = 2;
+x += 1;
+print(x);
+
+y = 2;
+y *= 3;
+print(y);
+
+mese = "egyszer\nvolt\nhol\nnem\nvolt"
+print(mese);
+
+gyumolcs = ["alma", "korte", "cseresznye"]
+
+for gyumolcs in gyumolcs:
+    print(gyumolcs, "fa", end=", ")
