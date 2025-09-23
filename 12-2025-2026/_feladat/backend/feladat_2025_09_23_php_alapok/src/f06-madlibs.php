d60dac2f69578df3bd119ac47cf4375c0e915479
VB2007 <admin@vb2007.hu>
2025-09-23 23:12:48 +0200

    added & completed missing f06-madlibs.php backend task

---
+++ 12-2025-2026/_feladat/backend/feladat_2025_09_23_php_alapok/src/f06-madlibs.php
@@ -0,0 +1,13 @@
+<?php
+
+$kereskedo = "kereskedő";
+$erdon = "erdőn";
+$egymast = "egymást";
+$penz = "pénz";
+$rablokkal = "rablókkal";
+$penzet = "pénzét";
+
+echo "Vásárba indult három ", $kereskedo, ". Rengeteg nagy ", $erdon, " kellett átmenniük, s amint mentek, mendegéltek, rájuk esteledett.\n";
+echo "Hiszen ha csak rájuk esteledett volna! Olyan sűrű, vastag sötétség ereszkedett le, hogy fejszét lehetett volna rája akasztani.\n";
+echo "Persze hogy nem látták ", $egymast, " s egyszerre csak azon vette észre magát az egyik ", $kereskedo, ", hogy egymaga tapogatódzik a vaksötétségben, a másik kettő isten tudja, hová merre kalandozott el mellőle.\n";
+echo "Na, most mit csináljon szegény feje? Sok ",  $penz, " volt nála, félt, hátha rablókkal ", $rablokkal, ", s mind azon tűnődött, hová rejthetné el a ", $penzet, ".\n";
