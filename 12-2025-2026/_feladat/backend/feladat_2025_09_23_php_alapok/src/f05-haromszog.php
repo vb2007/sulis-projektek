eae177ffe970ab85c302df53723e71b702c49a35
VB2007 <admin@vb2007.hu>
2025-09-23 12:23:56 +0200

    simplified task 5

--- 12-2025-2026/_feladat/backend/feladat_2025_09_23_php_alapok/src/f05-haromszog.php
+++ 12-2025-2026/_feladat/backend/feladat_2025_09_23_php_alapok/src/f05-haromszog.php
@@ -4,17 +4,7 @@ $a = (float) $argv[1];
 $b = (float) $argv[2];
 $c = (float) $argv[3];
 
-if ($a + $b < $c) {
-    echo "A háromszög nem szerkeszthető meg\n";
-    return;
-}
-
-if ($b + $c < $a) {
-    echo "A háromszög nem szerkeszthető meg\n";
-    return;
-}
-
-if ($a + $c < $b) {
+if ($a + $b <= $c || $b + $c <= $a || $a + $c <= $b) {
     echo "A háromszög nem szerkeszthető meg\n";
     return;
 }
