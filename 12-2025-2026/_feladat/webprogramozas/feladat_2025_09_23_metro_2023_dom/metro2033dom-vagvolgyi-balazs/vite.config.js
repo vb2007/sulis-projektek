e6989f9b64e8613b9cfbdbb3360645c5fb751127
VB2007 <admin@vb2007.hu>
2025-09-23 17:18:25 +0200

    fixed project root & updated formatting in vite config

--- 12-2025-2026/_feladat/webprogramozas/feladat_2025_09_23_metro_2023_dom/metro2033dom-vagvolgyi-balazs/vite.config.js
+++ 12-2025-2026/_feladat/webprogramozas/feladat_2025_09_23_metro_2023_dom/metro2033dom-vagvolgyi-balazs/vite.config.js
@@ -1,18 +1,18 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  root: './',
+  root: "./src",
   server: {
     port: 3000,
     open: true,
-    host: true
+    host: true,
   },
   build: {
-    outDir: 'dist',
+    outDir: "dist",
     rollupOptions: {
       input: {
-        main: './metro2033.html'
-      }
-    }
-  }
-})
+        main: "./metro2033.html",
+      },
+    },
+  },
+});
