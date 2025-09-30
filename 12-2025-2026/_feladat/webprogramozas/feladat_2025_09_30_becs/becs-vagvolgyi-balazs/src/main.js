2724ed7dd298034224e14610c9199728c460be34
VB2007 <admin@vb2007.hu>
2025-09-30 11:16:02 +0200

    added task source

---
+++ 12-2025-2026/_feladat/webprogramozas/feladat_2025_09_30_becs/becs-vagvolgyi-balazs/src/main.js
@@ -0,0 +1,18 @@
+"use strict"
+
+const buses = 166200000;
+const ubahn = 352400000;
+const trams = 273400000;
+
+
+
+
+
+const walking = '32%: sétáló';
+const publicTransit = '32%: tömegközelekedő';
+const cycling = '10%: biciklista';
+const driving = '26%: autós';
+
+
+
+
