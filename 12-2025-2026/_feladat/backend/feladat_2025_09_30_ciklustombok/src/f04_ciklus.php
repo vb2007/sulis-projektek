140ad733f9133b931ccf6697ffa910f4331cecb6
VB2007 <admin@vb2007.hu>
2025-09-30 10:39:39 +0200

    started 4th task

--- 12-2025-2026/_feladat/backend/feladat_2025_09_30_ciklustombok/src/f04_ciklus.php
+++ 12-2025-2026/_feladat/backend/feladat_2025_09_30_ciklustombok/src/f04_ciklus.php
@@ -2,3 +2,16 @@
 
 $a = $argv[1];
 $b = $argv[2];
+
+if ($a > $b) {
+    while ($b > $a) {
+        echo "$b\n";
+        $b++;
+    }
+}
+else {
+    while ($a < $b) {
+        echo "$a\n";
+        $a++;
+    }
+}
