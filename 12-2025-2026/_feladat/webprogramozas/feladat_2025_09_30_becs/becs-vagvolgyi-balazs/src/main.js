6a35844f9a801787568ab997518b77d34bbe1aa1
VB2007 <admin@vb2007.hu>
2025-10-05 00:53:41 +0200

    completed rest of js tasks

--- 12-2025-2026/_feladat/webprogramozas/feladat_2025_09_30_becs/becs-vagvolgyi-balazs/src/main.js
+++ 12-2025-2026/_feladat/webprogramozas/feladat_2025_09_30_becs/becs-vagvolgyi-balazs/src/main.js
@@ -11,3 +11,32 @@ const driving = "26%: autós";
 
 const passengers = document.getElementById("passengers");
 passengers.textContent = `${Math.round(buses + ubahn + trams, 1) / 1000000} millió`;
+
+const createBar = (width, text) => {
+  const barWrapper = document.createElement("div");
+
+  const bar = document.createElement("div");
+  bar.classList.add("bar");
+  bar.textContent = width;
+
+  barWrapper.append(bar, text);
+
+  return barWrapper;
+};
+
+const diagram = document.getElementById("diagram");
+diagram.innerHTML = "";
+
+const splitterString = "%: ";
+
+const walkingSplit = walking.split(splitterString);
+diagram.append(createBar(walkingSplit[0], walkingSplit[1]));
+
+const publicTransitSplit = publicTransit.split(splitterString);
+diagram.append(createBar(publicTransitSplit[0], publicTransitSplit[1]));
+
+const cyclingSplit = cycling.split(splitterString);
+diagram.append(createBar(cyclingSplit[0], cyclingSplit[1]));
+
+const drivingSplit = driving.split(splitterString);
+diagram.append(createBar(drivingSplit[0], drivingSplit[1]));
