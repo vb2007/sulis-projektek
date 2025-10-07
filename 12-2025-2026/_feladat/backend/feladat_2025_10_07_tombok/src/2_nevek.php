06582156d721f6c7611b8166481d417687dfd883
VB2007 <admin@vb2007.hu>
2025-10-07 10:10:00 +0200

    completed 2nd task 1-3

---
+++ 12-2025-2026/_feladat/backend/feladat_2025_10_07_tombok/src/2_nevek.php
@@ -0,0 +1,12 @@
+<?php
+//1.
+$nevek = ["Robert Downey Jr.", "Chris Hemsworth", "Scarlett Johansson", "Karen Gillan", "Benedict Cumberbatch"];
+
+//2.
+echo "2. feladat\n";
+echo "Első: $nevek[0]\n";
+
+//3,
+$utolsoKolcs = array_key_last($nevek);
+echo "3 .feladat\n";
+echo "Utolsó: $nevek[$utolsoKolcs]\n";
