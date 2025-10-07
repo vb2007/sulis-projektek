6d2518838b35dbc182fbccedcc1ec082bc02caa8
VB2007 <admin@vb2007.hu>
2025-10-07 10:12:40 +0200

    completed 4th task

--- 12-2025-2026/_feladat/backend/feladat_2025_10_07_tombok/src/2_nevek.php
+++ 12-2025-2026/_feladat/backend/feladat_2025_10_07_tombok/src/2_nevek.php
@@ -10,3 +10,9 @@ echo "Első: $nevek[0]\n";
 $utolsoKolcs = array_key_last($nevek);
 echo "3 .feladat\n";
 echo "Utolsó: $nevek[$utolsoKolcs]\n";
+
+//4.
+echo "4. feladat\n";
+for ($i = 0; $i < count($nevek); $i++) {
+    echo "- $nevek[$i]\n";
+}
