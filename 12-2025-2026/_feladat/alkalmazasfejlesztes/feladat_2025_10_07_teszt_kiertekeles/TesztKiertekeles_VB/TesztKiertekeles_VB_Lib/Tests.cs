95c425779e950a6623051c7dfd1c763ea54bf1e0
VB2007 <admin@vb2007.hu>
2025-10-07 14:22:22 +0200

    implemented tests class

--- 12-2025-2026/_feladat/alkalmazasfejlesztes/feladat_2025_10_07_teszt_kiertekeles/TesztKiertekeles_VB/TesztKiertekeles_VB_Lib/Tests.cs
+++ 12-2025-2026/_feladat/alkalmazasfejlesztes/feladat_2025_10_07_teszt_kiertekeles/TesztKiertekeles_VB/TesztKiertekeles_VB_Lib/Tests.cs
@@ -2,5 +2,13 @@ namespace TesztKiertekeles_VB_Lib;
 
 public class Tests
 {
-    
+    private readonly List<Test> tests = new List<Test>();
+
+    public Tests(IEnumerable<string> lines)
+    {
+        foreach (var line in lines)
+        {
+            tests.Add(new Test(line));
+        }
+    }
 }
