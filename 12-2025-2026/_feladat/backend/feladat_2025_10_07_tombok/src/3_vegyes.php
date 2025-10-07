faa22c1abdfa06183f76f06a3dcf49eddee8c064
VB2007 <admin@vb2007.hu>
2025-10-07 10:32:21 +0200

    added comments, type check

--- 12-2025-2026/_feladat/backend/feladat_2025_10_07_tombok/src/3_vegyes.php
+++ 12-2025-2026/_feladat/backend/feladat_2025_10_07_tombok/src/3_vegyes.php
@@ -1,7 +1,15 @@
 <?php
+//1.
 $vegyes = [5, 9, "Hello", 11.2, "Béla", 33, "Márta", 48.98, 7];
 
+//2.
 if ($argc != 2) {
     echo "A szkript pontosan egy paramétert vár!\n";
     return;
+}
+
+//3.
+$param = $argv[1];
+if (!is_numeric($param) && !is_string($param)) {
+    echo "A paraméter csak 'szamok', 'egesz', 'valos' vagy 'szoveg' lehet.\n";
 }
