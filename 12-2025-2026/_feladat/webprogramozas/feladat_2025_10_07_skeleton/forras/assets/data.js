26f4d807912ca04ab332636a320813fef8ba60c3
VB2007 <admin@vb2007.hu>
2025-10-08 14:53:11 +0200

    added source

---
+++ 12-2025-2026/_feladat/webprogramozas/feladat_2025_10_07_skeleton/forras/assets/data.js
@@ -0,0 +1,3 @@
+const data01 = ["A", "természetjárás", "csodálatos", "élményeket", "kínál", "mindenkinek", "aki", "szereti", "felfedezni", "az", "erdei", "ösvényeket", "és", "élvezni", "a", "friss", "levegőt", "valamint", "megfigyelni", "az", "állatokat"]
+const data02 = [-10, 50, -94, 63, 71, 42, -13, 34, -64, 77]
+const data03 = [3, 15, 90, 51, 55, 30, 75, 99, 15, 100]
---
+++ 12-2025-2026/_feladat/webprogramozas/feladat_2025_10_07_skeleton/forras/main.js
@@ -0,0 +1,38 @@
+/**
+ * A paraméterben megadott tömb alapján számold ki, 
+ * hogy a tömben található szavaknak, mekkora az átlagos 
+ * hosszuk. Az átlag a függvény visszatérési értéke legyen.
+ */
+function task01(arr){
+
+}
+
+/** 
+ * A paraméterben kapott tömbben található számokat alakítsd át 
+ * úgy, hogy minden számra vedd az abszolút értékét és oszd el hárommal,
+ * ezután az eredményt kerekítsd. Az így kapott tömb legyen a függvény visszatérési értéke. 
+ */
+function task02(arr){
+
+}
+/**
+ * Keresd meg azt az utolsó elemet a paraméterként kapott tömbben,
+ * amelyik hárommal és öttel is osztható.
+ */
+function task03(arr){
+
+}
+
+/**
+ * A paraméterben megadott tömbből add vissza annak az elemnek az indexét,
+ * amelyik pontosan 5 karakter hosszú. Ha több ilyen is van akkor az első előfordulást.
+ */
+function task04(arr){
+
+}
+/**
+ * A paraméterben megadott tömbből válogasd ki a pozitív páros számokat.
+ */
+function task05(arr){
+
+}
