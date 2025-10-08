f405bf7ba81a8ea9e1108f6afa446ac304fabec7
VB2007 <admin@vb2007.hu>
2025-10-08 15:29:16 +0200

    created package & vite config

---
+++ 12-2025-2026/_feladat/webprogramozas/feladat_2025_10_07_skeleton/skeleton-vagvolgyi-balazs/vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  root: "./",
+  server: {
+    port: 3000,
+    open: "index.html",
+    host: true,
+  },
+  build: {
+    outDir: "../dist",
+    assetsDir: "assets",
+    rollupOptions: {
+      input: "./index.html",
+    },
+  },
+});
