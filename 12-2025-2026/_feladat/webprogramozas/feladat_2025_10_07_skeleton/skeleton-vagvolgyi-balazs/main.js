3fca2be236a704bcc71d6b6b6ba294fa7c5b8d6c
VB2007 <admin@vb2007.hu>
2025-10-08 15:39:28 +0200

    completed task 1

--- 12-2025-2026/_feladat/webprogramozas/feladat_2025_10_07_skeleton/skeleton-vagvolgyi-balazs/main.js
+++ 12-2025-2026/_feladat/webprogramozas/feladat_2025_10_07_skeleton/skeleton-vagvolgyi-balazs/main.js
@@ -1,38 +1,32 @@
 /**
- * A paraméterben megadott tömb alapján számold ki, 
- * hogy a tömben található szavaknak, mekkora az átlagos 
+ * A paraméterben megadott tömb alapján számold ki,
+ * hogy a tömben található szavaknak, mekkora az átlagos
  * hosszuk. Az átlag a függvény visszatérési értéke legyen.
  */
-function task01(arr){
-
+function task01(arr) {
+  let wordsLengthTotal = 0;
+  arr.forEach((word) => (wordsLengthTotal += word.length));
+  return wordsLengthTotal / arr.length;
 }
 
-/** 
- * A paraméterben kapott tömbben található számokat alakítsd át 
+/**
+ * A paraméterben kapott tömbben található számokat alakítsd át
  * úgy, hogy minden számra vedd az abszolút értékét és oszd el hárommal,
- * ezután az eredményt kerekítsd. Az így kapott tömb legyen a függvény visszatérési értéke. 
+ * ezután az eredményt kerekítsd. Az így kapott tömb legyen a függvény visszatérési értéke.
  */
-function task02(arr){
-
-}
+function task02(arr) {}
 /**
  * Keresd meg azt az utolsó elemet a paraméterként kapott tömbben,
  * amelyik hárommal és öttel is osztható.
  */
-function task03(arr){
-
-}
+function task03(arr) {}
 
 /**
  * A paraméterben megadott tömbből add vissza annak az elemnek az indexét,
  * amelyik pontosan 5 karakter hosszú. Ha több ilyen is van akkor az első előfordulást.
  */
-function task04(arr){
-
-}
+function task04(arr) {}
 /**
  * A paraméterben megadott tömbből válogasd ki a pozitív páros számokat.
  */
-function task05(arr){
-
-}
+function task05(arr) {}
