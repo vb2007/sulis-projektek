76331da87078afe3c330734ef387197dda3bb883
VB2007 <admin@vb2007.hu>
2025-10-08 02:12:27 +0200

    fixed type issues

--- 12-2025-2026/_feladat/alkalmazasfejlesztes/feladat_2025_10_07_teszt_kiertekeles/TesztKiertekeles_VB/TesztKiertekeles_VB_Lib/Tests.cs
+++ 12-2025-2026/_feladat/alkalmazasfejlesztes/feladat_2025_10_07_teszt_kiertekeles/TesztKiertekeles_VB/TesztKiertekeles_VB_Lib/Tests.cs
@@ -19,51 +19,52 @@ public class Tests
         tests.Any(x => x.Name == studentName) ? tests.First(x => x.Name == studentName) : null!;
 
     //3. feladat
-    private static int? Task1Count => tests.Count(x => x.Score1 != null);
-    private static int? Task2Count => tests.Count(x => x.Score2 != null);
-    private static int? Task3Count => tests.Count(x => x.Score3 != null);
-    private static int? Task4Count => tests.Count(x => x.Score4 != null);
-    private static int? Task5Count => tests.Count(x => x.Score5 != null);
-    private static int? Task1Average => tests.Sum(x => x.Score1) / testCount;
-    private static int? Task2Average => tests.Sum(x => x.Score2) / testCount;
-    private static int? Task3Average => tests.Sum(x => x.Score3) / testCount;
-    private static int? Task4Average => tests.Sum(x => x.Score4) / testCount;
-    private static int? Task5Average => tests.Sum(x => x.Score5) / testCount;
+    private int? Task1Count => tests.Count(x => x.Score1 != null);
+    private int? Task2Count => tests.Count(x => x.Score2 != null);
+    private int? Task3Count => tests.Count(x => x.Score3 != null);
+    private int? Task4Count => tests.Count(x => x.Score4 != null);
+    private int? Task5Count => tests.Count(x => x.Score5 != null);
+    
+    private double? Task1Average => Task1Count > 0 ? (double)tests.Where(x => x.Score1 != null).Sum(x => x.Score1!.Value) / Task1Count : null;
+    private double? Task2Average => Task2Count > 0 ? (double)tests.Where(x => x.Score2 != null).Sum(x => x.Score2!.Value) / Task2Count : null;
+    private double? Task3Average => Task3Count > 0 ? (double)tests.Where(x => x.Score3 != null).Sum(x => x.Score3!.Value) / Task3Count : null;
+    private double? Task4Average => Task4Count > 0 ? (double)tests.Where(x => x.Score4 != null).Sum(x => x.Score4!.Value) / Task4Count : null;
+    private double? Task5Average => Task5Count > 0 ? (double)tests.Where(x => x.Score5 != null).Sum(x => x.Score5!.Value) / Task5Count : null;
 
-    private static int? Task1NullCount =>
+    private int? Task1NullCount =>
         tests
             .Where(x => x.Score1 != null || x.Score2 != null || x.Score3 != null || x.Score4 != null ||
                         x.Score5 != null)
             .Count(x => x.Score1 == null);
 
-    private static int? Task2NullCount =>
+    private int? Task2NullCount =>
         tests
             .Where(x => x.Score1 != null || x.Score2 != null || x.Score3 != null || x.Score4 != null ||
                         x.Score5 != null)
             .Count(x => x.Score1 == null);
 
-    private static int? Task3NullCount =>
+    private int? Task3NullCount =>
         tests
             .Where(x => x.Score1 != null || x.Score2 != null || x.Score3 != null || x.Score4 != null ||
                         x.Score5 != null)
             .Count(x => x.Score1 == null);
 
-    private static int? Task4NullCount =>
+    private int? Task4NullCount =>
         tests
             .Where(x => x.Score1 != null || x.Score2 != null || x.Score3 != null || x.Score4 != null ||
                         x.Score5 != null)
             .Count(x => x.Score1 == null);
 
-    private static int? Task5NullCount =>
+    private int? Task5NullCount =>
         tests
             .Where(x => x.Score1 != null || x.Score2 != null || x.Score3 != null || x.Score4 != null ||
                         x.Score5 != null)
             .Count(x => x.Score1 == null);
 
-    public readonly string TaskDataString =
-        $"1. Teszt\nEnnyien oldották meg: {Task1Count}\nEnnyi az átlagos pontszám: {Task1Average}\nEnnyi tanuló nem adott be semmit, de megírta a tesztet: {Task1NullCount}\n\n" +
-        $"2. Teszt\nEnnyien oldották meg: {Task2Count}\nEnnyi az átlagos pontszám: {Task2Average}\nEnnyi tanuló nem adott be semmit, de megírta a tesztet: {Task2NullCount}\n\n" +
-        $"3. Teszt\nEnnyien oldották meg: {Task3Count}\nEnnyi az átlagos pontszám: {Task3Average}\nEnnyi tanuló nem adott be semmit, de megírta a tesztet: {Task3NullCount}\n\n" +
-        $"4. Teszt\nEnnyien oldották meg: {Task4Count}\nEnnyi az átlagos pontszám: {Task4Average}\nEnnyi tanuló nem adott be semmit, de megírta a tesztet: {Task4NullCount}\n\n" +
-        $"5. Teszt\nEnnyien oldották meg: {Task5Count}\nEnnyi az átlagos pontszám: {Task5Average}\nEnnyi tanuló nem adott be semmit, de megírta a tesztet: {Task5NullCount}\n\n";
+    public string TaskDataString =>
+        $"1. Teszt\nEnnyien oldották meg: {Task1Count}\nEnnyi az átlagos pontszám: {Task1Average:F2}\nEnnyi tanuló nem adott be semmit, de megírta a tesztet: {Task1NullCount}\n\n" +
+        $"2. Teszt\nEnnyien oldották meg: {Task2Count}\nEnnyi az átlagos pontszám: {Task2Average:F2}\nEnnyi tanuló nem adott be semmit, de megírta a tesztet: {Task2NullCount}\n\n" +
+        $"3. Teszt\nEnnyien oldották meg: {Task3Count}\nEnnyi az átlagos pontszám: {Task3Average:F2}\nEnnyi tanuló nem adott be semmit, de megírta a tesztet: {Task3NullCount}\n\n" +
+        $"4. Teszt\nEnnyien oldották meg: {Task4Count}\nEnnyi az átlagos pontszám: {Task4Average:F2}\nEnnyi tanuló nem adott be semmit, de megírta a tesztet: {Task4NullCount}\n\n" +
+        $"5. Teszt\nEnnyien oldották meg: {Task5Count}\nEnnyi az átlagos pontszám: {Task5Average:F2}\nEnnyi tanuló nem adott be semmit, de megírta a tesztet: {Task5NullCount}\n\n";
 }
