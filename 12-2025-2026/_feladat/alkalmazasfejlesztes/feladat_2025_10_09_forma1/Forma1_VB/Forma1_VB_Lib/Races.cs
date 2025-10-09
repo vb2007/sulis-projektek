bc1b59bc56cb8786fe5d762e99e75734a67daf3f
VB2007 <admin@vb2007.hu>
2025-10-09 15:03:14 +0200

    created races class

--- 12-2025-2026/_feladat/alkalmazasfejlesztes/feladat_2025_10_09_forma1/Forma1_VB/Forma1_VB_Lib/Races.cs
+++ 12-2025-2026/_feladat/alkalmazasfejlesztes/feladat_2025_10_09_forma1/Forma1_VB/Forma1_VB_Lib/Races.cs
@@ -2,5 +2,13 @@ namespace Forma1_VB_Lib;
 
 public class Races
 {
-     
+     private readonly List<Race> _races = new();
+
+     public Races(IEnumerable<string> races)
+     {
+          foreach (var race in races)
+          {
+               _races.Add(new Race(race));
+          }
+     }
 }
