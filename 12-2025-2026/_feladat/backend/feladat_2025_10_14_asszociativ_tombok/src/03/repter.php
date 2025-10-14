e5f01841aae51c57cd94ad5d02aaa4400bc24b66
VB2007 <admin@vb2007.hu>
2025-10-14 12:42:54 +0200

    updated 3rd task

--- 12-2025-2026/_feladat/backend/feladat_2025_10_14_asszociativ_tombok/src/03/repter.php
+++ 12-2025-2026/_feladat/backend/feladat_2025_10_14_asszociativ_tombok/src/03/repter.php
@@ -12,3 +12,16 @@ if ($argc == 1) {
         echo "{$jaratszam}\t{$adat['honnan']}-{$adat['hova']}\t{$adat['indul']}-{$adat['erkezik']}\n";
     }
 }
+
+if ($argc == 2) {
+    $talalat = false;
+    foreach ($jaratok as $jaratszam => $adat) {
+        if ($jaratszam == $argv[1]) {
+            $talalat = true;
+            echo "{$jaratszam} {$adat['indul']}-{$adat['erkezik']} ({$adat['legitarsasag']})";
+        }
+    }
+    if (!$talalat) {
+        echo "A keresett járat ({$argv[1]}) nem található!";
+    }
+}
