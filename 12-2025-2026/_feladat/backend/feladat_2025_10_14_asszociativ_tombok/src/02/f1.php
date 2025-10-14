0cd9b6cc668af1a14c7de5215ac74f629be25dd7
VB2007 <admin@vb2007.hu>
2025-10-14 10:33:54 +0200

    completed task 2

---
+++ 12-2025-2026/_feladat/backend/feladat_2025_10_14_asszociativ_tombok/src/02/f1.php
@@ -0,0 +1,23 @@
+<?php
+
+include("versenyzok.php");
+
+if ($argc > 2) {
+    echo "Túl sok paraméter!";
+    exit(8);
+}
+
+if ($argc == 1) {
+    echo "rajtsz.\tNév\tOrszág\tSzületési dátum\n";
+    foreach ($versenyzok as $rajtszam => $adat) {
+        echo "{$rajtszam}\t{$adat['nev']}\t{$adat['orszag']}\t{$adat['szulido']}\n";
+    }
+}
+
+if ($argc == 2) {
+    foreach ($versenyzok as $rajtszam => $adat) {
+        if ($adat['orszag'] === $argv[1]) {
+            echo "- $rajtszam : $adat[nev]\n";
+        }
+    }
+}
