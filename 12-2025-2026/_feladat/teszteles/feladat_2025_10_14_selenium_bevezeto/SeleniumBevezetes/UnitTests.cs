4101cccde763056e053e1872a0577e265a4323c4
VB2007 <admin@vb2007.hu>
2025-10-14 16:55:22 +0200

    created setup & teardown logic

--- 12-2025-2026/_feladat/teszteles/feladat_2025_10_14_selenium_bevezeto/SeleniumBevezetes/UnitTests.cs
+++ 12-2025-2026/_feladat/teszteles/feladat_2025_10_14_selenium_bevezeto/SeleniumBevezetes/UnitTests.cs
@@ -1,12 +1,26 @@
-﻿namespace SeleniumBevezetes;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace SeleniumBevezetes;
 
 public class Tests
 {
-    private readonly string baseUrl = "https://books.toscrape.com/";
-    
+    private WebDriver _webDriver;
+    private readonly string _baseUrl = "https://books.toscrape.com/";
+
     [SetUp]
     public void Setup()
     {
+        FirefoxOptions firefoxOptions = new FirefoxOptions();
+        _webDriver = new FirefoxDriver(firefoxOptions);
+        _webDriver.Navigate().GoToUrl(_baseUrl);
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        _webDriver?.Quit();
+        _webDriver?.Dispose();
     }
 
     [Test]
