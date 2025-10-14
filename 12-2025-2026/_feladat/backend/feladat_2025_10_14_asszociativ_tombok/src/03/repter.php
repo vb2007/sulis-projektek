12660cd897501a86ed7406aea1d40f06292d2d0d
VB2007 <admin@vb2007.hu>
2025-10-14 15:30:11 +0200

    finished task 3

--- 12-2025-2026/_feladat/backend/feladat_2025_10_14_asszociativ_tombok/src/03/repter.php
+++ 12-2025-2026/_feladat/backend/feladat_2025_10_14_asszociativ_tombok/src/03/repter.php
@@ -2,7 +2,7 @@
 
 require "jaratok.php";
 
-if ($argc > 2) {
+if ($argc > 3) {
     echo "Túl sok paraméter!\n";
     exit(4);
 }
@@ -21,7 +21,37 @@ if ($argc == 2) {
             echo "{$adat['honnan']}-{$adat['hova']} {$adat['indul']}-{$adat['erkezik']} ({$adat['legitarsasag']})\n";
         }
     }
+
     if (!$talalat) {
         echo "A keresett járat ({$argv[1]}) nem található!\n";
+        exit(7);
+    }
+}
+
+if ($argc == 3) {
+    switch ($argv[1]){
+        case "legitarsasag":
+            $jaratszam = 0;
+            foreach ($jaratok as $adat) {
+                if ($adat['legitarsasag'] == $argv[2]) {
+                    $jaratszam++;
+                }
+            }
+
+            echo "A(z) {$argv[2]} légitársaságnak {$jaratszam} járata van az adatok között.\n";
+            break;
+        case "repter":
+            $repterszam = 0;
+            foreach ($jaratok as $adat) {
+                if ($adat['honnan'] == $argv[2] || $adat['hova'] == $argv[2]) {
+                    $repterszam++;
+                }
+            }
+
+            echo "A(z) {$argv[2]} azonosítójú reptér {$repterszam}x szerepel az adatok között.\n";
+            break;
+        default:
+            echo "Ismeretlen paraméter '$argv[2]'\n";
+            exit(9);
     }
 }
