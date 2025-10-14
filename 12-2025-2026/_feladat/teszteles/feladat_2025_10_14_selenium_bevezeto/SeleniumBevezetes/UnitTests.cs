961763928f9c7afddb2901109b1a8a2faf7548ef
VB2007 <admin@vb2007.hu>
2025-10-14 17:19:41 +0200

    created CheckLabels test case

--- 12-2025-2026/_feladat/teszteles/feladat_2025_10_14_selenium_bevezeto/SeleniumBevezetes/UnitTests.cs
+++ 12-2025-2026/_feladat/teszteles/feladat_2025_10_14_selenium_bevezeto/SeleniumBevezetes/UnitTests.cs
@@ -3,6 +3,8 @@ using OpenQA.Selenium.Firefox;
 
 namespace SeleniumBevezetes;
 
+[TestFixture]
+//[Parallelizable(ParallelScope.All)] //egyszerre futna le mindegyik teszt, de ahhoz más setup & teardown logika kell
 public class Tests
 {
     private WebDriver _webDriver;
@@ -24,8 +26,46 @@ public class Tests
     }
 
     [Test]
+    [Description("Checks the page's title.")]
     public void CheckTitle()
     {
         Assert.That("All products | Books to Scrape - Sandbox", Is.EqualTo(_webDriver.Title));
+    }
+
+    [Test]
+    [Description("Checks if all book type labels are present in the sidebar list.")]
+    public void CheckLabels()
+    {
+        IEnumerable<string> expectedLabels = new List<string>
+        { 
+            "Travel", "Mystery", "Historical Fiction", "Sequential Art", 
+            "Classics", "Philosophy", "Romance", "Womens Fiction", 
+            "Fiction", "Childrens", "Religion", "Nonfiction", 
+            "Music", "Default", "Science Fiction", "Sports and Games", 
+            "Add a comment", "Fantasy", "New Adult", "Young Adult", 
+            "Science", "Poetry", "Paranormal", "Art", "Psychology", 
+            "Autobiography", "Parenting", "Adult Fiction", "Humor", 
+            "Horror", "History", "Food and Drink", "Christian Fiction", 
+            "Business", "Biography", "Thriller", "Contemporary", 
+            "Spirituality", "Academic", "Self Help", "Historical", 
+            "Christian", "Suspense", "Short Stories", "Novels", "Health", 
+            "Politics", "Cultural", "Erotica", "Crime"
+        };
+
+        IWebElement listContainer = _webDriver.FindElement(By.CssSelector("div.side_categories ul.nav.nav-list li ul"));
+        IEnumerable<IWebElement> labelContainers = listContainer.FindElements(By.CssSelector("li > a"));
+        IEnumerable<string> actualLabels = labelContainers.Select(x => x.Text.Trim()).ToList();
+        
+        foreach (string expectedLabel in expectedLabels)
+        {
+            Assert.That(actualLabels, Contains.Item(expectedLabel), $"Expected category '{expectedLabel}' was not found in the list");
+        }
+    }
+
+    [Test]
+    [Description("Checks the 'A Light in the Attic' book's price.")]
+    public void CheckPrice()
+    {
+        
     }
 }
