6e103fd357ce8519456a077d1b8bccb8c3a1b555
VB2007 <admin@vb2007.hu>
2025-10-21 14:52:14 +0200

    completed 1st & 2nd task

--- 12-2025-2026/_feladat/backend/feladat_2025_10_21_fuggvenyek/src/fuggvenyek.php
+++ 12-2025-2026/_feladat/backend/feladat_2025_10_21_fuggvenyek/src/fuggvenyek.php
@@ -20,3 +20,24 @@ function hetNapja(int $napSzama): string {
             return "Érvénytelen napszám.";
     }
 }
+
+function napSorszama(string $napNeve): int {
+    switch($napNeve) {
+        case "hétfő":
+            return 1;
+        case "kedd":
+            return 2;
+        case "szerda":
+            return 3;
+        case "csütörtök":
+            return 4;
+        case "péntek":
+            return 5;
+        case "szombat":
+            return 6;
+        case "vasárnap":
+            return 7;
+        default:
+            return 0;
+    }
+}
--- 12-2025-2026/_feladat/backend/feladat_2025_10_21_fuggvenyek/src/test.php
+++ 12-2025-2026/_feladat/backend/feladat_2025_10_21_fuggvenyek/src/test.php
@@ -1,1 +1,13 @@
 <?php
+
+require("fuggvenyek.php");
+
+//1. feladat
+echo hetNapja(1) . "\n";
+echo hetNapja(5) . "\n";
+echo hetNapja(11) . "\n";
+
+//2. feladat
+echo napSorszama(1) . "\n";
+echo napSorszama(5) . "\n";
+echo napSorszama(11) . "\n";
