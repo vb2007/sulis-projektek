451e64c41d495ce89e49c7388d8b5f67ecdbc0bd
VB2007 <admin@vb2007.hu>
2025-10-24 00:25:56 +0200

    fixed output formatting: completed task fogyasztas

--- 12-2025-2026/_feladat/backend/feladat_2025_10_21_fuggvenyek/src/fogyasztas.php
+++ 12-2025-2026/_feladat/backend/feladat_2025_10_21_fuggvenyek/src/fogyasztas.php
@@ -9,8 +9,8 @@ $jelenlegiTankolas = $argv[3];
 $km = $kmJelenleg - $kmLegutobbiTankolas;
 $atlagFogyasztas = fogyasztas($km, $jelenlegiTankolas);
 
-echo "Előző óraállás: {$kmLegutobbiTankolas} km\n";
-echo "Mostani óraállás: {$kmJelenleg} km\n";
-echo "Megtett út: {$km} km\n";
+echo "Előző óraállás: " . number_format($kmLegutobbiTankolas, 0, ".", " ") . " km\n";
+echo "Mostani óraállás: " . number_format($kmJelenleg, 0, ".", " ") . " km\n";
+echo "Megtett út: " . number_format($km, 0, ".", " ") . " km\n";
 echo "Tankolt üzemanyag: {$jelenlegiTankolas} liter\n";
 echo "Átlagfogyasztás: {$atlagFogyasztas} liter/100km\n";
