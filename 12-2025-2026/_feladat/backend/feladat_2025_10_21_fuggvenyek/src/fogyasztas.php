440edb7b22ab0f368dbd671d7b2cb524e80d1a8c
VB2007 <admin@vb2007.hu>
2025-10-24 00:24:46 +0200

    half completed task fogyasztas

--- 12-2025-2026/_feladat/backend/feladat_2025_10_21_fuggvenyek/src/fogyasztas.php
+++ 12-2025-2026/_feladat/backend/feladat_2025_10_21_fuggvenyek/src/fogyasztas.php
@@ -6,4 +6,11 @@ $kmLegutobbiTankolas = $argv[1];
 $kmJelenleg = $argv[2];
 $jelenlegiTankolas = $argv[3];
 
-$km = $kmLegutobbiTankolas - $kmJelenleg;
+$km = $kmJelenleg - $kmLegutobbiTankolas;
+$atlagFogyasztas = fogyasztas($km, $jelenlegiTankolas);
+
+echo "Előző óraállás: {$kmLegutobbiTankolas} km\n";
+echo "Mostani óraállás: {$kmJelenleg} km\n";
+echo "Megtett út: {$km} km\n";
+echo "Tankolt üzemanyag: {$jelenlegiTankolas} liter\n";
+echo "Átlagfogyasztás: {$atlagFogyasztas} liter/100km\n";
