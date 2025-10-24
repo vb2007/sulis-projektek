8df342a79d3d5c51730348bb2e5c6cd1be3258f8
VB2007 <admin@vb2007.hu>
2025-10-25 01:57:55 +0200

    completed task 4

--- 12-2025-2026/_feladat/webprogramozas/feladat_2025_10_21_mozi/mozi-vagvolgyi-balazs/src/js/main.js
+++ 12-2025-2026/_feladat/webprogramozas/feladat_2025_10_21_mozi/mozi-vagvolgyi-balazs/src/js/main.js
@@ -1,13 +1,46 @@
 "use strict";
 
-const createSpan = (text, cssClasses) => {
+const createSpan = (text, cssClasses = []) => {
   const span = document.createElement("span");
   span.textContent = text;
 
-  cssClasses.foreach((cssClass) => span.classList.add(cssClass));
+  cssClasses.forEach((cssClass) => span.classList.add(cssClass));
 
   return span;
 };
+
+const filmCountElement = document.getElementById("film-count");
+filmCountElement.innerHTML = "";
+const filmCountSpan = createSpan(`${films.length} film`, ["text-lg"]);
+filmCountElement.append(filmCountSpan);
+
+//feladatleírás itt is "film‑count"-ot írt, gondolom elírás
+const filmLengthElement = document.getElementById("film-length");
+filmLengthElement.innerHTML = "";
+const filmLengthSpan = createSpan(`${films.length * 120} órányi élmény`, [
+  "text-lg",
+  "astrict",
+]);
+filmLengthElement.append(filmLengthSpan);
+
+const sortedTickets = [...tickets].sort((a, b) => a[2] - b[2]);
+const [name, type, price] = sortedTickets[0];
+
+const cheapestTicketPriceElement = document.getElementById(
+  "cheapest-ticket-price",
+);
+cheapestTicketPriceElement.innerHTML = "";
+const cheapestPriceSpan = createSpan(`${price} forinttól`, [
+  "text-lg",
+  "double-astrict",
+]);
+cheapestTicketPriceElement.append(cheapestPriceSpan);
+
+const cheapestTicketDetailsElement = document.getElementById(
+  "cheapest-ticket-details",
+);
+const ticketDetailsSpan = createSpan(`${name} (${type})`, []);
+cheapestTicketDetailsElement.append(ticketDetailsSpan);
 
 const generateTicketCards = () => {
   const cards = [];
