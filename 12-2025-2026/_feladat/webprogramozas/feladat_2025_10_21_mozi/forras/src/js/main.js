986e7ad10dec1fa2bef02d1f6224097b8cfbccad
VB2007 <admin@vb2007.hu>
2025-10-24 19:18:56 +0200

    added source & task description

---
+++ 12-2025-2026/_feladat/webprogramozas/feladat_2025_10_21_mozi/forras/src/js/main.js
@@ -0,0 +1,20 @@
+"use strict"
+
+
+
+
+
+function generateTicketCards() {
+    const cards = [];
+
+}
+generateTicketCards();
+
+function generateFilmCards() {
+    const cards = [];
+    const mappedFilms = films;
+
+    const images = [];
+
+}
+generateFilmCards();
