fbc4d46a01e86258ca78f3ff7b6aea090f0bf8fb
VB2007 <admin@vb2007.hu>
2025-11-04 10:47:32 +0100

    created task

---
+++ 12-2025-2026/_feladat/backend/feladat_2025_11_04_szuperhosok/pindur.php
@@ -0,0 +1,13 @@
+<?php
+
+$pp = [
+    "sziporka" => "Blossom",
+    "puszedli" => "Bubbles",
+    "csuporka" => "Buttercup",
+    "pukkancs" => "Bliss",
+    "nyuszi" => "Bunny"
+];
+
+echo "Pindúr Pandúrok (The Powerpuff Girls)\n\n";
+
+$angolnevek = [];
