3b77fb9b45acf896fc1afaf95ec8c936947408ea
VB2007 <admin@vb2007.hu>
2025-11-05 22:32:53 +0100

    update

--- 12-2025-2026/_feladat/alkalmazasfejlesztes/feladat_2025_11_04_havi_kifizetesek/HaviKifizetesek_VB/HaviKifizetesek_VB_Lib/Payouts.cs
+++ 12-2025-2026/_feladat/alkalmazasfejlesztes/feladat_2025_11_04_havi_kifizetesek/HaviKifizetesek_VB/HaviKifizetesek_VB_Lib/Payouts.cs
@@ -2,5 +2,13 @@ namespace HaviKifizetesek_VB_Lib;
 
 public class Payouts
 {
-    
+    public readonly List<Payout> _payouts = new();
+
+    public Payouts(IEnumerable<string> dataLines)
+    {
+        foreach (string dataLine in dataLines)
+        {
+            _payouts.Add(new Payout(dataLine));
+        }
+    }
 }
