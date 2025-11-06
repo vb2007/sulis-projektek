819f61b9e0d67b568a30fc131a0c1bcf163eba2e
VB2007 <admin@vb2007.hu>
2025-11-06 09:44:35 +0100

    created matrix demo project

---
+++ 12-2025-2026/_feladat/alkalmazasfejlesztes/feladat_2025_11_06_matrix/MatrixBemutato/MatrixDemo/Program.cs
@@ -0,0 +1,103 @@
+﻿namespace MatrixDemo;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[,] matrix = new int[5, 5];
+
+        //Szimpla random számokkal feltöltés
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                matrix[i, j] = Random.Shared.Next(10, 50);
+            }
+        }
+
+        //Sorösszeg számolás
+        int[] sorokOsszege = new int[5];
+        for (int i = 0; i < 5; i++)
+        {
+            int osszeg = 0;
+            for (int j = 0; j < 5; j++)
+            {
+                osszeg += matrix[i, j];
+            }
+            sorokOsszege[i] = osszeg;
+        }
+
+        //Átlósan csak nullákkal feltöltés
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                if (i == j)
+                {
+                    matrix[i, j] = 0;
+                }
+                else
+                {
+                    matrix[i, j] = Random.Shared.Next(10, 20);
+                }
+            }
+        }
+        
+        //Felső háromszög
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                if (i < j)
+                {
+                    matrix[i, j] = 0;
+                }
+                else
+                {
+                    matrix[i, j] = Random.Shared.Next(10, 20);
+                }
+            }
+        }
+        
+        //Alsó háromszög
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                if (i > j)
+                {
+                    matrix[i, j] = 0;
+                }
+                else
+                {
+                    matrix[i, j] = Random.Shared.Next(10, 20);
+                }
+            }
+        }
+        
+        //Alsó háromszög összege
+        int osszeg = 0;
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                if (i >= j)
+                {
+                    osszeg += matrix[i, j];
+                }
+            }
+        }
+        //Generikus matrixkiírás
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                Console.Write($"{matrix[i, j]} | ");
+            }
+
+            Console.WriteLine($"Összeg: {sorokOsszege[i]}");
+            Console.WriteLine();
+        }
+        Console.WriteLine($"Alsó háromszög összege: {osszeg}");
+    }
+}
