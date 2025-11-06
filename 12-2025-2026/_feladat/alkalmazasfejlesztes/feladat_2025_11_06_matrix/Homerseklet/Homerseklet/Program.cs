e431b976f7e9acf5aba5d350c0e3a2492e9f3f41
VB2007 <admin@vb2007.hu>
2025-11-06 10:25:57 +0100

    completed first task

--- 12-2025-2026/_feladat/alkalmazasfejlesztes/feladat_2025_11_06_matrix/Homerseklet/Homerseklet/Program.cs
+++ 12-2025-2026/_feladat/alkalmazasfejlesztes/feladat_2025_11_06_matrix/Homerseklet/Homerseklet/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        int[,] matrix = new int[5, 5];
+        IEnumerable<string> lines = File.ReadAllLines("homerseklet.txt").Skip(1);
+        int[,] matrix = new int[lines.Count(), 5]; //oszlopszám hardcoded mert lusta vagyok kiszámolni
+
+        int lineCount = 0;
+        foreach (var line in lines)
+        {
+            string[] columns = line.Split(' ');
+            
+            int columnCount = 0;
+            foreach (var column in columns)
+            {
+                matrix[lineCount, columnCount] = int.Parse(column);
+                columnCount++;
+            }
+            lineCount++;
+        }
+
+        //https://stackoverflow.com/questions/9404683/how-to-get-the-length-of-row-column-of-multidimensional-array-in-c
+        Console.WriteLine("1. feladat:");
+        
+        //Táblázat headere
+        Console.Write("\t");
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            Console.Write($"\t{i + 1}. mérés");
+        }
+        Console.WriteLine();
+        
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            Console.Write($"{i + 1}. nap:");
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                Console.Write($"\t{matrix[i, j]}");
+            }
+            Console.WriteLine();
+        }
     }
 }
