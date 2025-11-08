fc77f19e8af480be82c4253a58fc3b4bd8d3d629
VB2007 <admin@vb2007.hu>
2025-11-08 22:43:58 +0100

    completed task 1-4

--- 12-2025-2026/_feladat/backend/feladat_2025_11_04_string/szuperhosok/php/szuperhos.php
+++ 12-2025-2026/_feladat/backend/feladat_2025_11_04_string/szuperhosok/php/szuperhos.php
@@ -1,1 +1,12 @@
 <?php
+
+$hero = $argv[1];
+
+echo "2. feladat:\n";
+echo "Hős neve:\t" . strtoupper($hero) . "\n"; //mb_strtoupper
+
+echo "3. feladat:\n";
+echo "Hős karaktereinek száma: " . strlen($hero) . "\n"; //mb_strlen
+
+echo "4. feladat:\n";
+echo "A hősnek " . ($argc > 2 ? "van" : "nincs") . " társa\n";
