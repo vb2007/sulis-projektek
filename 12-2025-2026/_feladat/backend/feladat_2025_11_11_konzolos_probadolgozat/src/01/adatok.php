3c2d35eecccbe9eb6792f8bedd2647819a6cf006
VB2007 <admin@vb2007.hu>
2025-11-11 10:05:14 +0100

    completed 1st task (2.)

--- 12-2025-2026/_feladat/backend/feladat_2025_11_11_konzolos_probadolgozat/src/01/adatok.php
+++ 12-2025-2026/_feladat/backend/feladat_2025_11_11_konzolos_probadolgozat/src/01/adatok.php
@@ -8,3 +8,38 @@ $parok = [
     "Pécsi Anna",
     "Budai András"
 ];
+
+$versenyzok = [
+    "Mercedes" => [
+        [
+            "rajtszam" => 77,
+            "nev" => "Valtteri Bottas",
+            "orszag" => "Australia",
+            "dobogo" => 37,
+            "vb" => 0
+        ],
+        [
+            "rajtszam" => 44,
+            "nev" => "Lewis Hamilton",
+            "orszag" => "United Kingdom",
+            "dobogo" => 142,
+            "vb" => 5
+        ]
+    ],
+    "Ferrari" => [
+        [
+            "rajtszam" => 5,
+            "nev" => "Sebastian Vettel",
+            "orszag" => "Germany",
+            "dobogo" => 115,
+            "vb" => 4
+        ],
+        [
+            "rajtszam" => 16,
+            "nev" => "Charles Leclerc",
+            "orszag" => "Finland",
+            "dobogo" => 3,
+            "vb" => 0
+        ]
+    ]
+];
