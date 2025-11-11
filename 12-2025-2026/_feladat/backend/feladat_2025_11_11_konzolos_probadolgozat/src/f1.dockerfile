e87c5e9028fe1e353b20d20a4d1b3757948ae5bc
VB2007 <admin@vb2007.hu>
2025-11-11 10:15:08 +0100

    started task f1

--- 12-2025-2026/_feladat/backend/feladat_2025_11_11_konzolos_probadolgozat/src/f1.dockerfile
+++ 12-2025-2026/_feladat/backend/feladat_2025_11_11_konzolos_probadolgozat/src/f1.dockerfile
@@ -5,4 +5,4 @@ COPY . /app
 ENTRYPOINT [ "php", "f1.php" ]
 
 # docker build -t vb/f1 . -f f1.dockerfile
-# docker run -it --rm -v $(pwd):/app vb/f1
+# docker run -it --rm -v $(pwd):/app vb/f1 44
--- 12-2025-2026/_feladat/backend/feladat_2025_11_11_konzolos_probadolgozat/src/f1.php
+++ 12-2025-2026/_feladat/backend/feladat_2025_11_11_konzolos_probadolgozat/src/f1.php
@@ -1,3 +1,16 @@
 <?php
 
 require "adatok.php";
+
+function vb() {
+    $vbCount = 0;
+    foreach ($versenyzok as $team => $drivers) {
+        foreach ($drivers as $driver) {
+            $total_vb += $driver["vb"];
+        }
+    }
+}
+
+function dobogo($rajtszam) {
+
+}
