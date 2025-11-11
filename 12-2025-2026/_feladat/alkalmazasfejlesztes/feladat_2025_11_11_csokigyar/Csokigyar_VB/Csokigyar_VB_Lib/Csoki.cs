b2d172137f71c70df4249ac2d96d29bd582cf3ad
VB2007 <admin@vb2007.hu>
2025-11-11 13:54:36 +0100

    updated csoki class

--- 12-2025-2026/_feladat/alkalmazasfejlesztes/feladat_2025_11_11_csokigyar/Csokigyar_VB/Csokigyar_VB_Lib/Csoki.cs
+++ 12-2025-2026/_feladat/alkalmazasfejlesztes/feladat_2025_11_11_csokigyar/Csokigyar_VB/Csokigyar_VB_Lib/Csoki.cs
@@ -1,6 +1,33 @@
 namespace Csokigyar_VB_Lib;
 
-public class Csoki
+internal class Csoki : IEtel
 {
+    private string Csokifajta { get; init; }
+    private string[] Alapanyagok { get; init; }
+    private int KakaoTartalom { get; init; }
     
+    public IEnumerable<string> MibolKeszul()
+    {
+        return Alapanyagok;
+    }
+
+    public bool MegfeleloMinoseg
+        => KakaoTartalom switch
+        {
+            > 50 => true,
+            >= 0 => false,
+            _ => throw new SilanyMinosegException()
+        };
+    
+    public Csoki(string csokifajta, IEnumerable<string> alapanyagok, int kakaoTartalom)
+    {
+        Csokifajta = csokifajta;
+        Alapanyagok = alapanyagok.ToArray();
+        KakaoTartalom = kakaoTartalom;
+    }
+
+    public override string ToString()
+    {
+        return $"{Csokifajta} kakótartalom: {KakaoTartalom}%. Alapanyagai: {string.Join(", ", Alapanyagok)}";
+    }
 }
