6709817d245302d27bf3f972709355457499573f
VB2007 <admin@vb2007.hu>
2025-11-11 16:51:42 +0100

    updated formatting, completed task 1

--- 12-2025-2026/_feladat/webprogramozas/feladat_2025_11_11_test_subjects/test-subjects-vagvolgyi-balazs/src/js/main.js
+++ 12-2025-2026/_feladat/webprogramozas/feladat_2025_11_11_test_subjects/test-subjects-vagvolgyi-balazs/src/js/main.js
@@ -1,56 +1,57 @@
-"use strict"
+"use strict";
 
-function capitalize(text) {
-    return text.toUpperCase().slice(0, 1) + text.substring(1);
-}
+const capitalize = (text) => {
+  return text.toUpperCase().slice(0, 1) + text.substring(1);
+};
 
-function createTableRow(subject) {
-    const row = document.createElement('tr');
-    
-    
-    const id = document.createElement('td');
-    
+const createTableRow = (subject) => {
+  const row = document.createElement("tr");
 
-    const name = document.createElement('td');
-    
+  const id = document.createElement("td");
 
-    const statusWraper = document.createElement('td')
-    const status = document.createElement('span');
-    
-    
-    statusWraper.append(status);
+  const name = document.createElement("td");
 
-    row.append(id, name, statusWraper);
-    return row;
-}
+  const statusWraper = document.createElement("td");
+  const status = document.createElement("span");
 
-function fillTable(array) {
-    const rows = [];
-    for (const subject of array) {
-        rows.push(createTableRow(subject));
-    }
-    table.replaceChildren(...rows);
-    addEventListenersToRows()
-}
+  statusWraper.append(status);
 
-const table = document.querySelector('#list-of-subjects tbody');
+  row.append(id, name, statusWraper);
+  return row;
+};
+
+const fillTable = (array) => {
+  const rows = [];
+  for (const subject of array) {
+    rows.push(createTableRow(subject));
+  }
+  table.replaceChildren(...rows);
+  addEventListenersToRows();
+};
+
+const table = document.querySelector("#list-of-subjects tbody");
 fillTable(subjects);
 
-function showSubject(subject) {
-    
-}
+const showSubject = (subject) => {};
 
-function addEventListenersToRows() {
-    table.querySelectorAll('tr').forEach(x => x.addEventListener('click', e => 
-        showSubject(subjects.find(y => true))
-    ));
-}
+const filterSubjects = (name) => {};
 
-function filterSubjects(name) {
-    
-}
+const userProfile = {
+  name: "Balázs Vágvölgyi",
+  position: "unemployed",
+};
 
-document.querySelector('#search').addEventListener('submit', e => {
-    e.preventDefault();
-    fillTable(filterSubjects(document.querySelector('#name').value));
+const addEventListenersToRows = () => {
+  table
+    .querySelectorAll("tr")
+    .forEach((x) =>
+      x.addEventListener("click", (e) =>
+        showSubject(subjects.find((y) => true)),
+      ),
+    );
+};
+
+document.querySelector("#search").addEventListener("submit", (e) => {
+  e.preventDefault();
+  fillTable(filterSubjects(document.querySelector("#name").value));
 });
