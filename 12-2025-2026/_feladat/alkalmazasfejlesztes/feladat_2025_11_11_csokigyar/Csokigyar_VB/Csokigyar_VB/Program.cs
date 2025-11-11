f531accf00af967b47d89ac088c6c5d940fa65d2
VB2007 <admin@vb2007.hu>
2025-11-11 14:13:27 +0100

    created reference & Feltoltes() func in program.cs

--- 12-2025-2026/_feladat/alkalmazasfejlesztes/feladat_2025_11_11_csokigyar/Csokigyar_VB/Csokigyar_VB/Program.cs
+++ 12-2025-2026/_feladat/alkalmazasfejlesztes/feladat_2025_11_11_csokigyar/Csokigyar_VB/Csokigyar_VB/Program.cs
@@ -1,9 +1,23 @@
-﻿namespace Csokigyar_VB;
+﻿using Csokigyar_VB_Lib;
+
+namespace Csokigyar_VB;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        IEnumerable<IEtel> Feltoltes(string fajlNev)
+        {
+            if (!File.Exists(fajlNev))
+            {
+                throw new FileNotFoundException($"A(z) \"{fajlNev}\" állomány nem található!");
+            }
+
+            EtelFactory etelFactory = new EtelFactory();
+            foreach (var item in File.ReadAllLines(fajlNev).Skip(1))
+            {
+                yield return etelFactory.Factory(item);
+            }
+        }
     }
 }
