bbcd9ec2becbc77f3c5fcbf488d7b6bacf2187a6
VB2007 <admin@vb2007.hu>
2025-11-11 10:24:35 +0100

    completed task 3 - 5 (2.)

--- 12-2025-2026/_feladat/backend/feladat_2025_11_11_konzolos_probadolgozat/src/f1.php
+++ 12-2025-2026/_feladat/backend/feladat_2025_11_11_konzolos_probadolgozat/src/f1.php
@@ -3,14 +3,34 @@
 require "adatok.php";
 
 function vb() {
+    global $versenyzok;
+
     $vbCount = 0;
-    foreach ($versenyzok as $team => $drivers) {
-        foreach ($drivers as $driver) {
-            $total_vb += $driver["vb"];
+    foreach ($versenyzok as $csapat => $resztvevok) {
+        foreach ($resztvevok as $resztvevo) {
+            $vbCount += $resztvevo["vb"];
+        }
+    }
+
+    return $vbCount;
+}
+
+function dobogo($rajtszam) {
+    global $versenyzok;
+
+    foreach ($versenyzok as $csapat => $resztvevok) {
+        foreach ($resztvevok as $resztvevo) {
+            if ($resztvevo["rajtszam"] == $rajtszam) {
+                return $resztvevo["dobogo"];
+            }
         }
     }
 }
 
-function dobogo($rajtszam) {
+if ($argv[1] == "vb") {
+    echo vb() . "\n";
+}
 
+if (is_int($argv[1]) || $argv[1] >= 1 || $argv[1] <= 100) {
+    echo dobogo($argv[1]) . "\n";
 }
