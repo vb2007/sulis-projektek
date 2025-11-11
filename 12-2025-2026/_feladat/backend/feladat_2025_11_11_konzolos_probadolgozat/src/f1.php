e7a85d0fb195cc02160ef75e19a532bb6e83d698
VB2007 <admin@vb2007.hu>
2025-11-11 10:28:18 +0100

    finished 2nd part

--- 12-2025-2026/_feladat/backend/feladat_2025_11_11_konzolos_probadolgozat/src/f1.php
+++ 12-2025-2026/_feladat/backend/feladat_2025_11_11_konzolos_probadolgozat/src/f1.php
@@ -27,10 +27,22 @@ function dobogo($rajtszam) {
     }
 }
 
+if ($argc < 2) {
+    foreach ($versenyzok as $csapat => $resztvevok) {
+        foreach ($resztvevok as $resztvevo) {
+            echo mb_strtoupper($resztvevo["nev"]) . " (" . $resztvevo["rajtszam"] . ") [" . $csapat . "]\n";
+        }
+    }
+
+    exit();
+}
+
 if ($argv[1] == "vb") {
     echo vb() . "\n";
+    exit();
 }
 
 if (is_int($argv[1]) || $argv[1] >= 1 || $argv[1] <= 100) {
     echo dobogo($argv[1]) . "\n";
+    exit();
 }
