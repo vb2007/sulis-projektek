2f7de4cece1ea52c068d36517552c8838b4917b0
VB2007 <admin@vb2007.hu>
2025-11-19 17:37:50 +0100

    updated code formatting

--- 12-2025-2026/_feladat/webprogramozas/feladat_2025_11_18_zenek/forras/src/js/main.js
+++ 12-2025-2026/_feladat/webprogramozas/feladat_2025_11_18_zenek/forras/src/js/main.js
@@ -1,42 +1,39 @@
-"use strict"
+"use strict";
 
 const ITEMS_PER_PAGE = 20;
 
 let tracks = [...data];
 
-const tracksContainer = document.querySelector('#tracks');
+const tracksContainer = document.querySelector("#tracks");
 
-document.querySelector('#next-page').addEventListener('click', e => {
-    if (tracksContainer.dataset.page > data.length / ITEMS_PER_PAGE) return;
-    tracksContainer.dataset.page = +tracksContainer.dataset.page + 1;
-    generateCards(tracksContainer.dataset.page);
+document.querySelector("#next-page").addEventListener("click", (e) => {
+  if (tracksContainer.dataset.page > data.length / ITEMS_PER_PAGE) return;
+  tracksContainer.dataset.page = +tracksContainer.dataset.page + 1;
+  generateCards(tracksContainer.dataset.page);
 });
 
-document.querySelector('#prev-page').addEventListener('click', e => {
-    if (tracksContainer.dataset.page <= 1) return;
-    tracksContainer.dataset.page = +tracksContainer.dataset.page - 1;
-    generateCards(tracksContainer.dataset.page);
+document.querySelector("#prev-page").addEventListener("click", (e) => {
+  if (tracksContainer.dataset.page <= 1) return;
+  tracksContainer.dataset.page = +tracksContainer.dataset.page - 1;
+  generateCards(tracksContainer.dataset.page);
 });
 
-const paginator = document.querySelector('#page-numbers');
-function generatePaginator() {
-
-}
+const paginator = document.querySelector("#page-numbers");
+function generatePaginator() {}
 generatePaginator();
 
-function generateCards(page) {
-    
-}
+function generateCards(page) {}
 generateCards(1);
 
 function filterTracks(value) {
-    
-    generateCards(tracksContainer.dataset.page);
-    generatePaginator();
+  generateCards(tracksContainer.dataset.page);
+  generatePaginator();
 }
 
-const resultsText = document.querySelector('#number-of-results');
-document.querySelector('#search').addEventListener('input', e => {
-    filterTracks(e.currentTarget.value)
-    resultsText.textContent = e.currentTarget.value ? `${tracks.length} találat a(z) "${e.currentTarget.value}" kifejezésre:` : "";
-})
+const resultsText = document.querySelector("#number-of-results");
+document.querySelector("#search").addEventListener("input", (e) => {
+  filterTracks(e.currentTarget.value);
+  resultsText.textContent = e.currentTarget.value
+    ? `${tracks.length} találat a(z) "${e.currentTarget.value}" kifejezésre:`
+    : "";
+});
