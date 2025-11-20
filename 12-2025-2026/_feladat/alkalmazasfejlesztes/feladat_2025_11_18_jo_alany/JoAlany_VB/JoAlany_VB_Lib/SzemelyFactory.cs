228c839db1ba92384a4695e4a91e8c34a654f0e2
VB2007 <admin@vb2007.hu>
2025-11-20 09:53:21 +0100

    finished szemelyfactory

--- 12-2025-2026/_feladat/alkalmazasfejlesztes/feladat_2025_11_18_jo_alany/JoAlany_VB/JoAlany_VB_Lib/SzemelyFactory.cs
+++ 12-2025-2026/_feladat/alkalmazasfejlesztes/feladat_2025_11_18_jo_alany/JoAlany_VB/JoAlany_VB_Lib/SzemelyFactory.cs
@@ -2,8 +2,20 @@ namespace JoAlany_VB_Lib;
 
 public class SzemelyFactory
 {
-    public static Szemely CreateSzemely(string adat)
+    public static Szemely CreateSzemely(string adatSor)
     {
-            
+        string[] split = adatSor.Split(';');
+
+        string szemelyNev = split[0];
+        string szemelyFajta = split[1];
+        DateTime szuletesiIdo = Convert.ToDateTime(split[2]);
+        if (szemelyFajta == "t")
+        {
+            return new Tanar(szemelyNev, szuletesiIdo, Convert.ToDouble(split[3]));
+        }
+        else
+        {
+            return new Tanar(szemelyNev, szuletesiIdo, Convert.ToInt32(split[3]));
+        }
     }
 }
