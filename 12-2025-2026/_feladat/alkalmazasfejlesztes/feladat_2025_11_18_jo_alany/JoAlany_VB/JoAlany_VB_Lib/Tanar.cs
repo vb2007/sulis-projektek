40209588beb9814960b293fc1d60ed47632b80e4
VB2007 <admin@vb2007.hu>
2025-11-20 09:46:53 +0100

    created class tanar

---
+++ 12-2025-2026/_feladat/alkalmazasfejlesztes/feladat_2025_11_18_jo_alany/JoAlany_VB/JoAlany_VB_Lib/Tanar.cs
@@ -0,0 +1,21 @@
+namespace JoAlany_VB_Lib;
+
+public class Tanar : Szemely, IVizsgalat
+{
+    public double EvVegiAtlag { get; init; }
+
+    public Tanar(string szemelyNev, DateTime szuletesiDatum, double evVegiAtlag) : base(szemelyNev, szuletesiDatum)
+    {
+        this.EvVegiAtlag = evVegiAtlag;
+    }
+
+    public bool JoAlanyE()
+    {
+        return EvVegiAtlag >= 3.5 && Kor < 30 ? true : false;
+    }
+    
+    public override string ToString()
+    {
+        return $"{SzemelyNev} ({kor} éves), {(JoAlanyE() == true ? "Jó tanár!" : "Rossz tanár!")}";
+    }
+}
