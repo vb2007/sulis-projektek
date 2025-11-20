27f49ec259cb1d01195589e27033aca1b9aa3946
VB2007 <admin@vb2007.hu>
2025-11-20 10:07:31 +0100

    created input reading logic in program.cs

--- 12-2025-2026/_feladat/alkalmazasfejlesztes/feladat_2025_11_18_jo_alany/JoAlany_VB/JoAlany_VB/Program.cs
+++ 12-2025-2026/_feladat/alkalmazasfejlesztes/feladat_2025_11_18_jo_alany/JoAlany_VB/JoAlany_VB/Program.cs
@@ -6,6 +6,37 @@ class Program
 {
     static void Main(string[] args)
     {
+        static IEnumerable<Szemely> SzemelyAdatok()
+        {
+            if (!File.Exists("input.txt"))
+            {
+                throw new FileNotFoundException("A fájl nem található!");
+            }
+            else
+            {
+                foreach (var item in File.ReadAllLines("input.txt"))
+                {
+                    Szemely? szemely = null;
+                    
+                    try
+                    {
+                        szemely = SzemelyFactory.CreateSzemely(item);
+                    }
+                    catch (HibasEletkorException ex)
+                    {
+                        Console.WriteLine($"Hiba a személy feldolgozása közben: {ex.Message}");
+                    }
+
+                    if (szemely != null)
+                    {
+                        //yield = addig returnöl, amíg van adat aka. "többszörös return"
+                        yield return szemely;
+                    }
+                }
+            }
+        }
+
+        Nyilvantartas szemelyek = new(SzemelyAdatok());
         
     }
 }
