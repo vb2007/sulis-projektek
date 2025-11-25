0332543425c6d866b70f88ecb19099b7058f957d
VB2007 <admin@vb2007.hu>
2025-11-25 11:33:21 +0100

    implemented example calls in console.php

--- 12-2025-2026/_feladat/backend/feladat_2025_11_25_oop/demo/console.php
+++ 12-2025-2026/_feladat/backend/feladat_2025_11_25_oop/demo/console.php
@@ -1,1 +1,31 @@
 <?php
+
+require "src/Acme/Math.php";
+require "src/Acme/Math/Geometry/Circle.php";
+require "src/Acme/Math/Shapes/Circle.php";
+
+use Acme\Math;
+use Acme\Math\Geometry\Circle;
+use Acme\Graphics\Shapes\Circle as ShapesCircle;
+
+$math1 = new Math(6, 7);
+echo $math1->sum() . PHP_EOL;
+$math2 = new Math(4, 1);
+echo $math2->sub() . PHP_EOL;
+
+echo "----------------";
+
+$circle1 = new Circle(4);
+echo $circle1->circumference() . PHP_EOL;
+echo $circle1->area() . PHP_EOL;
+
+echo "----------------";
+
+$circle2 = new ShapesCircle(3, 5, 10);
+$position = $circle2->position();
+echo "($position[0] ; $position[1])" . PHP_EOL;
+echo "1. size: " . $circle2->getSize() . PHP_EOL;
+$circle2->setSize(20);
+echo "2. size: " . $circle2->getSize() . PHP_EOL;
+$circle2->setSize(-5);
+echo "3. size: " . $circle2->getSize() . PHP_EOL;
