510f7ce24cc1f53a47fdde753c749dcf071479ee
VB2007 <admin@vb2007.hu>
2025-11-25 11:36:13 +0100

    fixed require import & echo formatting in console.php

--- 12-2025-2026/_feladat/backend/feladat_2025_11_25_oop/demo/console.php
+++ 12-2025-2026/_feladat/backend/feladat_2025_11_25_oop/demo/console.php
@@ -2,7 +2,7 @@
 
 require "src/Acme/Math.php";
 require "src/Acme/Math/Geometry/Circle.php";
-require "src/Acme/Math/Shapes/Circle.php";
+require "src/Acme/Graphics/Shapes/Circle.php";
 
 use Acme\Math;
 use Acme\Math\Geometry\Circle;
@@ -13,13 +13,13 @@ echo $math1->sum() . PHP_EOL;
 $math2 = new Math(4, 1);
 echo $math2->sub() . PHP_EOL;
 
-echo "----------------";
+echo "----------------" . PHP_EOL;
 
 $circle1 = new Circle(4);
 echo $circle1->circumference() . PHP_EOL;
 echo $circle1->area() . PHP_EOL;
 
-echo "----------------";
+echo "----------------" . PHP_EOL;
 
 $circle2 = new ShapesCircle(3, 5, 10);
 $position = $circle2->position();
