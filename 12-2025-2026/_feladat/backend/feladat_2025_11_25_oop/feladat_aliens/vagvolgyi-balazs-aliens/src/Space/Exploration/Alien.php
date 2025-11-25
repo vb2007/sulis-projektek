4d1850f2a89f60ba80a746f4ab1609fcbb83769c
VB2007 <admin@vb2007.hu>
2025-11-25 16:20:11 +0100

    written actual login in getters & setters

--- 12-2025-2026/_feladat/backend/feladat_2025_11_25_oop/feladat_aliens/vagvolgyi-balazs-aliens/src/Space/Exploration/Alien.php
+++ 12-2025-2026/_feladat/backend/feladat_2025_11_25_oop/feladat_aliens/vagvolgyi-balazs-aliens/src/Space/Exploration/Alien.php
@@ -14,26 +14,26 @@ class Alien {
     }
 
     public function getSpecies(): string {
-
+        return $this->species;
     }
 
     public function setSpecies(string $species): void {
-
+        $this->species = $species;
     }
 
     public function getPlanet(): string {
-
+        return $this->planet;
     }
 
     public function setPlanet(string $planet): void {
-
+        $this->planet = $planet;
     }
 
     public function isFriendly(): bool {
-
+        return $this->isFriendly;
     }
 
     public function setFriendly(bool $isFriendly): void {
-
+        $this->isFriendly = $isFriendly;
     }
 }
