f937e30e3c2b6fa4da7d2b84ff91dc368f390417
VB2007 <admin@vb2007.hu>
2025-11-25 16:24:18 +0100

    declared strict types, imported class, created data

--- 12-2025-2026/_feladat/backend/feladat_2025_11_25_oop/feladat_aliens/vagvolgyi-balazs-aliens/aliens.php
+++ 12-2025-2026/_feladat/backend/feladat_2025_11_25_oop/feladat_aliens/vagvolgyi-balazs-aliens/aliens.php
@@ -1,1 +1,12 @@
 <?php
+
+declare(strict_types=1);
+
+require "src/Space/Exploration/Alien.php";
+
+use Space\Exploration\Alien;
+
+$alien1 = new Alien("Zog", "Zogonia", true);
+$alien2 = new Alien("Blip", "Blipton", false);
+$alien3 = new Alien("Xar", "Xarax", true);
+$alien4 = new Alien("Gloop", "Gloopia", false);
