5243e18f8c5de608c14de86b59ae7f94f0b3f15f
VB2007 <admin@vb2007.hu>
2025-11-25 17:07:24 +0100

    completed data management in idezetek.php

--- 12-2025-2026/_feladat/backend/feladat_2025_11_25_oop/feladat_idezetek/vagvolgyi-balazs-idezetek/idezetek.php
+++ 12-2025-2026/_feladat/backend/feladat_2025_11_25_oop/feladat_idezetek/vagvolgyi-balazs-idezetek/idezetek.php
@@ -1,1 +1,17 @@
 <?php
+
+declare(strict_types=1);
+
+require "src/Culture/Movie/Quotation.php";
+
+use Culture\Movie\Quotation;
+
+$quotations = [
+    new Quotation("Az Erő legyen veled.", "Obi-Wan Kenobi", "Csillagok háborúja"),
+    new Quotation("Az új magyar narancs. Kicsit sárgább, kicsit savanyúbb, de a mienk!", "Pelikán", "A tanú"),
+    new Quotation("Tigris van a fürdőszobában!", "Stu Price", "Másnaposok")
+];
+
+foreach($quotations as $quotation) {
+    echo $quotation->getText() . PHP_EOL . $quotation->getPerson() . " - " . $quotation->getTitle();
+}
