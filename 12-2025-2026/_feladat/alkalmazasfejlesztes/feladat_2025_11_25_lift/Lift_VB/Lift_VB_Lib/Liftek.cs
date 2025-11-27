45f18da4fedc3fcb9b6d0857cb5e9436980d37b9
VB2007 <admin@vb2007.hu>
2025-11-27 15:21:03 +0100

    implemented class liftek correctly

--- 12-2025-2026/_feladat/alkalmazasfejlesztes/feladat_2025_11_25_lift/Lift_VB/Lift_VB_Lib/Liftek.cs
+++ 12-2025-2026/_feladat/alkalmazasfejlesztes/feladat_2025_11_25_lift/Lift_VB/Lift_VB_Lib/Liftek.cs
@@ -4,13 +4,18 @@ public class Liftek
 {
     private readonly List<Lift> _liftek;
 
-    public Liftek(IEnumerable<string> sorok)
+    public Liftek(IEnumerable<Lift> liftek)
     {
-        
+        _liftek = new List<Lift>(liftek);
     }
 
     public Lift this[int sorszam]
     {
-        get { return _liftek[sorszam]; }
+        get
+        {
+            if (sorszam < 1 || sorszam > _liftek.Count)
+                throw new IndexOutOfRangeException($"Nincs {sorszam}. számú lift!");
+            return _liftek[sorszam - 1];
+        }
     }
 }
