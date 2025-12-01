980f52028f3faa696de3dc287828c8bf63db60f8
VB2007 <admin@vb2007.hu>
2025-12-01 12:34:33 +0100

    copied & modifed NavigateToFantasy test case

--- 12-2025-2026/_feladat/teszteles/feladat_2025_11_24_selenium_bevezeto_2/SeleniumBevezetes/Feladat2.cs
+++ 12-2025-2026/_feladat/teszteles/feladat_2025_11_24_selenium_bevezeto_2/SeleniumBevezetes/Feladat2.cs
@@ -1,3 +1,5 @@
+using OpenQA.Selenium;
+
 namespace SeleniumBevezetes;
 
 [TestFixture]
@@ -19,6 +21,9 @@ public class Feladat2 : BrowserSiteHelper
     [Description("Navigates to the 'Fantasy' category.")]
     public void NavigateToFantasy()
     {
-        
+        IWebElement fantasyButton = _webDriver.FindElement(By.XPath("/html/body/div/div/div/aside/div[2]/ul/li/ul/li[18]/a"));
+        fantasyButton.Click();
+        string foundText = _webDriver.FindElement(By.XPath("/html/body/div/div/div/div/div[1]/h1")).Text;
+        Assert.That(foundText, Is.EqualTo("Fantasy"));
     }
 }
