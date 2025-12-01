a16e8fa0c43bf0ba4deb0d71b42581287fdc5bc9
VB2007 <admin@vb2007.hu>
2025-12-01 12:42:45 +0100

    imported & updated SumBookPrices test case

--- 12-2025-2026/_feladat/teszteles/feladat_2025_11_24_selenium_bevezeto_2/SeleniumBevezetes/Feladat2.cs
+++ 12-2025-2026/_feladat/teszteles/feladat_2025_11_24_selenium_bevezeto_2/SeleniumBevezetes/Feladat2.cs
@@ -26,4 +26,22 @@ public class Feladat2 : BrowserSiteHelper
         string foundText = _webDriver.FindElement(By.XPath("/html/body/div/div/div/div/div[1]/h1")).Text;
         Assert.That(foundText, Is.EqualTo("Fantasy"));
     }
+    
+    [Test]
+    public void SumBookPrices()
+    {
+        IWebElement fantasyButton = _webDriver.FindElement(By.XPath("/html/body/div/div/div/aside/div[2]/ul/li/ul/li[18]/a"));
+        fantasyButton.Click();
+        IEnumerable<IWebElement> booksList = _webDriver.FindElements(By.CssSelector(".price_color"));
+        
+        double sum = 0;
+        foreach (IWebElement book in booksList)
+        {
+            string bookpricestring = book.Text;
+            double bookprice = Convert.ToDouble(bookpricestring.Replace("£", ""));
+            sum += bookprice;
+        }
+        
+        Assert.That(sum, Is.EqualTo(793.0799999999999));
+    }
 }
