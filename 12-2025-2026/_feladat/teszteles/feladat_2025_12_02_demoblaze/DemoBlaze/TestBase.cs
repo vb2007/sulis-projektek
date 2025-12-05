74be66bd77d818670cdb57aae5c7898ec57f0a50
VB2007 <admin@vb2007.hu>
2025-12-05 23:14:55 +0100

    added new test data, created new test class

--- 12-2025-2026/_feladat/teszteles/feladat_2025_12_02_demoblaze/DemoBlaze/TestBase.cs
+++ 12-2025-2026/_feladat/teszteles/feladat_2025_12_02_demoblaze/DemoBlaze/TestBase.cs
@@ -1,9 +1,15 @@
 namespace DemoBlaze;
 
-//all static side data
 public class TestBase
 {
     public static string _baseUrl = "https://www.demoblaze.com/";
     
     public static string Title = "STORE";
+
+    public class UserData
+    {
+        public static string Username = "vb2007";
+        public static string Password = "abc123";
+        public static string InvalidPassword = "abc1234";
+    }
 }
---
+++ 12-2025-2026/_feladat/teszteles/feladat_2025_12_02_demoblaze/DemoBlaze/Tests/UserTest.cs
@@ -0,0 +1,6 @@
+namespace DemoBlaze.Tests;
+
+public class UserTest
+{
+    
+}
