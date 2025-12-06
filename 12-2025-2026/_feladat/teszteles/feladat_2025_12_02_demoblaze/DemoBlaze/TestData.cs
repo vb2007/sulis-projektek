4b6cbc8e0201a8dedf0aa36bd231130676d40502
VB2007 <admin@vb2007.hu>
2025-12-06 15:12:39 +0100

    added FooterData to TestData

--- 12-2025-2026/_feladat/teszteles/feladat_2025_12_02_demoblaze/DemoBlaze/TestData.cs
+++ 12-2025-2026/_feladat/teszteles/feladat_2025_12_02_demoblaze/DemoBlaze/TestData.cs
@@ -12,4 +12,24 @@ public class TestData
         public static string Password = "abc123";
         public static string InvalidPassword = "abc1234";
     }
+
+    public class FooterData
+    {
+        class Titles
+        {
+            public static string AboutUs = "About Us";
+            public static string GetInTouch = "Get in Touch";
+        }
+
+        class Content
+        {
+            public static string AboutUs =
+                "We believe performance needs to be validated at every stage of the software development cycle and our open source compatible, massively scalable platform makes that a reality.";
+            public static string Address = "Address: 2390 El Camino Real";
+            public static string Phone = "Phone: +440 123456";
+            public static string Email = "Email: demo@blazemeter.com";
+        }
+
+        public static string CopyrightNotice = "Copyright © Product Store";
+    }
 }
