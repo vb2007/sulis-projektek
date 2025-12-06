c70762860832a2042cdf40c0319d972a7f9cecad
VB2007 <admin@vb2007.hu>
2025-12-06 23:05:01 +0100

    fixed selectors with xpath

--- 12-2025-2026/_feladat/teszteles/feladat_2025_12_02_demoblaze/DemoBlaze/Tests/StaticTextTest.cs
+++ 12-2025-2026/_feladat/teszteles/feladat_2025_12_02_demoblaze/DemoBlaze/Tests/StaticTextTest.cs
@@ -11,14 +11,14 @@ public class StaticTextTest
     private static WebDriver WebDriver => _browser.WebDriver;
 
     private IWebElement TitleElement => WebDriver.FindElement(By.Id("nava"));
-    private IWebElement FooterContainerElement = WebDriver.FindElement(By.Id("fotcont"));
-    private IWebElement FooterAboutUsHeaderElement => FooterContainerElement.FindElement(By.CssSelector("div.col-*:nth-of-type(1) h4 b"));
-    private IWebElement FooterAboutUsContentElement => FooterContainerElement.FindElement(By.CssSelector("div.col-*:nth-of-type(1) h4 p"));
-    private IWebElement FooterGetInTouchHeaderElement => FooterContainerElement.FindElement(By.CssSelector("div.col-*:nth-of-type(2) h4 b"));
-    private IWebElement FooterGetInTouchAddressContentElement => FooterContainerElement.FindElement(By.CssSelector("div.col-*:nth-of-type(2) h4 p:nth-of-type(1)"));
-    private IWebElement FooterGetInTouchPhoneContentElement => FooterContainerElement.FindElement(By.CssSelector("div.col-*:nth-of-type(2) h4 p:nth-of-type(2)"));
-    private IWebElement FooterGetInTouchEmailContentElement => FooterContainerElement.FindElement(By.CssSelector("div.col-*:nth-of-type(2) h4 p:nth-of-type(3)"));
-    private IWebElement FooterProductStoreHeaderElement => FooterContainerElement.FindElement(By.CssSelector("div.col-*:nth-of-type(3) h4"));
+    private IWebElement FooterContainerElement => WebDriver.FindElement(By.Id("fotcont"));
+    private IWebElement FooterAboutUsHeaderElement => FooterContainerElement.FindElement(By.XPath(".//div[contains(@class, 'col-sm-4')][1]//h4/b"));
+    private IWebElement FooterAboutUsContentElement => FooterContainerElement.FindElement(By.XPath(".//div[contains(@class, 'col-sm-4')][1]//p"));
+    private IWebElement FooterGetInTouchHeaderElement => FooterContainerElement.FindElement(By.XPath(".//div[contains(@class, 'col-sm-3')]//h4/b"));
+    private IWebElement FooterGetInTouchAddressContentElement => FooterContainerElement.FindElement(By.XPath(".//div[contains(@class, 'col-sm-3')]//p[1]"));
+    private IWebElement FooterGetInTouchPhoneContentElement => FooterContainerElement.FindElement(By.XPath(".//div[contains(@class, 'col-sm-3')]//p[2]"));
+    private IWebElement FooterGetInTouchEmailContentElement => FooterContainerElement.FindElement(By.XPath(".//div[contains(@class, 'col-sm-3')]//p[3]"));
+    private IWebElement FooterProductStoreHeaderElement => FooterContainerElement.FindElement(By.XPath(".//div[contains(@class, 'col-sm-4')][2]//h4"));
 
     [SetUp]
     public void Setup()
