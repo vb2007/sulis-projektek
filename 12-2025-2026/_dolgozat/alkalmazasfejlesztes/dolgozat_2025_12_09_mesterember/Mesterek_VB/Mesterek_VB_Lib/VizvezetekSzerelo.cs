cc562d2875910baaec36d7b3cf20bee8250872d3
VB2007 <admin@vb2007.hu>
2025-12-09 12:13:26 +0100

    implemented MunkatVallal & SzabadnapokSzama

--- 12-2025-2026/_dolgozat/alkalmazasfejlesztes/dolgozat_2025_12_09_mesterember/Mesterek_VB/Mesterek_VB_Lib/VizvezetekSzerelo.cs
+++ 12-2025-2026/_dolgozat/alkalmazasfejlesztes/dolgozat_2025_12_09_mesterember/Mesterek_VB/Mesterek_VB_Lib/VizvezetekSzerelo.cs
@@ -9,8 +9,29 @@ public sealed class VizvezetekSzerelo : MesterEmber
         TapasztalatokEve = tapasztalatokEve;
     }
     
+    public new bool MunkatVallal(int nap)
+    {
+        if (nap < 2 || nap > 30)
+        {
+            return false;
+        }
+        
+        if (!this[nap - 1] || !this[nap] || !this[nap + 1])
+        {
+            return false;
+        }
+        
+        Elfoglaltsag[nap - 2] = false;
+        Elfoglaltsag[nap - 1] = false;
+        Elfoglaltsag[nap] = false;
+        
+        return true;
+    }
+    
+    public new int SzabadnapokSzama => FoglalhatoNapok().Count();
+    
     public override string ToString()
     {
-        return $"Neve: {Nev}, Tapasztalatok évei: {TapasztalatokEve} Napidíja: {NapiDij}, Szabadnapok: {string.Join(", ", FoglalhatoNapok())}";
+        return $"Neve: {Nev}, Tapasztalatok évei: {TapasztalatokEve}, Napidíja: {NapiDij}, Szabadnapok: {string.Join(", ", FoglalhatoNapok())}";
     }
 }
