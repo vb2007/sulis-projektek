54f9ff733dc53ec8654248c6957708d31fb5e86d
VB2007 <admin@vb2007.hu>
2025-12-09 11:27:31 +0100

    update

--- 12-2025-2026/_dolgozat/alkalmazasfejlesztes/dolgozat_2025_12_09_mesterember/Mesterek_VB/Mesterek_VB_Lib/TulSokElfoglaltsagException.cs
+++ 12-2025-2026/_dolgozat/alkalmazasfejlesztes/dolgozat_2025_12_09_mesterember/Mesterek_VB/Mesterek_VB_Lib/TulSokElfoglaltsagException.cs
@@ -1,6 +1,6 @@
 namespace Mesterek_VB_Lib;
 
-public class TulSokElfoglaltsagException
+public class TulSokElfoglaltsagException : Exception
 {
-    
+    public TulSokElfoglaltsagException() : base("A mester túl sok munkát vállalt!") { }
 }
