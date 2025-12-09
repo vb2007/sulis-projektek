edff842e8014528d08687a14f066fe03b90e4465
VB2007 <admin@vb2007.hu>
2025-12-09 10:37:14 +0100

    created class params & constructor

--- 12-2025-2026/_feladat/backend/feladat_2025_12_09_tanulok/vagvolgyi-balazs-diak-faker/Neu/Iskola/Diak.php
+++ 12-2025-2026/_feladat/backend/feladat_2025_12_09_tanulok/vagvolgyi-balazs-diak-faker/Neu/Iskola/Diak.php
@@ -1,1 +1,23 @@
 <?php
+
+namespace Neu\Iskola;
+
+use DateTime;
+
+class Diak {
+    private string $vnev;
+    private string $knev;
+    private string $email;
+    private DateTime $szuletett;
+    private int $szamlalo;
+    private int $sorszam;
+
+    public function __construct(string $vnev, string $knev, string $email, DateTime $szuletett, int $szamlalo, int $sorszam) {
+        $this->vnev = $vnev;
+        $this->knev = $knev;
+        $this->email = $email;
+        $this->szuletett = $szuletett;
+        $this->szamlalo = $szamlalo;
+        $this->sorszam = $sorszam;
+    }
+}
