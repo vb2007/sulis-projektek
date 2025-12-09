34ceb2f4e0e894ca838efb77f656e65b74145629
VB2007 <admin@vb2007.hu>
2025-12-09 11:45:25 +0100

    added method & property for free days

--- 12-2025-2026/_dolgozat/alkalmazasfejlesztes/dolgozat_2025_12_09_mesterember/Mesterek_VB/Mesterek_VB_Lib/MesterEmber.cs
+++ 12-2025-2026/_dolgozat/alkalmazasfejlesztes/dolgozat_2025_12_09_mesterember/Mesterek_VB/Mesterek_VB_Lib/MesterEmber.cs
@@ -16,4 +16,18 @@ public class MesterEmber : IFoglalhato
             Elfoglaltsag[i] = true;
         }
     }
+    
+    public IEnumerable<int> FoglalhatoNapok()
+    {
+        for (int i = 0; i < Elfoglaltsag.Length; i++)
+        {
+            if (Elfoglaltsag[i])
+            {
+                // return i;
+                yield return i;
+            }
+        }
+    }
+
+    public int SzabadnapokSzama => Elfoglaltsag.Count(x => x);
 }
