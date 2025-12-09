5547bcce50dd701c9ad3d7b4a820aae17ed608a1
VB2007 <admin@vb2007.hu>
2025-12-09 10:44:08 +0100

    updated constructor, written magic getter

--- 12-2025-2026/_feladat/backend/feladat_2025_12_09_tanulok/vagvolgyi-balazs-diak-faker/Neu/Iskola/Diak.php
+++ 12-2025-2026/_feladat/backend/feladat_2025_12_09_tanulok/vagvolgyi-balazs-diak-faker/Neu/Iskola/Diak.php
@@ -12,12 +12,23 @@ class Diak {
     private int $szamlalo;
     private int $sorszam;
 
-    public function __construct(string $vnev, string $knev, string $email, DateTime $szuletett, int $szamlalo, int $sorszam) {
+    public function __construct(string $vnev, string $knev, string $email, DateTime $szuletett) {
         $this->vnev = $vnev;
         $this->knev = $knev;
         $this->email = $email;
         $this->szuletett = $szuletett;
-        $this->szamlalo = $szamlalo;
-        $this->sorszam = $sorszam;
+        $this->szamlalo = 0;
+        $this->sorszam = 0;
+    }
+
+    public function __get($name) {
+        switch ($name) {
+            case "teljes_nev":
+                return $this->vnev . " " . $this->knev;
+            case "szuletett_iso":
+                return $this->szuletett;
+            default:
+                return $this->name;
+        }
     }
 }
