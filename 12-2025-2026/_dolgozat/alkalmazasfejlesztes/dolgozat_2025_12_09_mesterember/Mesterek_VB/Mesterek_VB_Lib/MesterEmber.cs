9e0e24a8bff95220739b55fb3362c8cedf55c414
VB2007 <admin@vb2007.hu>
2025-12-09 11:31:44 +0100

    created base properties for mesterember

---
+++ 12-2025-2026/_dolgozat/alkalmazasfejlesztes/dolgozat_2025_12_09_mesterember/Mesterek_VB/Mesterek_VB_Lib/MesterEmber.cs
@@ -0,0 +1,10 @@
+namespace Mesterek_VB_Lib;
+
+public class MesterEmber : IFoglalhato
+{
+    public string Name { get; init; }
+    public int NapiDij { get; init; }
+    public bool[] Elfoglaltsan = new bool[31];
+    
+    
+}
