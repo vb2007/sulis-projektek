c2f6e71a90da6f0942ee7c55a29544f2f602e7d1
VB2007 <admin@vb2007.hu>
2025-12-11 00:41:35 +0100

    fixed diak.php

--- 12-2025-2026/_feladat/backend/feladat_2025_12_09_tanulok/vagvolgyi-balazs-diak-faker/src/Neu/Iskola/Diak.php
+++ 12-2025-2026/_feladat/backend/feladat_2025_12_09_tanulok/vagvolgyi-balazs-diak-faker/src/Neu/Iskola/Diak.php
@@ -9,28 +9,28 @@ class Diak {
     private string $knev;
     private string $email;
     private DateTime $szuletett;
-    private int $szamlalo;
+    private static int $szamlalo; //UML aláhúzás = static
     private int $sorszam;
 
     public function __construct(string $vnev, string $knev, string $email, DateTime $szuletett) {
-        $this->vnev = $vnev;
-        $this->knev = $knev;
-        $this->email = $email;
-        $this->szuletett = $szuletett;
-        $this->szamlalo = 0;
-        $this->sorszam = 0;
-    }
+      $this->vnev = $vnev;
+      $this->knev = $knev;
+      $this->email = $email;
+      $this->szuletett = $szuletett;
+      self::$szamlalo++;
+      $this->sorszam = self::$szamlalo;
+   }
 
-    public function __get($name) {
-        switch ($name) {
-            case "teljes_nev":
-                return $this->vnev . " " . $this->knev;
-            case "szuletett_iso":
-                return $this->szuletett;
-            default:
-                return $this->$name;
-        }
-    }
+   public function __get($name) {
+      switch ($name) {
+         case "teljes_nev":
+               return $this->vnev . " " . $this->knev;
+         case "szuletett_iso":
+               return $this->szuletett->format('Y-m-d');
+         default:
+               return $this->$name;
+      }
+   }
 
     public function __set($name, $value){
         $this->$name = $value;
