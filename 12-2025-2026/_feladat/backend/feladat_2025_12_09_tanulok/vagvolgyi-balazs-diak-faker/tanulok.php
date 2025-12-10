7cfb3b550d152e116fc4e143a82a4c90ef61ca8d
VB2007 <admin@vb2007.hu>
2025-12-10 23:37:22 +0100

    added param check

--- 12-2025-2026/_feladat/backend/feladat_2025_12_09_tanulok/vagvolgyi-balazs-diak-faker/tanulok.php
+++ 12-2025-2026/_feladat/backend/feladat_2025_12_09_tanulok/vagvolgyi-balazs-diak-faker/tanulok.php
@@ -1,2 +1,17 @@
  <?php
+
 require __DIR__ . "/vendor/autoload.php";
+
+use Neu\Iskola\Diak;
+
+if ($argc != 2) {
+    echo "A szkriptnek nem lett megadva a kimeneti fájl neve" . PHP_EOL;
+    exit(1);
+}
+
+$outputFile = $argv[1];
+
+if (!str_ends_with($outputFile, ".txt") && !str_ends_with($outputFile, ".csv")) {
+    echo "A kimeneti fájl kiterjesztése csak `txt` vagy `csv` lehet!" . PHP_EOL;
+    exit(2);
+}
