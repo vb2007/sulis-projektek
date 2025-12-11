11635273631990d1005bf78a9bf11366b5c4e798
VB2007 <admin@vb2007.hu>
2025-12-11 02:02:09 +0100

    addeed code for gen & fw

--- 12-2025-2026/_feladat/backend/feladat_2025_12_09_tanulok/demo/index.php
+++ 12-2025-2026/_feladat/backend/feladat_2025_12_09_tanulok/demo/index.php
@@ -5,9 +5,32 @@ require __DIR__ . "/vendor/autoload.php";
 use Acme\Vehicles\Train;
 use Faker\Factory;
 
-$train = new Train(3, "ic", "MÁV");
-echo "Number: " . $train->number . PHP_EOL;
-echo "Type: " . $train->type . PHP_EOL;
-echo "Old operator: " . $train->operator . PHP_EOL;
-$train->operator = "GYSEV";
-echo "New operator: " . $train->operator . PHP_EOL;
+// $train = new Train(3, "ic", "MÁV");
+// echo "Number: " . $train->number . PHP_EOL;
+// echo "Type: " . $train->type . PHP_EOL;
+// echo "Old operator: " . $train->operator . PHP_EOL;
+// $train->operator = "GYSEV";
+// echo "New operator: " . $train->operator . PHP_EOL;
+
+$faker = Factory::create();
+
+$trains = [];
+
+for ($i = 0; $i < 100; $i++) {
+   $train = new Train(
+      $faker->numberBetween(1, 9999),
+      $faker->randomElement(["rjx", "ec", "en", "ic", "ir", "s", "gy", "sz"]),
+      $faker->randomElement(["MÁV", "ÖBB", "CD", "ZSSK", "GYSEV"])
+   );
+
+   array_push($trains, $train);
+}
+
+$file = fopen("out/tains.txt", "w");
+fwrite($file, "type\tnumber\toperator" . PHP_EOL);
+
+foreach ($trains as $train) {
+   fwrite($file, $train . PHP_EOL);
+}
+
+fclose($file);
