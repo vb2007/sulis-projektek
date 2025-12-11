60cc764c4133951d2110f27da9f271e26291afda
VB2007 <admin@vb2007.hu>
2025-12-11 02:27:35 +0100

    added setup, teardown logic

--- 12-2025-2026/_feladat/teszteles/feladat_2025_12_08_sajat_oldal_tesztelese/OrszagokTest/Tests/HTMLStuctureTest.cs
+++ 12-2025-2026/_feladat/teszteles/feladat_2025_12_08_sajat_oldal_tesztelese/OrszagokTest/Tests/HTMLStuctureTest.cs
@@ -9,5 +9,23 @@ public class HTMLStuctureTest
 {
     private static Browser _browser;
     private static WebDriver WebDriver => _browser.WebDriver;
+
+    [SetUp]
+    public void Setup()
+    {
+        ExcelReportGenerator.StartTest(TestContext.CurrentContext.Test.FullName);
+        
+        _browser = new Browser();
+        _browser.Init();
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        _browser?.Teardown();
+        
+        ExcelReportGenerator.EndTest();
+    }
+    
     
 }
