77b33be85070e015ab71243fdee38bd8c4d1ecbb
VB2007 <admin@vb2007.hu>
2025-12-11 02:27:02 +0100

    added sturecure for the only test file

--- 12-2025-2026/_feladat/teszteles/feladat_2025_12_08_sajat_oldal_tesztelese/OrszagokTest/Tests/HTMLStuctureTest.cs
+++ 12-2025-2026/_feladat/teszteles/feladat_2025_12_08_sajat_oldal_tesztelese/OrszagokTest/Tests/HTMLStuctureTest.cs
@@ -1,6 +1,13 @@
+using OrszagokTest.Helpers;
+using OpenQA.Selenium;
+
 namespace OrszagokTest.Tests;
 
+[TestFixture]
+[Parallelizable(ParallelScope.All)]
 public class HTMLStuctureTest
 {
+    private static Browser _browser;
+    private static WebDriver WebDriver => _browser.WebDriver;
     
 }
