28bc160d2c5e77f75681adbc9e7f024f3f8c71fb
VB2007 <admin@vb2007.hu>
2025-12-18 00:07:01 +0100

    created & implemented base function for jegy class

--- 12-2025-2026/_feladat/backend/feladat_2025_12_15_jegyek/vagvolgyi-balazs-jegyek/src/Acme/Iskola/Jegy.php
+++ 12-2025-2026/_feladat/backend/feladat_2025_12_15_jegyek/vagvolgyi-balazs-jegyek/src/Acme/Iskola/Jegy.php
@@ -1,1 +1,36 @@
 <?php
+
+namespace Acme\Iskola;
+
+use DateTime;
+
+class Jegy {
+    private static array $tipusok = ["Témazáró", "Órai munka", "Teszt"];
+    private static array $osztalyzatok = ["elégtelen", "elégséges", "közepes", "jó", "jeles"];
+    private static array $tantargyak = ["Matematika", "Angol", "Programozás", "Nyelvtan", "Történelem"];
+    private string $tipus;
+    private int $jegy;
+    private string $tantargy;
+    private string $tanar;
+    private DateTime $beirva;
+
+    private function __construct(string $tipus, int $jegy, string $tantargy, string $tanar, DateTime $beirva) {
+        $this->tipus = $tipus;
+        $this->jegy = $jegy;
+        $this->tantargy = $tantargy;
+        $this->tanar = $tanar;
+        $this->beirva = $beirva;
+    }
+
+    private function lehetsegesTipusok() :array {
+        return $this->tipusok;
+    }
+
+    private function lehetsegesOsztalyzatok() :array {
+        return $this->osztalyzatok;
+    }
+
+    private function lehetsegesTantargyak() :array {
+        return $this->tantargyak;
+    }
+}
