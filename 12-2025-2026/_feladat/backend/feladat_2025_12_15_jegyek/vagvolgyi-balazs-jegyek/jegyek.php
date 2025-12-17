663d523f0bc1cd2e169ef2dba486e78b1629e6a1
VB2007 <admin@vb2007.hu>
2025-12-18 00:30:51 +0100

    added param checks to main file

--- 12-2025-2026/_feladat/backend/feladat_2025_12_15_jegyek/vagvolgyi-balazs-jegyek/jegyek.php
+++ 12-2025-2026/_feladat/backend/feladat_2025_12_15_jegyek/vagvolgyi-balazs-jegyek/jegyek.php
@@ -7,3 +7,31 @@ use Faker\Factory;
 use Acme\Iskola\Jegy;
 
 $faker = Factory::create("hu_HU");
+
+if ($argc < 2) {
+    echo "Az első két paraméter megadása kötelező!" . PHP_EOL;
+    exit(3);
+}
+
+$tipus = $argv[1];
+if ($tipus !== "mozi" && $tipus !== "osztalyzat") {
+    echo "Nem megfelelő paraméter!" . PHP_EOL;
+    echo "Az első paraméter csak 'busz', 'mozi', 'osztalyzat' vagy 'repulo' lehet!" . PHP_EOL;
+    exit(7);
+}
+
+$darabszam = $argv[2];
+if (!is_int($darabszam) || intval($darabszam) < 1) {
+    echo "Nem megfelelő paraméter!" . PHP_EOL;
+    echo "A második paraméternek 0-nál nagyobb számnak kell lennie!" . PHP_EOL;
+    exit(2);
+}
+
+$kimenet = "csv";
+if (isset($argv[3])) {
+    if ($argv[3] !== "csv" && $argv[3] !== "json") {
+        echo "Nem megfelelő paraméter!" . PHP_EOL;
+        echo "A harmadik paraméter csak 'csv' vagy 'json' lehet amennyiben meg lett adva!" . PHP_EOL;
+        exit(9);
+    }
+}
