b5d7dd91a4902bba762eeaca979b8aa7667ca003
VB2007 <admin@vb2007.hu>
2025-12-18 01:36:50 +0100

    implemented data gen with faker

--- 12-2025-2026/_feladat/backend/feladat_2025_12_15_jegyek/vagvolgyi-balazs-jegyek/jegyek.php
+++ 12-2025-2026/_feladat/backend/feladat_2025_12_15_jegyek/vagvolgyi-balazs-jegyek/jegyek.php
@@ -20,18 +20,36 @@ if ($tipus != "busz" && $tipus != "mozi" && $tipus != "osztalyzat" && $tipus != 
     exit(7);
 }
 
-$darabszam = $argv[2];
-if (!is_int($darabszam) && intval($darabszam) < 1) {
+if (!is_int($argv[2]) && intval($argv[2]) < 1) {
     echo "Nem megfelelő paraméter!" . PHP_EOL;
     echo "A második paraméternek 0-nál nagyobb számnak kell lennie!" . PHP_EOL;
     exit(2);
 }
+$darabszam = $argv[2];
 
-$kimenet = "csv";
+$kimenet = null;
 if (isset($argv[3])) {
     if ($argv[3] !== "csv" && $argv[3] !== "json") {
         echo "Nem megfelelő paraméter!" . PHP_EOL;
         echo "A harmadik paraméter csak 'csv' vagy 'json' lehet amennyiben meg lett adva!" . PHP_EOL;
         exit(9);
+    }
+
+    $kimenet = $argv[3];
+}
+
+$jegyek = [];
+
+if ($tipus == "osztalyzat") {
+    for ($i = 0; $i < $darabszam; $i++) {
+        $jegy = new Jegy(
+            $faker->randomElement(Jegy::lehetsegesTipusok()),
+            $faker->numberBetween(1, 5),
+            $faker->randomElement(Jegy::lehetsegesTantargyak()),
+            $faker->name(),
+            $faker->dateTimeBetween('-2 weeks', 'now')
+        );
+
+        $jegyek[] = $jegy;
     }
 }
