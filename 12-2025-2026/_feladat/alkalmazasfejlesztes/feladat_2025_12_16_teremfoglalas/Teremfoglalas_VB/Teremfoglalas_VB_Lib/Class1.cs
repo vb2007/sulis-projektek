ae316621e7d4d8e389a2bf9707bdf1978be2d3f4
VB2007 <admin@vb2007.hu>
2025-12-18 02:34:16 +0100

    created interface

--- 12-2025-2026/_feladat/alkalmazasfejlesztes/feladat_2025_12_16_teremfoglalas/Teremfoglalas_VB/Teremfoglalas_VB_Lib/Class1.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace Teremfoglalas_VB_Lib;
-
-public class Class1
-{
-}
---
+++ 12-2025-2026/_feladat/alkalmazasfejlesztes/feladat_2025_12_16_teremfoglalas/Teremfoglalas_VB/Teremfoglalas_VB_Lib/IFoglalas.cs
@@ -0,0 +1,8 @@
+namespace Teremfoglalas_VB_Lib;
+
+public interface IFoglalas
+{
+    int HelyszinAzonosito();
+    DateTime Kezdete();
+    DateTime Vege();
+}
