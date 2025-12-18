fa947bc573bcaa5731ff7f6d879cf28f60d74100
VB2007 <admin@vb2007.hu>
2025-12-18 22:18:05 +0100

    created static variables

--- 12-2025-2026/_feladat/alkalmazasfejlesztes/feladat_2025_12_16_teremfoglalas/Teremfoglalas_VB/Teremfoglalas_VB/Program.cs
+++ 12-2025-2026/_feladat/alkalmazasfejlesztes/feladat_2025_12_16_teremfoglalas/Teremfoglalas_VB/Teremfoglalas_VB/Program.cs
@@ -1,11 +1,13 @@
-﻿using Teremfoglalas_VB_Lib;
+using Teremfoglalas_VB_Lib;
 
 namespace Teremfoglalas_VB;
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        
-    }
+    static Dictionary<int, string> teremAzonositoMap = new Dictionary<int, string>();
+    static Dictionary<int, string> tanarAzonositoMap = new Dictionary<int, string>();
+    static int teremCounter = 1;
+    static int tanarCounter = 1;
+
+    
 }
