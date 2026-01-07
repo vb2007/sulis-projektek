c68584456ee88b05b9b4e37345995b544849e968
VB2007 <admin@vb2007.hu>
2026-01-07 06:45:51 +0100

    implemented class termekek

--- 12-2025-2026/_feladat/alkalmazasfejlesztes/feladat_2026_01_06_viragkoteszet/Viragkoteszet_VB/Viragkoteszet_VB_Lib/Termekek.cs
+++ 12-2025-2026/_feladat/alkalmazasfejlesztes/feladat_2026_01_06_viragkoteszet/Viragkoteszet_VB/Viragkoteszet_VB_Lib/Termekek.cs
@@ -2,5 +2,20 @@ namespace Viragkoteszet_VB_Lib;
 
 public class Termekek
 {
+    public List<Termek> TermekLista { get; } = new List<Termek>();
     
+    public Termekek(IEnumerable<Termek> termekek)
+    {
+        foreach (Termek termek in termekek)
+        {
+            TermekLista.Add(termek);
+        }
+    }
+    
+    public Termek this[int sorszam] => TermekLista.ElementAt(sorszam);
+
+    public override string ToString()
+    {
+        return base.ToString();
+    }
 }
