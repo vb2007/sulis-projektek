d51b933a46e4894f58a1b025402a1c27333af88c
VB2007 <admin@vb2007.hu>
2026-01-07 06:42:47 +0100

    created class termekek

---
+++ 12-2025-2026/_feladat/alkalmazasfejlesztes/feladat_2026_01_06_viragkoteszet/Viragkoteszet_VB/Viragkoteszet_VB_Lib/Termek.cs
@@ -0,0 +1,16 @@
+namespace Viragkoteszet_VB_Lib;
+
+public class Termek : ITermek
+{
+    public int Azonosito { get; }
+    public string Tipus { get; }
+    public string Megnevezes { get; }
+    public int ElkeszitesiIdo { get; }
+    public List<IDictionary<string, int>> AlapanyagLista { get; }
+    public int Ar { get; }
+
+    public Termek(int azonosito, string tipus, string megnevezes, List<IDictionary<string, int>> alapanyagLista)
+    {
+        
+    }
+}
