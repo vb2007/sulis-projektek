6a563131673f02a2ac50569a3013604864e92358
VB2007 <admin@vb2007.hu>
2026-01-07 06:18:16 +0100

    created constructor for alapanyag class

--- 12-2025-2026/_feladat/alkalmazasfejlesztes/feladat_2026_01_06_viragkoteszet/Viragkoteszet_VB/Viragkoteszet_VB_Lib/Alapanyag.cs
+++ 12-2025-2026/_feladat/alkalmazasfejlesztes/feladat_2026_01_06_viragkoteszet/Viragkoteszet_VB/Viragkoteszet_VB_Lib/Alapanyag.cs
@@ -6,4 +6,12 @@ public class Alapanyag
     public string Nev { get; set; }
     public int Ar { get; set; }
     public int ElkeszitesiIdo { get; set; } //percben
+
+    public Alapanyag(string azonosito, string nev, int ar, int elkeszitesiIdo)
+    {
+        Azonosito = azonosito;
+        Nev = nev;
+        Ar = ar;
+        ElkeszitesiIdo = elkeszitesiIdo;
+    }
 }
