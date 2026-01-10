8f2fed99f67531920759ef668511d15bf72260e8
VB2007 <admin@vb2007.hu>
2026-01-10 23:11:03 +0100

    completed task 20, 21

--- 12-2025-2026/_feladat/webprogramozas/feladat_2026_01_06_repulok/vagvolgyi-balazs-repulok/src/render.js
+++ 12-2025-2026/_feladat/webprogramozas/feladat_2026_01_06_repulok/vagvolgyi-balazs-repulok/src/render.js
@@ -2,9 +2,48 @@ import { Table } from "cli-table3";
 
 export default {
     renderCLITable(flights, column) {
+        const colHeaderNames = [
+            "#",
+            "Légitársaság",
+            "Repülő",
+            "Honnan",
+            "Hova",
+        ];
+
+        const colHeaderNamesColored = colHeaderNames.map((col) => {
+            if (col === column) {
+                return chalk.blue(col);
+            }
+
+            return col;
+        });
+
         const table = new Table({
-            head: ["#", "Légitársaság", "Repülő", "Honnan", "Hova"],
-            colWidths: [100, 200, 100, 100, 100],
+            head: colHeaderNamesColored,
+            colWidths: [5, 20, 15, 15, 15],
+        });
+
+        flights.forEach((flight, index) => {
+            const { flightNumber, airline, airplane, departure, arrival } =
+                flight;
+
+            const rowData = [
+                index + 1,
+                airline.name,
+                airplane.name,
+                departure.name,
+                arrival.name,
+            ];
+
+            const rowDataColored = rowData.map((data, colIndex) => {
+                if (colHeaderNames[colIndex] === column) {
+                    return chalk.blue(data);
+                }
+
+                return data;
+            });
+
+            table.push(rowDataColored);
         });
 
         return table.toString();
