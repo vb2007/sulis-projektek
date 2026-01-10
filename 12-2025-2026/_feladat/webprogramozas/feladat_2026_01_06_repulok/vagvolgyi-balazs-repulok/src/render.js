2885462d4a312e6a563196c7266c1bbebd193866
VB2007 <admin@vb2007.hu>
2026-01-10 23:12:13 +0100

    fixed default export formatting

--- 12-2025-2026/_feladat/webprogramozas/feladat_2026_01_06_repulok/vagvolgyi-balazs-repulok/src/render.js
+++ 12-2025-2026/_feladat/webprogramozas/feladat_2026_01_06_repulok/vagvolgyi-balazs-repulok/src/render.js
@@ -1,51 +1,42 @@
 import { Table } from "cli-table3";
 
-export default {
-    renderCLITable(flights, column) {
-        const colHeaderNames = [
-            "#",
-            "Légitársaság",
-            "Repülő",
-            "Honnan",
-            "Hova",
+export default renderCLITable = (flights, column) => {
+    const colHeaderNames = ["#", "Légitársaság", "Repülő", "Honnan", "Hova"];
+
+    const colHeaderNamesColored = colHeaderNames.map((col) => {
+        if (col === column) {
+            return chalk.blue(col);
+        }
+
+        return col;
+    });
+
+    const table = new Table({
+        head: colHeaderNamesColored,
+        colWidths: [5, 20, 15, 15, 15],
+    });
+
+    flights.forEach((flight, index) => {
+        const { flightNumber, airline, airplane, departure, arrival } = flight;
+
+        const rowData = [
+            index + 1,
+            airline.name,
+            airplane.name,
+            departure.name,
+            arrival.name,
         ];
 
-        const colHeaderNamesColored = colHeaderNames.map((col) => {
-            if (col === column) {
-                return chalk.blue(col);
+        const rowDataColored = rowData.map((data, colIndex) => {
+            if (colHeaderNames[colIndex] === column) {
+                return chalk.blue(data);
             }
 
-            return col;
+            return data;
         });
 
-        const table = new Table({
-            head: colHeaderNamesColored,
-            colWidths: [5, 20, 15, 15, 15],
-        });
+        table.push(rowDataColored);
+    });
 
-        flights.forEach((flight, index) => {
-            const { flightNumber, airline, airplane, departure, arrival } =
-                flight;
-
-            const rowData = [
-                index + 1,
-                airline.name,
-                airplane.name,
-                departure.name,
-                arrival.name,
-            ];
-
-            const rowDataColored = rowData.map((data, colIndex) => {
-                if (colHeaderNames[colIndex] === column) {
-                    return chalk.blue(data);
-                }
-
-                return data;
-            });
-
-            table.push(rowDataColored);
-        });
-
-        return table.toString();
-    },
+    return table.toString();
 };
