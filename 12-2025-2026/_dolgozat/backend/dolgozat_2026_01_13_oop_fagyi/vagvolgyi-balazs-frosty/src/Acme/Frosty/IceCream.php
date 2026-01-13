858fd69ba05d635c02f9d531208fa36b71baa673
VB2007 <admin@vb2007.hu>
2026-01-13 10:04:06 +0100

    created class IceCream

--- 12-2025-2026/_dolgozat/backend/dolgozat_2026_01_13_oop_fagyi/vagvolgyi-balazs-frosty/src/Acme/Frosty/IceCream.php
+++ 12-2025-2026/_dolgozat/backend/dolgozat_2026_01_13_oop_fagyi/vagvolgyi-balazs-frosty/src/Acme/Frosty/IceCream.php
@@ -1,1 +1,41 @@
 <?php
+
+namespace Acme\Frosty;
+
+class IceCream {
+    private int $scoop;
+    private int $price;
+    private bool $sweetCone;
+    private array $flavours;
+    private static array $availableFlavours;
+
+    public function __construct(int $scoop, bool $sweetCone) {
+        $this->scoop = $scoop;
+        $this->sweetCone = $sweetCone;
+    }
+
+    public function __get(string $name) :mixed {
+        return $this->$name;
+    }
+
+    public function __set(string $name, mixed $value) :void {
+        $this->$name = $value;
+    }
+
+    //static?
+    public function availableFlavours() :array {
+        return $this->availableFlavours;
+    }
+
+    public function addFlavour(string $flavour) :void {
+        array_push($availableFlavours, $flavour);
+    }
+
+    public function __tostring() :string {
+        return "";
+    }
+
+    public function toArray() :array {
+
+    }
+}
