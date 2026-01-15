e950ac9169ab89f5efbe08d7c4a67b1e95da16cc
VB2007 <admin@vb2007.hu>
2026-01-15 12:04:47 +0100

    used custom class in program.cs

--- 12-2025-2026/_feladat/alkalmazasfejlesztes/feladat_2026_01_15_alma/Alma_VB/Alma_VB/Program.cs
+++ 12-2025-2026/_feladat/alkalmazasfejlesztes/feladat_2026_01_15_alma/Alma_VB/Alma_VB/Program.cs
@@ -1,9 +1,24 @@
-﻿namespace Alma_VB;
+﻿using System.Text.Json;
+using Alma_VB_Lib;
+
+namespace Alma_VB;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Alma alma;
+        
+        string fajlNev = "alma.json";
+        try
+        {
+            alma = JsonSerializer.Deserialize<Alma>(File.ReadAllText(fajlNev))!;
+        }
+        catch
+        {
+            alma = new Alma();
+        }
+        
+        
     }
 }
