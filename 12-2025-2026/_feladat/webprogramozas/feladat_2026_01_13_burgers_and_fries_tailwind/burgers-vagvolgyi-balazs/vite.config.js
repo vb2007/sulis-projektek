ed734e0acca92e3f13d5b1ca1f4d74137f764421
VB2007 <admin@vb2007.hu>
2026-01-18 23:04:48 +0100

    added docker commands, created vite config, added pnpm package

---
+++ 12-2025-2026/_feladat/webprogramozas/feladat_2026_01_13_burgers_and_fries_tailwind/burgers-vagvolgyi-balazs/vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from "vite";
+import { fileURLToPath } from "node:url";
+import tailwindcss from "@tailwindcss/vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [tailwindcss()],
+    server: {
+        host: true,
+        port: 8080,
+        strictPort: true,
+    },
+    resolve: {
+        alias: {
+            "@": fileURLToPath(new URL("./src", import.meta.url)),
+            "@assets": fileURLToPath(new URL("./src/assets", import.meta.url)),
+        },
+    },
+});
