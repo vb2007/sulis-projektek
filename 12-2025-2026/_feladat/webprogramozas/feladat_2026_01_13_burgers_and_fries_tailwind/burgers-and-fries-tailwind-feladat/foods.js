6c65447b8f2647c93b8ca181db41ddef4a35ce53
VB2007 <admin@vb2007.hu>
2026-01-18 22:28:42 +0100

    added source files

---
+++ 12-2025-2026/_feladat/webprogramozas/feladat_2026_01_13_burgers_and_fries_tailwind/burgers-and-fries-tailwind-feladat/foods.js
@@ -0,0 +1,102 @@
+const foodList = {
+    "burgers": {
+        title: "Hamburgers",
+        description: "100% fresh beef, no fillers or preservatives",
+        items: [
+            {
+                name: "Hamburger",
+                price: 11.40,
+            },
+            {
+                name: "Cheeseburger",
+                price: 12.40,
+            },
+            {
+                name: "Bacon burger",
+                price: 13.60,
+            },
+            {
+                name: "Bacon Cheeseburger",
+                price: 14.60,
+            }
+        ]
+    },
+    "hotdogs": {
+        title: "Dogs",
+        items: [
+            {
+                name: "All beff hot dog",
+                price: 8.20
+            },
+            {
+                name: "Cheese dog",
+                price: 9.20
+            },
+            {
+                name: "Bacon dog",
+                price: 10.40
+            },
+            {
+                name: "Bacon cheese dog",
+                price: 11.40
+            }
+        ]
+    },
+    "sandwitches": {
+        title: "Sandwitches",
+        items: [
+            {
+                name: "Veggie sandwitch",
+                price: 7.00
+            },
+            {
+                name: "Veggie sandwitch with cheese",
+                price: 8.00
+            },
+            {
+                name: "Grilled cheese",
+                price: 8.00
+            },
+            {
+                name: "BLT (Bacon, Lettuce, Tomato)",
+                price: 9.90
+            }
+        ]
+    },
+    fries: {
+        title: "Fries",
+        description: "Coocked in penaut oil",
+        items: [
+            {
+                name: "Little",
+                price: 5.90
+            },
+            {
+                name: "Regular",
+                price: 7.20
+            },
+            {
+                name: "Large",
+                price: 8.20
+            }
+        ]
+    }
+
+}
+
+const toppings =  [
+    "Mayo",
+    "Lettuce",
+    "Pickles",
+    "Tomatoes",
+    "Ketchup",
+    "Mustard",
+    "Grilled onions",
+    "Grilled mushrooms",
+    "Relish onions",
+    "Green peppers",
+    "Jalapeño peppers",
+    "Steak sauce",
+    "Bar-B-Que Sauce",
+    "Hot sauce"
+]
---
+++ 12-2025-2026/_feladat/webprogramozas/feladat_2026_01_13_burgers_and_fries_tailwind/burgers-and-fries-tailwind-feladat/main.js
@@ -0,0 +1,67 @@
+import './assets/app.css'
+
+const container = document.querySelector(".container")
+
+function createGroup({title = null, description = null, items = []})
+{
+  const articleEl = document.createElement("article")
+
+  const titleEl = document.createElement("h1")
+  titleEl.textContent = title
+  articleEl.append(titleEl)
+
+  if(null !== description)
+  {
+    const descriptionEl = document.createElement("p")
+    descriptionEl.textContent = description
+    articleEl.append(descriptionEl)
+  }
+
+
+  const ul = document.createElement("ul")
+  
+
+  for(const item of items)
+  {
+    const liEl = document.createElement("li")
+
+    const productEl = document.createElement("span")
+
+    productEl.textContent = item.name
+    const priceEl = document.createElement("span")
+    priceEl.textContent = item.price.toLocaleString("de-DE", { style: "currency", currency: "EUR" })
+
+    liEl.append(productEl, priceEl)
+    ul.append(liEl) 
+  }
+  articleEl.append(ul)
+
+  return articleEl
+}
+
+
+function createToppings()
+{
+  const toppingsEl = document.createElement("div")
+
+  const titleEl = document.createElement("h1")
+  titleEl.textContent = "Toppings"
+
+  const ulEl = document.createElement("ul")
+
+  for(let topping of getToppings())
+  { 
+    const liEL = document.createElement("li")
+    liEL.textContent = topping
+    ulEl.append(liEL)
+  }
+  toppingsEl.append(titleEl, ulEl)
+  return toppingsEl
+} 
+
+const foodList = getFoodList()
+const foodsEl = document.createElement("div")
+
+foodsEl.append(...Object.keys(foodList).map(group => createGroup(foodList[group])))
+
+container.replaceChildren(foodsEl, createToppings())
