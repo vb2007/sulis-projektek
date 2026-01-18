cc473a2eb9c10fc3d508c419c94ccfcad29b5637
VB2007 <admin@vb2007.hu>
2026-01-18 22:58:58 +0100

    updated formatting

--- 12-2025-2026/_feladat/webprogramozas/feladat_2026_01_13_burgers_and_fries_tailwind/burgers-vagvolgyi-balazs/src/assets/data/foods.js
+++ 12-2025-2026/_feladat/webprogramozas/feladat_2026_01_13_burgers_and_fries_tailwind/burgers-vagvolgyi-balazs/src/assets/data/foods.js
@@ -1,67 +1,67 @@
 const foodList = {
-    "burgers": {
+    burgers: {
         title: "Hamburgers",
         description: "100% fresh beef, no fillers or preservatives",
         items: [
             {
                 name: "Hamburger",
-                price: 11.40,
+                price: 11.4,
             },
             {
                 name: "Cheeseburger",
-                price: 12.40,
+                price: 12.4,
             },
             {
                 name: "Bacon burger",
-                price: 13.60,
+                price: 13.6,
             },
             {
                 name: "Bacon Cheeseburger",
-                price: 14.60,
-            }
-        ]
+                price: 14.6,
+            },
+        ],
     },
-    "hotdogs": {
+    hotdogs: {
         title: "Dogs",
         items: [
             {
                 name: "All beff hot dog",
-                price: 8.20
+                price: 8.2,
             },
             {
                 name: "Cheese dog",
-                price: 9.20
+                price: 9.2,
             },
             {
                 name: "Bacon dog",
-                price: 10.40
+                price: 10.4,
             },
             {
                 name: "Bacon cheese dog",
-                price: 11.40
-            }
-        ]
+                price: 11.4,
+            },
+        ],
     },
-    "sandwitches": {
+    sandwitches: {
         title: "Sandwitches",
         items: [
             {
                 name: "Veggie sandwitch",
-                price: 7.00
+                price: 7.0,
             },
             {
                 name: "Veggie sandwitch with cheese",
-                price: 8.00
+                price: 8.0,
             },
             {
                 name: "Grilled cheese",
-                price: 8.00
+                price: 8.0,
             },
             {
                 name: "BLT (Bacon, Lettuce, Tomato)",
-                price: 9.90
-            }
-        ]
+                price: 9.9,
+            },
+        ],
     },
     fries: {
         title: "Fries",
@@ -69,22 +69,21 @@ const foodList = {
         items: [
             {
                 name: "Little",
-                price: 5.90
+                price: 5.9,
             },
             {
                 name: "Regular",
-                price: 7.20
+                price: 7.2,
             },
             {
                 name: "Large",
-                price: 8.20
-            }
-        ]
-    }
+                price: 8.2,
+            },
+        ],
+    },
+};
 
-}
-
-const toppings =  [
+const toppings = [
     "Mayo",
     "Lettuce",
     "Pickles",
@@ -98,5 +97,5 @@ const toppings =  [
     "Jalapeño peppers",
     "Steak sauce",
     "Bar-B-Que Sauce",
-    "Hot sauce"
-]
+    "Hot sauce",
+];
