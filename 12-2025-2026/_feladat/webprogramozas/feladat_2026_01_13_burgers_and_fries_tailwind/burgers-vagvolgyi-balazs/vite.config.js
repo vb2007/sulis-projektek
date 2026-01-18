e4c5459318e77b2e21a39a2c57b06d2a27095b1e
VB2007 <admin@vb2007.hu>
2026-01-18 23:09:54 +0100

    fixed vite config

--- 12-2025-2026/_feladat/webprogramozas/feladat_2026_01_13_burgers_and_fries_tailwind/burgers-vagvolgyi-balazs/vite.config.js
+++ 12-2025-2026/_feladat/webprogramozas/feladat_2026_01_13_burgers_and_fries_tailwind/burgers-vagvolgyi-balazs/vite.config.js
@@ -2,18 +2,24 @@ import { defineConfig } from "vite";
 import { fileURLToPath } from "node:url";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+    root: "./src",
     plugins: [tailwindcss()],
     server: {
         host: true,
         port: 8080,
-        strictPort: true,
+        open: "./index.html",
     },
     resolve: {
         alias: {
             "@": fileURLToPath(new URL("./src", import.meta.url)),
             "@assets": fileURLToPath(new URL("./src/assets", import.meta.url)),
+        },
+    },
+    build: {
+        outDir: "../dist",
+        rollupOptions: {
+            input: "./index.html",
         },
     },
 });
