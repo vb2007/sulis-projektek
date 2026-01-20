df7c9ce1c49496203f387d33de3240f016d25115
VB2007 <admin@vb2007.hu>
2026-01-20 10:41:48 +0100

    created & implemented jarmu.php

---
+++ 12-2025-2026/_feladat/backend/feladat_2026_01_20_oroklodes/feladat_jarmuvek/jarmuvek-vagvolgyi-balazs/src/Acme/Kozlekedes/Jarmu.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Acme\Kozlekedes;
+
+use LogicException;
+
+class Jarmu {
+    protected string $gyarto;
+    protected string $tipus;
+    protected string $szin;
+
+    public function __construct(string $gyarto, string $tipus, string $szin) {
+        $this->gyarto = $gyarto;
+        $this->tipus = $tipus;
+        $this->szin = $szin;
+    }
+
+    public function __get($name): void {
+        if (in_array($name, array_keys(get_object_vars($this)))) {
+            return $this->$name;
+        }
+
+        throw new LogicException("Nem olvasható tulajdonság: $name");
+    }
+}
