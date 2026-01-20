e7479ea586c7b2ac3eb41f0313170998c2096760
VB2007 <admin@vb2007.hu>
2026-01-20 09:52:47 +0100

    added dolgozo to demo

--- 12-2025-2026/_feladat/backend/feladat_2026_01_20_oroklodes/demo_nyilvantartas/demo.php
+++ 12-2025-2026/_feladat/backend/feladat_2026_01_20_oroklodes/demo_nyilvantartas/demo.php
@@ -3,7 +3,11 @@ declare(strict_types=1);
 
 require __DIR__ . "/vendor/autoload.php";
 
+use Neu\Nyilvantartas\Dolgozo;
 use Neu\Nyilvantartas\Szemely;
 
 $szemely = new Szemely("John Pork", DateTime::createFromFormat("Y-m-d", "2002-01-20"));
-echo $szemely;
+echo $szemely . PHP_EOL;
+
+$dolgozo = new Dolgozo("Meowl", DateTime::createFromFormat("Y-m-d", "2026-01-01"), 271000);
+echo $dolgozo . PHP_EOL;
