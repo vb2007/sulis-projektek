986c9b8c789ee8aa281343a456b4bbe6970aefc4
VB2007 <admin@vb2007.hu>
2026-01-20 09:51:29 +0100

    finished dolgozo class

--- 12-2025-2026/_feladat/backend/feladat_2026_01_20_oroklodes/demo_nyilvantartas/src/Neu/Nyilvantartas/Dolgozo.php
+++ 12-2025-2026/_feladat/backend/feladat_2026_01_20_oroklodes/demo_nyilvantartas/src/Neu/Nyilvantartas/Dolgozo.php
@@ -3,11 +3,18 @@
 namespace Neu\Nyilvantartas;
 
 use DateTime;
+use Stringable;
 
-class Dolgozo extends Szemely {
+class Dolgozo extends Szemely implements Stringable {
     private float $fizetes;
 
     public function __construct(string $nev, DateTime $szuletett, float $fizetes) {
         $this->nev = $nev;
+        $this->szuletett = $szuletett;
+        $this->fizetes = $fizetes;
+    }
+
+    public function __tostring(): string {
+        return parent::__toString() . " Fizetés: " . number_format($this->fizetes, 0, ",", " ");
     }
 }
