12dc892958961b8c7223e073b5507e3736b5fce6
VB2007 <admin@vb2007.hu>
2026-01-20 13:59:23 +0100

    added source file

---
+++ 12-2025-2026/_feladat/webprogramozas/feladat_2026_01_20_subjects/subjects.js
@@ -0,0 +1,198 @@
+export default [
+    {
+        code: "BMEVIIIAB12",
+        subject: "Számítógépes grafika",
+        credit: "5",
+    },
+    {
+        code: "BMEVITMAD01",
+        subject: "Információs rendszerek üzemeltetése",
+        credit: "5",
+    },
+    {
+        code: "BMEVIHIAB04",
+        subject: "Kódolástechnika",
+        credit: "4",
+    },
+    {
+        code: "BMEVIMIAA03",
+        subject: "Digitális technika",
+        credit: "5",
+    },
+    {
+        code: "BMETE11AX52",
+        subject: "Fizika alapismeretek",
+        credit: "2",
+    },
+    {
+        code: "BMEVIETAV03",
+        subject: "Vállalati folyamatok modellezése",
+        credit: "2",
+    },
+    {
+        code: "BMEVITMAB06",
+        subject: "Kommunikációs hálózatok",
+        credit: "7",
+    },
+    {
+        code: "BMEGT43A001",
+        subject: "Kommunikáció",
+        credit: "2",
+    },
+    {
+        code: "BMEGT60W61A",
+        subject: "Kommunikációfejlesztés TED előadások alapján",
+        credit: "2",
+    },
+    {
+        code: "BMEVIAUBXAV081-00",
+        subject:
+            "Mesterséges intelligencia alapú szoftverek és szolgáltatások fejlesztése",
+        credit: "4",
+    },
+    {
+        code: "BMEVITMAB04",
+        subject: "Adatbázisok",
+        credit: "5",
+    },
+    {
+        code: "BMEGT35A001",
+        subject: "Pénzügyek",
+        credit: "2",
+    },
+    {
+        code: "BMEGT35A002",
+        subject: "Számvitel",
+        credit: "2",
+    },
+    {
+        code: "BMEGT55A405",
+        subject: "Jogi alapismeretek",
+        credit: "3",
+    },
+    {
+        code: "BMEGT70BS1B",
+        subject: "Testnevelés BSc - 1/B",
+        credit: "0",
+    },
+    {
+        code: "BMEVIETAA00",
+        subject: "Villamos alapismeretek",
+        credit: "3",
+    },
+    {
+        code: "BMEVIAUAV15",
+        subject: "iOS alapú szoftverfejlesztés",
+        credit: "4",
+    },
+    {
+        code: "BMEVIIIAB00",
+        subject: "A programozás alapjai 3",
+        credit: "5",
+    },
+    {
+        code: "BMEGT70BS2B",
+        subject: "Testnevelés BSc - 2/B",
+        credit: "0",
+    },
+    {
+        code: "BMEVISZAA06",
+        subject: "Bevezetés a számításelméletbe 1",
+        credit: "6",
+    },
+    {
+        code: "BMEVIMIAB04",
+        subject: "Szoftvertechnológia",
+        credit: "5",
+    },
+    {
+        code: "BMEGT20A001",
+        subject: "Menedzsment és vállalkozásgazdaságtan",
+        credit: "4",
+    },
+    {
+        code: "BMEVIHIAC07",
+        subject: "IT biztonság",
+        credit: "5",
+    },
+    {
+        code: "BMETE11AX53",
+        subject: "Fizika i",
+        credit: "4",
+    },
+    {
+        code: "BMEVIMIAB03",
+        subject: "Operációs rendszerek",
+        credit: "5",
+    },
+    {
+        code: "BMEGT20V100",
+        subject: "Innovatív vállalkozások indítása és működtetése",
+        credit: "2",
+    },
+    {
+        code: "BMEVIIIAA03",
+        subject: "A programozás alapjai 2",
+        credit: "6",
+    },
+    {
+        code: "BMEVISZAB04",
+        subject: "Valószínűségszámítás és statisztika",
+        credit: "6",
+    },
+    {
+        code: "BMEVISZAA04",
+        subject: "Bevezetés a számításelméletbe 2",
+        credit: "5",
+    },
+    {
+        code: "BMEVIHIAA03",
+        subject: "Számítógép-architektúrák",
+        credit: "5",
+    },
+    {
+        code: "BMEVIAUAA02",
+        subject: "Digitális kompetenciák",
+        credit: "2",
+    },
+    {
+        code: "BMEVISZAA08",
+        subject: "Algoritmuselmélet",
+        credit: "5",
+    },
+    {
+        code: "BMEVIAUAV45",
+        subject: "Flutter alapú szoftverfejlesztés",
+        credit: "2",
+    },
+    {
+        code: "BMETE90AX21",
+        subject: "Analízis 1 informatikusoknak",
+        credit: "6",
+    },
+    {
+        code: "BMEGT30A430",
+        subject: "Mikro- és makroökonómia B",
+        credit: "3",
+    },
+    {
+        code: "BMEVIMIAD03",
+        subject: "Rendszermodellezés",
+        credit: "5",
+    },
+    {
+        code: "BMEVITMAK47",
+        subject: "Mérnöki menedzsment módszerek",
+        credit: "2",
+    },
+    {
+        code: "BMEVIEEAA00",
+        subject: "A programozás alapjai 1",
+        credit: "7",
+    },
+    {
+        code: "BMETE90AX57",
+        subject: "Analízis 2 informatikusoknak",
+        credit: "6",
+    },
+];
