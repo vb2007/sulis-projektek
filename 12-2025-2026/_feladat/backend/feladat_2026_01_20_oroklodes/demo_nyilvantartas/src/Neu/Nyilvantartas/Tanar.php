6da7b1667f8c59a367821b090e5a1f6196d2d2da
VB2007 <admin@vb2007.hu>
2026-01-20 10:01:43 +0100

    finished tanar.php

--- 12-2025-2026/_feladat/backend/feladat_2026_01_20_oroklodes/demo_nyilvantartas/src/Neu/Nyilvantartas/Tanar.php
+++ 12-2025-2026/_feladat/backend/feladat_2026_01_20_oroklodes/demo_nyilvantartas/src/Neu/Nyilvantartas/Tanar.php
@@ -14,7 +14,11 @@ class Tanar extends Dolgozo implements Stringable {
         $this->om = $om;
     }
 
+    public function addTantargy(string $tantargy): void {
+        array_push($this->tantargyak, $tantargy);
+    }
+
     public function __tostring(): string {
-        return parent::__toString() . " Fizetés: " . number_format($this->fizetes, 0, ",", " ") . " Ft";
+        return $this->om . " " . parent::__toString() . "(" . implode(", ", $this->tantargyak) . ")";
     }
 }
