1736f0ccb3d76561c62c2718759d5fc834af8734
VB2007 <admin@vb2007.hu>
2026-01-22 11:33:07 +0100

    implemented demo

--- 12-2025-2026/_feladat/alkalmazasfejlesztes/feladat_2026_01_22_ajandekdoboz/Ajandekdoboz_VB/Ajandekdoboz_VB/Program.cs
+++ 12-2025-2026/_feladat/alkalmazasfejlesztes/feladat_2026_01_22_ajandekdoboz/Ajandekdoboz_VB/Ajandekdoboz_VB/Program.cs
@@ -1,9 +1,22 @@
-﻿namespace Ajandekdoboz_VB;
+﻿using Ajandekdoboz_VB_Lib;
+
+namespace Ajandekdoboz_VB;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        AjandekDoboz<Kozmetikum> kozmetikaiDoboz = new AjandekDoboz<Kozmetikum>();
+
+        kozmetikaiDoboz.Add(new Kozmetikum("Krém", 1990, "DM Sajátmárka"));
+        kozmetikaiDoboz.Add(new Kozmetikum("Szempillaspirál", 2990, "Kyx"));
+
+        AjandekDoboz<Husaru> husaruDoboz = new AjandekDoboz<Husaru>();
+        
+        husaruDoboz.Add(new Husaru("Füstölt sonka", 4990, true));
+        husaruDoboz.Add(new Husaru("Szalámi", 3990, false));
+        
+        Console.WriteLine(kozmetikaiDoboz);
+        Console.WriteLine(husaruDoboz);
     }
 }
