556199a5a09333fcfb6e7a6fb46e1ce292fb9417
VB2007 <admin@vb2007.hu>
2026-01-22 11:42:11 +0100

    added new data

--- 12-2025-2026/_feladat/alkalmazasfejlesztes/feladat_2026_01_22_ajandekdoboz/Ajandekdoboz_VB/Ajandekdoboz_VB/Program.cs
+++ 12-2025-2026/_feladat/alkalmazasfejlesztes/feladat_2026_01_22_ajandekdoboz/Ajandekdoboz_VB/Ajandekdoboz_VB/Program.cs
@@ -10,11 +10,15 @@ class Program
 
         kozmetikaiDoboz.Add(new Kozmetikum("Krém", 1990, "DM Sajátmárka"));
         kozmetikaiDoboz.Add(new Kozmetikum("Szempillaspirál", 2990, "Kyx"));
+        kozmetikaiDoboz.Add(new Kozmetikum("Parfüm", 9990, "Adidas"));
+        kozmetikaiDoboz.Add(new Kozmetikum("Testápoló", 2490, "Nivea"));
 
         AjandekDoboz<Husaru> husaruDoboz = new AjandekDoboz<Husaru>();
         
         husaruDoboz.Add(new Husaru("Füstölt sonka", 4990, true));
         husaruDoboz.Add(new Husaru("Szalámi", 3990, false));
+        husaruDoboz.Add(new Husaru("Kolbász", 2990, true));
+        husaruDoboz.Add(new Husaru("Párizsi", 1990, false));
         
         Console.WriteLine(kozmetikaiDoboz);
         Console.WriteLine(husaruDoboz);
