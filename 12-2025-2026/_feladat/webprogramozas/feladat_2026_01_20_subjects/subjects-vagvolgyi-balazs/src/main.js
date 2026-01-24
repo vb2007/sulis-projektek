0d0c58135640f373e347f9996f3ddb57fe174972
VB2007 <admin@vb2007.hu>
2026-01-24 17:40:02 +0100

    implemented header creation in js

--- 12-2025-2026/_feladat/webprogramozas/feladat_2026_01_20_subjects/subjects-vagvolgyi-balazs/src/main.js
+++ 12-2025-2026/_feladat/webprogramozas/feladat_2026_01_20_subjects/subjects-vagvolgyi-balazs/src/main.js
@@ -1,1 +1,14 @@
 import "./app.css";
+import subjects from "./data/subjects.js";
+
+const body = document.querySelector("body");
+
+const header = document.createElement("header");
+header.classList.add("bg-[#931634]");
+
+const h1 = document.createElement("h1");
+h1.classList.add("text-white", "text-6xl", "font-light", "py-12", "pl-8");
+h1.textContent = "Felvett tárgyak listája";
+
+header.append(h1);
+body.prepend(header);
