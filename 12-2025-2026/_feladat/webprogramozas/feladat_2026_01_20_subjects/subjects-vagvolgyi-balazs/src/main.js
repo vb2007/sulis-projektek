5a171319af8b44a29fb3b671020297052cf0dc5f
VB2007 <admin@vb2007.hu>
2026-01-24 18:34:08 +0100

    implemented rendering & sorting

--- 12-2025-2026/_feladat/webprogramozas/feladat_2026_01_20_subjects/subjects-vagvolgyi-balazs/src/main.js
+++ 12-2025-2026/_feladat/webprogramozas/feladat_2026_01_20_subjects/subjects-vagvolgyi-balazs/src/main.js
@@ -12,3 +12,70 @@ h1.textContent = "Felvett tárgyak listája";
 
 header.append(h1);
 body.prepend(header);
+
+const tbody = document.querySelector("tbody");
+const thName = document.querySelector("th")[1];
+const thCredit = document.querySelector("th")[2];
+
+let sortDirName = "asc";
+let sortDirCredit = "asc";
+
+const renderTable = () => {
+    tbody.innerHTML = "";
+
+    //params: jelenlegi subject elem, index
+    subjects.forEach((subj, i) => {
+        const tr = document.createElement("tr");
+
+        if (i != subjects.length - 1) {
+            tr.classList.add("border-b-2", "border-solid", "border-[#931634]");
+        }
+
+        tr.classList.add("hover:bg-black/10");
+
+        const tdCode = document.createElement("td");
+        tdCode.textContent = subj.code;
+        tdCode.classList.add("p-2");
+
+        const tdSubject = document.createElement("td");
+        tdSubject.textContent = subj.subject;
+        tdSubject.classList.add("p-2", "font-bold");
+
+        const tdCredit = document.createElement("td");
+        tdCredit.textContent = subj.credit;
+        tdCredit.classList.add("p-2");
+
+        tr.append(tdCode, tdSubject, tdCredit);
+        tbody.append(tr);
+    });
+};
+
+thName.addEventListener("click", () => {
+    subjects.sort((a, b) => {
+        if (sortDirName === "asc") {
+            return a.subject.localeCompare(b.subject);
+        } else {
+            return b.subject.localeCompare(a.subject);
+        }
+    });
+
+    sortDirName = sortDirName === "asc" ? "desc" : "asc";
+
+    renderTable();
+});
+
+thCredit.addEventListener("click", () => {
+    subjects.sort((a, b) => {
+        if (sortDirCredit === "asc") {
+            return parseInt(a.credit) - parseInt(b.credit);
+        } else {
+            return parseInt(b.credit) - parseInt(a.credit);
+        }
+    });
+
+    sortDirCredit = sortDirCredit === "asc" ? "desc" : "asc";
+
+    renderTable();
+});
+
+renderTable();
