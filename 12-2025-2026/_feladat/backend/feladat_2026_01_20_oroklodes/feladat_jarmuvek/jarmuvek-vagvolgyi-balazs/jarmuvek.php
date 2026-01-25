1dd3cb8c323b08620b486be718e59597d2b731e8
VB2007 <admin@vb2007.hu>
2026-01-25 02:06:49 +0100

    fixed str_ends_with function call

--- 12-2025-2026/_feladat/backend/feladat_2026_01_20_oroklodes/feladat_jarmuvek/jarmuvek-vagvolgyi-balazs/jarmuvek.php
+++ 12-2025-2026/_feladat/backend/feladat_2026_01_20_oroklodes/feladat_jarmuvek/jarmuvek-vagvolgyi-balazs/jarmuvek.php
@@ -20,7 +20,8 @@ if (!in_array(strtolower($type), $acceptedTypes)) {
 }
 
 $output = $argv[2];
-if (!str_ends_with(".csv", $output)) {
+//mi a tökömért kell az in_array-ben első paraméterben $needle-t először, $haystack-et másodszor megadni, a str_ends_with-ben meg fordítva??
+if (!str_ends_with($output, ".csv")) {
     echo "Érvénytelen kimeneti fájlkiterjesztés, csak a .csv elfogadott!" . PHP_EOL;
     exit(271);
 }
