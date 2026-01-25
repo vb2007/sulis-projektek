8c38a1fb55beb4120fff48931f3b338f50305bd7
VB2007 <admin@vb2007.hu>
2026-01-25 02:04:23 +0100

    fixed references in $argv

--- 12-2025-2026/_feladat/backend/feladat_2026_01_20_oroklodes/feladat_jarmuvek/jarmuvek-vagvolgyi-balazs/jarmuvek.php
+++ 12-2025-2026/_feladat/backend/feladat_2026_01_20_oroklodes/feladat_jarmuvek/jarmuvek-vagvolgyi-balazs/jarmuvek.php
@@ -12,14 +12,14 @@ if ($argc > 3) {
     exit(271);
 }
 
-$type = $argv[0];
+$type = $argv[1];
 $acceptedTypes = ["auto", "busz", "roller"];
 if (!in_array(strtolower($type), $acceptedTypes)) {
     echo "Érvénytelen járműtípus!" . PHP_EOL;
     exit(271);
 }
 
-$output = $argv[1];
+$output = $argv[2];
 if (!str_ends_with(".csv", $output)) {
     echo "Érvénytelen kimeneti fájlkiterjesztés, csak a .csv elfogadott!" . PHP_EOL;
     exit(271);
