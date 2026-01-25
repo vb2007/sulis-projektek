d0c821fef8b26116dee6a29aa1186a5ba8f9e516
VB2007 <admin@vb2007.hu>
2026-01-25 01:49:36 +0100

    created & implemented class auto

---
+++ 12-2025-2026/_feladat/backend/feladat_2026_01_20_oroklodes/feladat_jarmuvek/jarmuvek-vagvolgyi-balazs/src/Acme/Kozlekedes/Auto.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Acme\Kozlekedes;
+
+use LogicException;
+
+class Auto extends Gepjarmu {
+    protected int $ajtok;
+
+    protected function __construct(string $gyarto, string $tipus, string $szin, string $motor, int $ajtok) {
+        parent::__construct($gyarto, $tipus, $szin, $motor);
+        $this->ajtok = $ajtok;
+    }
+
+    public function __get($name): void {
+        if (in_array($name, array_keys(get_object_vars($this)))) {
+            return $this->$name;
+        }
+
+        throw new LogicException("Nem olvasható tulajdonság: $name");
+    }
+
+    public function __tostring(): string {
+        return "";
+    }
+}
--- 12-2025-2026/_feladat/backend/feladat_2026_01_20_oroklodes/feladat_jarmuvek/jarmuvek-vagvolgyi-balazs/src/Acme/Kozlekedes/Gepjarmu.php
+++ 12-2025-2026/_feladat/backend/feladat_2026_01_20_oroklodes/feladat_jarmuvek/jarmuvek-vagvolgyi-balazs/src/Acme/Kozlekedes/Gepjarmu.php
@@ -7,7 +7,7 @@ use LogicException;
 class Gepjarmu extends Jarmu {
     protected string $motor;
 
-    protected function __construct(string $gyarto, string $tipus, string $szin, $motor) {
+    protected function __construct(string $gyarto, string $tipus, string $szin, string $motor) {
         parent::__construct($gyarto, $tipus, $szin);
         $this->motor = $motor;
     }
