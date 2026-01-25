7012d06480879a594334107de14ef4faa30e5dfd
VB2007 <admin@vb2007.hu>
2026-01-25 02:00:21 +0100

    implemented autoload, imports & input validation in demo file

--- 12-2025-2026/_feladat/backend/feladat_2026_01_20_oroklodes/feladat_jarmuvek/jarmuvek-vagvolgyi-balazs/jarmuvek.php
+++ 12-2025-2026/_feladat/backend/feladat_2026_01_20_oroklodes/feladat_jarmuvek/jarmuvek-vagvolgyi-balazs/jarmuvek.php
@@ -1,1 +1,26 @@
 <?php
+declare(strict_types=1);
+
+require __DIR__ . "/vendor/autoload.php";
+
+use Acme\Kozlekedes\Auto;
+use Acme\Kozlekedes\Busz;
+use Acme\Kozlekedes\Roller;
+
+if ($argc > 3) {
+    echo "Túl sok paraméter!";
+    exit(271);
+}
+
+$type = $argv[0];
+$acceptedTypes = ["auto", "busz", "roller"];
+if (!in_array(strtolower($type), $acceptedTypes)) {
+    echo "Érvénytelen járműtípus!";
+    exit(271);
+}
+
+$output = $argv[1];
+if (!str_ends_with(".csv", $output)) {
+    echo "Érvénytelen kimeneti fájlkiterjesztés, csak a .csv elfogadott!";
+    exit(271);
+}
