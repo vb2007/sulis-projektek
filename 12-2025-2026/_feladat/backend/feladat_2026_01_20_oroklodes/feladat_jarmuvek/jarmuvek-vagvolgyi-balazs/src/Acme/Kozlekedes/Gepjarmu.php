87885db356b02ec0298c2d776dc863f51b3ec7e3
VB2007 <admin@vb2007.hu>
2026-01-25 01:45:53 +0100

    finished gepjarmu class

--- 12-2025-2026/_feladat/backend/feladat_2026_01_20_oroklodes/feladat_jarmuvek/jarmuvek-vagvolgyi-balazs/src/Acme/Kozlekedes/Gepjarmu.php
+++ 12-2025-2026/_feladat/backend/feladat_2026_01_20_oroklodes/feladat_jarmuvek/jarmuvek-vagvolgyi-balazs/src/Acme/Kozlekedes/Gepjarmu.php
@@ -2,11 +2,21 @@
 
 namespace Acme\Kozlekedes;
 
+use LogicException;
+
 class Gepjarmu extends Jarmu {
     protected string $motor;
 
     protected function __construct(string $gyarto, string $tipus, string $szin, $motor) {
         parent::__construct($gyarto, $tipus, $szin);
         $this->motor = $motor;
+    }
+
+    public function __get($name): void {
+        if (in_array($name, array_keys(get_object_vars($this)))) {
+            return $this->$name;
+        }
+
+        throw new LogicException("Nem olvasható tulajdonság: $name");
     }
 }
