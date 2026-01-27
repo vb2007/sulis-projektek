2c56ddbc1bdc2718ad0d1fbcc4f195a877749178
VB2007 <admin@vb2007.hu>
2026-01-27 13:44:36 +0100

    added task description, samples and sources

---
+++ 12-2025-2026/_feladat/webprogramozas/feladat_2026_01_27_car_owners/forras/persons.js
@@ -0,0 +1,5 @@
+const persons = [
+  { id: 1, first_name: "Alex", last_name: "Morgan", email_address: "alex.morgan@example.com" },
+  { id: 2, first_name: "Sam", last_name: "Lee", email_address: "sam.lee@example.com" },
+  { id: 3, first_name: "Pat", last_name: "Kim", email_address: "pat.kim@example.com" },
+];
