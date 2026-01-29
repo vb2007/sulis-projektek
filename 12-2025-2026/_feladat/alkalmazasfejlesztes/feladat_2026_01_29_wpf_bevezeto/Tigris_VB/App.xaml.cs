1f00b54834b880d8bc94614f12f2e63327074207
VB2007 <admin@vb2007.hu>
2026-01-29 12:40:48 +0100

    backed up completed project

---
+++ 12-2025-2026/_feladat/alkalmazasfejlesztes/feladat_2026_01_29_wpf_bevezeto/Tigris_VB/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Tigris_VB
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
