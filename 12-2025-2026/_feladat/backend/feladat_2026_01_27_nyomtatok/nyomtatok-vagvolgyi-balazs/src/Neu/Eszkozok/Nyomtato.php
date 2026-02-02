1af98dd186a384785a596697fde881324567fa04
VB2007 <admin@vb2007.hu>
2026-02-02 22:23:33 +0100

    implemented nyomtato class

--- 12-2025-2026/_feladat/backend/feladat_2026_01_27_nyomtatok/nyomtatok-vagvolgyi-balazs/src/Neu/Eszkozok/Nyomtato.php
+++ 12-2025-2026/_feladat/backend/feladat_2026_01_27_nyomtatok/nyomtatok-vagvolgyi-balazs/src/Neu/Eszkozok/Nyomtato.php
@@ -2,3 +2,31 @@
 
 namespace Neu\Eszkozok;
 
+use LogicException;
+
+class Nyomtato {
+    protected string $gyarto;
+    protected string $tipus;
+    protected bool $szines;
+    protected int $ar;
+    private static array $gyartok = [];
+
+    public function __construct(string $gyarto, string $tipus, bool $szines, int $ar) {
+        $this->gyarto = $gyarto;
+        $this->tipus = $tipus;
+        $this->szines = $szines;
+        $this->ar = $ar;
+    }
+
+    public function getGyartok(): array {
+        return $this->gyartok;
+    }
+
+    public function __get($name): mixed {
+        if (in_array($name, array_keys(get_object_vars($this)))) {
+            return $this->$name;
+        }
+
+        throw new LogicException("Nem olvasható tulajdonság: $name");
+    }
+}
