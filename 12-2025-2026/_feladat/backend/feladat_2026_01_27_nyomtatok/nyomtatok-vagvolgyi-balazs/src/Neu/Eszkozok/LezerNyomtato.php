6160f7dbf3989a11056bdd2012c81a40be3e37c9
VB2007 <admin@vb2007.hu>
2026-02-02 22:36:25 +0100

    created LezerNyomtato.php class

---
+++ 12-2025-2026/_feladat/backend/feladat_2026_01_27_nyomtatok/nyomtatok-vagvolgyi-balazs/src/Neu/Eszkozok/LezerNyomtato.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Neu\Eszkozok;
+
+use LogicException;
+
+class LezerNyomtato extends Nyomtato {
+    protected int $tonerekSzama;
+
+    public function __construct(string $gyarto, string $tipus, bool $szines, int $ar, int $tonerekSzama) {
+        parent::__construct($gyarto, $tipus, $szines, $ar);
+        $this->tonerekSzama = $tonerekSzama;
+    }
+
+    public function __get($name): mixed {
+        if (in_array($name, array_keys(get_object_vars($this)))) {
+            return $this->$name;
+        }
+
+        throw new LogicException("Nem olvasható tulajdonság: $name");
+    }
+}
