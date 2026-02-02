4eb8bb9d38a890b45f7f2e3d1e14baacdbe9beb2
VB2007 <admin@vb2007.hu>
2026-02-02 22:34:52 +0100

    implemented toArray

--- 12-2025-2026/_feladat/backend/feladat_2026_01_27_nyomtatok/nyomtatok-vagvolgyi-balazs/src/Neu/Eszkozok/TintasugarasNyomtato.php
+++ 12-2025-2026/_feladat/backend/feladat_2026_01_27_nyomtatok/nyomtatok-vagvolgyi-balazs/src/Neu/Eszkozok/TintasugarasNyomtato.php
@@ -23,4 +23,14 @@ class TintasugarasNyomtato extends Nyomtato {
     public function __toString(): string {
         return $this->gyarto . " " . $this->tipus . " " . ($this->szines ? "színes" : "fekete-fehér") . " tintasugaras nyomtató (" . $this->patronokSzama . " patron) ". $this->ar . " Ft";
     }
+
+    public function toArray(): array {
+        return [
+            'gyarto' => $this->gyarto,
+            'tipus' => $this->tipus,
+            'szines' => $this->szines,
+            'ar' => $this->ar,
+            'patronokSzama' => $this->patronokSzama
+        ];
+    }
 }
