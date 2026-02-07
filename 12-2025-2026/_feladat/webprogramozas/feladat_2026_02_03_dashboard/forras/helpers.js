4d49fd3deb9c723d3a5f68ccac3b7277d0b01aa3
VB2007 <admin@vb2007.hu>
2026-02-07 22:36:00 +0100

    updated formatting

--- 12-2025-2026/_feladat/webprogramozas/feladat_2026_02_03_dashboard/forras/helpers.js
+++ 12-2025-2026/_feladat/webprogramozas/feladat_2026_02_03_dashboard/forras/helpers.js
@@ -3,6 +3,6 @@
  * @param {string} emoji The emoji to be converted in te format: `U+hex`
  * @returns {string} The converted string
  */
-export function formatEmoji(emoji) {
+export const formatEmoji = (emoji) => {
     return String.fromCodePoint(parseInt(emoji.replace("U+", ""), 16));
-}
+};
