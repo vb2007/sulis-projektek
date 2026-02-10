f8f7633a4b0e29988fa9d47729602f9c28729164
VB2007 <admin@vb2007.hu>
2026-02-10 12:14:38 +0100

    moved main to games.php

--- 12-2025-2026/_feladat/backend/feladat_2026_02_10_konzolos_jatekok/vagvolgyi-balazs-console-games/www/index.php
+++ 12-2025-2026/_feladat/backend/feladat_2026_02_10_konzolos_jatekok/vagvolgyi-balazs-console-games/www/index.php
@@ -25,34 +25,7 @@ ob_start();
         </style>
     </head>
     <body>
-        <main class="mx-auto max-w-7xl px-4 py-6">
-            <h1 class="mb-6 text-4xl font-bold text-center"><?= $title ?></h1>
 
-            <section id="games" class="grid grid-cols-1 gap-6">
-                <div
-                    class="grid grid-rows-[auto_1fr_auto] rounded-2xl bg-white p-4 shadow-lg"
-                >
-                    <img
-                        src="https://picsum.photos/id/223/260/260"
-                        class="w-full"
-                    />
-
-                    <h2 class="text-base font-semibold">Cím</h2>
-
-                    <p
-                        class="flex items-center justify-between flex-wrap text-sm"
-                    >
-                        <span class="whitespace-nowrap"> Ár: 15 990 Ft </span>
-
-                        <span
-                            class="rounded-full px-4 py-1 text-xs font-semibold whitespace-nowrap"
-                        >
-                            PS5
-                        </span>
-                    </p>
-                </div>
-            </section>
-        </main>
     </body>
 </html>
 
