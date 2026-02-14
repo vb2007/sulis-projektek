9a451b29b1d24968293ed0ccdc42e509d393201b
Balázs Vágvölgyi <balazs.vagvolgyi@linistry.com>
2026-02-14 22:21:23 +0100

    created testdata, removed auto generated test class

---
+++ 12-2025-2026/_feladat/teszteles/feladat_2026_02_10_selenium_webteszt/Webteszt_VB/Webteszt_VB/TestData.cs
@@ -0,0 +1,12 @@
+﻿namespace Webteszt_VB;
+
+public class TestData
+{
+    public static string _baseUrl = "https://allatmenhely-ikt.vb2007.hu";
+
+    public class HTMLAttributes
+    {
+        public static string Language = "hu";
+        public static string Encoding = "UTF-8";
+    }
+}
