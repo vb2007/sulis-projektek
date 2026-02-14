c25695d02997f7cda7904c7c7ace3682a3a8ee1b
Balázs Vágvölgyi <balazs.vagvolgyi@linistry.com>
2026-02-14 23:18:45 +0100

    created base for RegisterTest class

--- 12-2025-2026/_feladat/teszteles/feladat_2026_02_10_selenium_webteszt/Webteszt_VB/Webteszt_VB/Tests/UserManagement/RegisterTest.cs
+++ 12-2025-2026/_feladat/teszteles/feladat_2026_02_10_selenium_webteszt/Webteszt_VB/Webteszt_VB/Tests/UserManagement/RegisterTest.cs
@@ -1,7 +1,29 @@
-﻿
+﻿using Webteszt_VB.Helpers;
+using OpenQA.Selenium;
 
 namespace Webteszt_VB.Tests.UserManagement;
 
+[TestFixture]
+[Parallelizable(ParallelScope.Self)]
 public class RegisterTest
 {
+    private static Browser _browser;
+    private static WebDriver _webDriver => _browser.WebDriver;
+
+    [SetUp]
+    public void Setup()
+    {
+        ExcelReportGenerator.StartTest(TestContext.CurrentContext.Test.FullName);
+
+        _browser = new Browser();
+        _browser.Init();
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        _browser?.Teardown();
+
+        ExcelReportGenerator.EndTest();
+    }
 }
