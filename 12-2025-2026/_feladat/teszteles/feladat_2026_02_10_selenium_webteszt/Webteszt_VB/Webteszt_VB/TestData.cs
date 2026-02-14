fc0d8f0345002f2b42ffceaea4f7e1343dc574ab
Balázs Vágvölgyi <balazs.vagvolgyi@linistry.com>
2026-02-14 22:41:49 +0100

    added crucial data to TestData class

--- 12-2025-2026/_feladat/teszteles/feladat_2026_02_10_selenium_webteszt/Webteszt_VB/Webteszt_VB/TestData.cs
+++ 12-2025-2026/_feladat/teszteles/feladat_2026_02_10_selenium_webteszt/Webteszt_VB/Webteszt_VB/TestData.cs
@@ -4,9 +4,17 @@ public class TestData
 {
     public static string _baseUrl = "https://allatmenhely-ikt.vb2007.hu";
 
-    public class HTMLAttributes
+    public static string BrowserTitle = "Főoldal - Állatmenhely";
+    public static string HeaderTitle = "Állatmenhely digitális kezelőfelület";
+
+    public class NavbarData
     {
-        public static string Language = "hu";
-        public static string Encoding = "UTF-8";
+        public static string NavbarTitle = "Allatmenhely.WebApp";
+
+        public static string HomeLinkText = "Főoldal";
+        public static string AnimalsLinkText = "Állatok";
+
+        public static string LoginButtonText = "Bejelentkezés";
+        public static string RegisterButtonText = "Regisztráció";
     }
 }
