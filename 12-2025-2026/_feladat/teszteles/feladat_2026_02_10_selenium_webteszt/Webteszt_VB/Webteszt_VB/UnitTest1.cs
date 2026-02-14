82a42a796b52e987a15b0da5b2e5ed0b93ec94aa
Balázs Vágvölgyi <balazs.vagvolgyi@linistry.com>
2026-02-14 22:13:11 +0100

    created test project

---
+++ 12-2025-2026/_feladat/teszteles/feladat_2026_02_10_selenium_webteszt/Webteszt_VB/Webteszt_VB/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿namespace Webteszt_VB
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
