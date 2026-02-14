e7560d53f029a32b9ee83e20c80199afee564d87
Balázs Vágvölgyi <balazs.vagvolgyi@linistry.com>
2026-02-14 22:31:03 +0100

    created base functions for HomepageTest class

--- 12-2025-2026/_feladat/teszteles/feladat_2026_02_10_selenium_webteszt/Webteszt_VB/Webteszt_VB/Tests/StaticContent/HomepageTest.cs
+++ 12-2025-2026/_feladat/teszteles/feladat_2026_02_10_selenium_webteszt/Webteszt_VB/Webteszt_VB/Tests/StaticContent/HomepageTest.cs
@@ -1,6 +1,29 @@
-﻿
+﻿using Webteszt_VB.Helpers;
+using OpenQA.Selenium;
+
 namespace Webteszt_VB.Tests.StaticContent;
 
+[TestFixture]
+[Parallelizable(ParallelScope.Self)]
 public class HomepageTest
 {
+    private static Browser _browser;
+    private static WebDriver _webDriver => _browser.WebDriver;
+
+    [SetUp]
+    public void Setup()
+    {
+        ExcelReportGenerator.StartTest(TestContext.CurrentContext.Test.FullName);
+
+        _browser = new Browser();
+        _browser.Init();
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        _browser?.Teardown();
+
+        ExcelReportGenerator.EndTest();
+    }
 }
