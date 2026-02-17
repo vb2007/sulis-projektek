a72d9140ae9b520fc9116cef1295dd05c692e0f3
VB2007 <admin@vb2007.hu>
2026-02-17 12:38:30 +0100

    implemented footer

---
+++ 12-2025-2026/_feladat/backend/feladat_2026_02_17_usa_lovak/vagvolgyi-balazs-usa-lovak/www/components/footer.php
@@ -0,0 +1,8 @@
+<footer class="mt-10 bg-blue-700 text-teal-50">
+    <div
+        class="mx-auto max-w-7xl px-4 py-6 text-center flex place-content-between"
+    >
+        <p>Készítette: TELJES_NÉV</p>
+        <p>© AKTUÁLIS_ÉV USA lovak</p>
+    </div>
+</footer>
