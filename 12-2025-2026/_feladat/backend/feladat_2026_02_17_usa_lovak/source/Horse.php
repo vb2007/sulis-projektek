ea07dec79d0a7cc622ed5e0152b7705429db6e62
VB2007 <admin@vb2007.hu>
2026-02-17 09:56:03 +0100

    added sorce files

---
+++ 12-2025-2026/_feladat/backend/feladat_2026_02_17_usa_lovak/source/Horse.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace Acme\Animal;
+
+class Horse {
+    private int $id;                // azonosító
+    private string $state;          // állam
+    private string $breed;          // fajta
+    private string $description;    // leírás
+    private string $image;          // kép
+    private int $year;              // év
+
+    private static array $states = [
+        "AL"=>"Alabama",
+        "AK"=>"Alaska",
+        "AZ"=>"Arizona",
+        "AR"=>"Arkansas",
+        "CA"=>"California",
+        "CO"=>"Colorado",
+        "CT"=>"Connecticut",
+        "DE"=>"Delaware",
+        "FL"=>"Florida",
+        "GA"=>"Georgia",
+        "HI"=>"Hawaii",
+        "ID"=>"Idaho",
+        "IL"=>"Illinois",
+        "IN"=>"Indiana",
+        "IA"=>"Iowa",
+        "KS"=>"Kansas",
+        "KY"=>"Kentucky",
+        "LA"=>"Louisiana",
+        "ME"=>"Maine",
+        "MD"=>"Maryland",
+        "MA"=>"Massachusetts",
+        "MI"=>"Michigan",
+        "MN"=>"Minnesota",
+        "MS"=>"Mississippi",
+        "MO"=>"Missouri",
+        "MT"=>"Montana",
+        "NE"=>"Nebraska",
+        "NV"=>"Nevada",
+        "NH"=>"New Hampshire",
+        "NJ"=>"New Jersey",
+        "NM"=>"New Mexico",
+        "NY"=>"New York",
+        "NC"=>"North Carolina",
+        "ND"=>"North Dakota",
+        "OH"=>"Ohio",
+        "OK"=>"Oklahoma",
+        "OR"=>"Oregon",
+        "PA"=>"Pennsylvania",
+        "RI"=>"Rhode Island",
+        "SC"=>"South Carolina",
+        "SD"=>"South Dakota",
+        "TN"=>"Tennessee",
+        "TX"=>"Texas",
+        "UT"=>"Utah",
+        "VT"=>"Vermont",
+        "VA"=>"Virginia",
+        "WA"=>"Washington",
+        "WV"=>"West Virginia",
+        "WI"=>"Wisconsin",
+        "WY"=>"Wyoming",
+    ];
+
+    
+    public function __construct($id, $state, $breed, $description, $image, $year)
+    {
+        $this->id = $id;
+        $this->state = $state;
+        $this->breed = $breed;
+        $this->description = $description;
+        $this->image = $image;
+        $this->year = $year;
+    }
+
+    public function __get(string $name) :mixed
+    {
+        if(property_exists($this,$name))
+        {
+            return $this->$name;
+        }
+        return null;
+    }
+
+    public function __set(string $name, mixed $value):void
+    {
+        if(property_exists($this,$name))
+        {
+            $this->$name = $value;
+        }
+    }
+
+
+    public static function getStates():array
+    {
+        return self::$states;
+    }
+
+}
---
+++ 12-2025-2026/_feladat/backend/feladat_2026_02_17_usa_lovak/source/data.php
@@ -0,0 +1,23 @@
+<?php
+
+
+
+use Acme\Animal\Horse;
+
+$horses = [
+ new Horse("1","Alabama","Racking Horse","The Racking Horse is a breed of horse known for its ambling gait. The breed association is headquartered in Decatur, Alabama.","1024px-Horseicon.svg.png","1975"),
+ new Horse("2","Florida","Florida Cracker Horse","The Florida Cracker Horse was first brought to what is now Florida in the 1500s by Spanish explorers, and it played a large part in the development of the state's cattle and general agriculture industries.","Remington_A_cracker_cowboy.jpg","2008"),
+ new Horse("3","Idaho","Appaloosa","The Appaloosa has made a substantial contribution to Idaho history, mainly through its association with the Nez Perce Indian tribe.","THIEL_619.jpg","1975"),
+ new Horse("4","Kentucky","Thoroughbred","The Thoroughbred is the center of a multi-billion dollar breeding and racing industry in Kentucky.","Horseracing_Churchill_Downs.jpg","1996"),
+ new Horse("5","Maryland","Thoroughbred","Maryland has a long history of breeding and racing Thoroughbreds, and today maintains an extensive network of breeding farms, training centers and racecourses.","Horse-racing-4.jpg","2003"),
+ new Horse("6","Massachusetts","Morgan","The foundation sire of the Morgan breed, named Figure, was born in West Springfield, Massachusetts in 1789.","American_Morgan_Horse_cropped.jpg","1970"),
+ new Horse("7","Missouri","Missouri Fox Trotter","The Fox Trotter is a gaited breed developed in the Missouri Ozarks.","Quick_Trigger_Missouri_Fox_Trotter.jpg","2002"),
+ new Horse("8","New Jersey","Horse (state animal)","As of the designation of the horse as the state animal, New Jersey contained over 4,500 horse farms housing almost 40,000 horses and played host to a horse industry that extensively contributed to the preservation of natural lands in the state.","Mangalarga_Marchador.jpg","1977"),
+ new Horse("9","North Carolina","Colonial Spanish Mustang","This state breed references the Banker horse of the Outer Banks, descended from Spanish stock.","SpanishMustangsOfCorolla.jpg","2010"),
+ new Horse("10","North Dakota","Nokota (honorary equine)","The Nokota developed in the badlands of southwestern North Dakota, and are named after the Nakota Indian tribe that inhabited the area.","Nokota_Horses.jpg","1993"),
+ new Horse("11","South Carolina","Carolina Marsh Tacky","The Marsh Tacky was developed in the swampy Low Country region of South Carolina, and has played an integral part in the state's history.","Carolina_Marsh_Tacky.jpg","2010"),
+ new Horse("12","Tennessee","Tennessee Walking Horse","The Tennessee Walker is a gaited breed initially developed in middle Tennessee.","MiltonHorseShow.jpg","2000"),
+ new Horse("13","Texas","American Quarter Horse","The history of the Quarter Horse is closely intertwined with that of Texas, where the breed was used for ranching and racing. The American Quarter Horse Association is headquartered in Amarillo, Texas.","Buckskin_mare_left_flank.jpg","2009"),
+ new Horse("14","Vermont","Morgan (state animal)","The Morgan breed was developed mainly in Vermont, where the founding stallion, Figure, lived most of his life and died in 1821.","American_Morgan_Horse_cropped.jpg","1961"),
+
+];
