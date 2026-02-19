5f6b72805350c7cc2ae1dc8ddb50701b81990b36
VB2007 <admin@vb2007.hu>
2026-02-19 08:57:14 +0100

    implemented card.js

--- 12-2025-2026/_feladat/webprogramozas/feladat_2026_02_17_aruhaz/vagvolgyi-balazs-aruhaz/web/src/js/cards.js
+++ 12-2025-2026/_feladat/webprogramozas/feladat_2026_02_17_aruhaz/vagvolgyi-balazs-aruhaz/web/src/js/cards.js
@@ -1,1 +1,48 @@
-export const generateProductCard = (product) => {};
+import { getProduct } from "./products.js";
+import { openProductDialog } from "./dialogs.js";
+
+export const generateProductCard = (product) => {
+    const template = document.getElementById("product-card");
+    const card = template.content.cloneNode(true);
+
+    const image = card.querySelector(".image");
+    image.src = product.image;
+    image.alt = product.name;
+
+    card.querySelector(".name").textContent = product.name;
+    card.querySelector(".volume").textContent = product.volume;
+
+    card.querySelector(".price").textContent = product.price.toLocaleString(
+        "de-DE",
+        {
+            style: "currency",
+            currency: "EUR",
+        },
+    );
+
+    const baseUnit = card.querySelector(".base-unit");
+    const unitSpan = baseUnit.querySelector(".unit");
+    const priceSpan = baseUnit.querySelector(".price");
+
+    unitSpan.textContent = product.baseUnitPrice.unit;
+
+    if (product.baseUnitPrice.price !== null) {
+        priceSpan.textContent = product.baseUnitPrice.price.toLocaleString(
+            "de-DE",
+            {
+                style: "currency",
+                currency: "EUR",
+            },
+        );
+    } else {
+        priceSpan.textContent = "";
+    }
+
+    const button = card.querySelector("button");
+    button.addEventListener("click", async () => {
+        const freshProduct = await getProduct(product.id);
+        openProductDialog(freshProduct);
+    });
+
+    return card;
+};
