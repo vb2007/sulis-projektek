5c8dd46b88b57ccf1c231bd296cdefa72bebfbf8
VB2007 <admin@vb2007.hu>
2026-02-19 09:04:37 +0100

    implemented product loading in main.js

--- 12-2025-2026/_feladat/webprogramozas/feladat_2026_02_17_aruhaz/vagvolgyi-balazs-aruhaz/web/src/main.js
+++ 12-2025-2026/_feladat/webprogramozas/feladat_2026_02_17_aruhaz/vagvolgyi-balazs-aruhaz/web/src/main.js
@@ -1,1 +1,19 @@
 import "@assets/app.css";
+import { getProducts } from "./js/products.js";
+import { generateProductCard } from "./js/cards.js";
+
+const generateProductCards = (products) => {
+    const productsGrid = document.getElementById("products-grid");
+    products.forEach((product) => {
+        const card = generateProductCard(product);
+        productsGrid.appendChild(card);
+    });
+};
+
+getProducts()
+    .then((products) => {
+        generateProductCards(products);
+    })
+    .catch((error) => {
+        console.error("Hiba a termékek betöltése közben:", error);
+    });
