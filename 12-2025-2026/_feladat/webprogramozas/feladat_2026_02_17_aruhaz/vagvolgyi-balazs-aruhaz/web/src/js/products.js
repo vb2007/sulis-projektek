fa858771590b3d3f01f087d05554a28db7ef2cea
VB2007 <admin@vb2007.hu>
2026-02-19 08:47:03 +0100

    fixed products.js

--- 12-2025-2026/_feladat/webprogramozas/feladat_2026_02_17_aruhaz/vagvolgyi-balazs-aruhaz/web/src/js/products.js
+++ 12-2025-2026/_feladat/webprogramozas/feladat_2026_02_17_aruhaz/vagvolgyi-balazs-aruhaz/web/src/js/products.js
@@ -1,10 +1,16 @@
 const BASE_URL = "http://localhost:8888";
 
 export const getProducts = async () => {
-    return await fetch(`${BASE_URL}/products`);
+    return await fetch(`${BASE_URL}/products`, {
+        method: "GET",
+        headers: { Accept: "application/json" },
+    }).then((res) => res.json());
 };
 
 export const getProduct = async (id) => {
-    const products = await fetch(`${BASE_URL}/products`);
+    const products = await fetch(`${BASE_URL}/products`, {
+        method: "GET",
+        headers: { Accept: "application/json" },
+    }).then((res) => res.json());
     return products.find((p) => p.id == id);
 };
