7212357be456073db848b592bb13a7db9b1c9591
VB2007 <admin@vb2007.hu>
2026-02-23 02:45:08 +0100

    reamed classes, updated base functions

---
+++ 12-2025-2026/_feladat/teszteles/feladat_2026_02_17_onkiszolgalo_kassza/OnkiszolgaloKassza_VB/OnkiszolgaloKassza_VB_Lib/CashRegister.cs
@@ -0,0 +1,34 @@
+﻿namespace OnkiszolgaloKassza_VB_Lib;
+
+public class CashRegister
+{
+    public static void StartNewSession()
+    {
+        
+    }
+
+    public static void ScanItem()
+    {
+        
+    }
+    
+    public static void RemoveItem()
+    {
+        
+    }
+
+    public static int FinalSum()
+    {
+        return 0;
+    }
+
+    public static string[] ItemsInSession()
+    {
+        return new[] { "" };
+    }
+
+    public static void Payment()
+    {
+        
+    }
+}
---
+++ 12-2025-2026/_feladat/teszteles/feladat_2026_02_17_onkiszolgalo_kassza/OnkiszolgaloKassza_VB/OnkiszolgaloKassza_VB_Test/CashRegisterTests.cs
@@ -0,0 +1,18 @@
+﻿using OnkiszolgaloKassza_VB_Lib;
+
+namespace OnkiszolgaloKassza_VB_Test;
+
+[TestFixture]
+public class CashRegisterTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void TestNewSession()
+    {
+        CashRegister.StartNewSession();
+    }
+}
