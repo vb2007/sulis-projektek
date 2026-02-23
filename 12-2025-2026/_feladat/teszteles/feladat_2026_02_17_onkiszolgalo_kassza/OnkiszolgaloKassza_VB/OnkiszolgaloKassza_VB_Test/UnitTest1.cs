d576599a13ae744ab9dc05db01188d815e160c58
VB2007 <admin@vb2007.hu>
2026-02-23 02:34:43 +0100

    added test project

---
+++ 12-2025-2026/_feladat/teszteles/feladat_2026_02_17_onkiszolgalo_kassza/OnkiszolgaloKassza_VB/OnkiszolgaloKassza_VB_Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace OnkiszolgaloKassza_VB_Test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
