84470593450d5d0bb03accb47bd592ea86f894cc
VB2007 <admin@vb2007.hu>
2026-02-23 02:21:06 +0100

    implemented actual display logic for pages

--- 12-2025-2026/_feladat/backend/feladat_2026_02_17_usa_lovak/vagvolgyi-balazs-usa-lovak/www/page/grid.php
+++ 12-2025-2026/_feladat/backend/feladat_2026_02_17_usa_lovak/vagvolgyi-balazs-usa-lovak/www/page/grid.php
@@ -1,19 +1,23 @@
-<div
-    class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 text-blue-700"
->
-    <div
-        class="horse h-full bg-white rounded-xl shadow-md p-4 flex flex-col border border-blue-200 transition"
-    >
-        <h2 class="text-xl font-semibold text-center">FAJTA</h2>
+<div class="mx-auto max-w-7xl px-4 flex-grow">
+    <h1 class="my-4 text-center text-4xl font-bold text-blue-700">
+        <?= $title ?>
+    </h1>
 
-        <p class="">ÁLLAM (ÉV)</p>
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 text-blue-700">
+        <?php foreach ($horses as $horse): ?>
+        <div class="horse h-full bg-white rounded-xl shadow-md p-4 flex flex-col border border-blue-200 transition hover:border-blue-700 hover:shadow-xl">
+            <h2 class="text-xl font-semibold text-center"><?= $horse->breed ?></h2>
 
-        <img
-            src="https://placehold.net/default.png"
-            alt="FÁJL_NEVE"
-            class="my-2"
-        />
+            <p class="text-center text-gray-500"><?= $horse->state ?> (<?= $horse->year ?>)</p>
 
-        <p class="">LEÍRÁS</p>
+            <img
+                src="<?= $horse->path ?>"
+                alt="<?= $horse->image ?>"
+                class="my-2"
+            />
+
+            <p class="text-gray-700"><?= $horse->description ?></p>
+        </div>
+        <?php endforeach; ?>
     </div>
 </div>
--- 12-2025-2026/_feladat/backend/feladat_2026_02_17_usa_lovak/vagvolgyi-balazs-usa-lovak/www/page/table.php
+++ 12-2025-2026/_feladat/backend/feladat_2026_02_17_usa_lovak/vagvolgyi-balazs-usa-lovak/www/page/table.php
@@ -1,17 +1,29 @@
-<div class="w-full">
-    <table
-        class="min-w-full border border-blue-200 overflow-hidden"
-    >
-        <thead class="">
-            <tr>
-                <th class="px-4 py-3 text-left text-sm">COL</th>
-            </tr>
-        </thead>
+<div class="mx-auto max-w-7xl px-4 flex-grow">
+    <h1 class="my-4 text-center text-4xl font-bold text-blue-700">
+        <?= $title ?>
+    </h1>
 
-        <tbody class="divide-y divide-blue-100 bg-white">
-            <tr class="odd:bg-blue-50 transition">
-                <td class="px-4 py-3 text-sm text-gray-700">COL</td>
-            </tr>
-        </tbody>
-    </table>
+    <div class="w-full">
+        <table class="min-w-full border border-blue-200 overflow-hidden">
+            <thead class="bg-blue-700 text-white">
+                <tr>
+                    <th class="px-4 py-3 text-left text-sm font-bold">Állam</th>
+                    <th class="px-4 py-3 text-left text-sm font-bold">Fajta</th>
+                    <th class="px-4 py-3 text-left text-sm font-bold">Leírás</th>
+                    <th class="px-4 py-3 text-left text-sm font-bold">Év</th>
+                </tr>
+            </thead>
+
+            <tbody class="divide-y divide-blue-100 bg-white">
+                <?php foreach ($horses as $horse): ?>
+                <tr class="odd:bg-blue-50 transition">
+                    <td class="px-4 py-3 text-sm text-gray-700"><?= $horse->state ?></td>
+                    <td class="px-4 py-3 text-sm text-gray-700 font-bold"><?= $horse->breed ?></td>
+                    <td class="px-4 py-3 text-sm text-gray-700"><?= $horse->description ?></td>
+                    <td class="px-4 py-3 text-sm text-gray-700"><?= $horse->year ?></td>
+                </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
 </div>
