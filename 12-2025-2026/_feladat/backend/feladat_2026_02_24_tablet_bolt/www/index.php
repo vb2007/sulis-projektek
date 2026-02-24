ec47753778b2f82f9c2355854818787ddc6dc968
VB2007 <admin@vb2007.hu>
2026-02-24 20:44:57 +0100

    implemented $page query param check

--- 12-2025-2026/_feladat/backend/feladat_2026_02_24_tablet_bolt/www/index.php
+++ 12-2025-2026/_feladat/backend/feladat_2026_02_24_tablet_bolt/www/index.php
@@ -8,3 +8,13 @@ require __DIR__ . "/vendor/autoload.php";
 $whoops = new \Whoops\Run;
 $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
 $whoops->register();
+
+$page = "home";
+
+if (!empty($_GET["page"])) {
+    $page = $_GET["page"];
+}
+
+if ($page != "table" && $page != "grid") {
+    $page = "404";
+}
