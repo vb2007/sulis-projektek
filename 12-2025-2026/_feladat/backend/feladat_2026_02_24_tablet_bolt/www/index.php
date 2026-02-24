db545dc1ac8d50c8d7c3aefcff92879764a73bcc
VB2007 <admin@vb2007.hu>
2026-02-24 20:55:05 +0100

    completed tasks 10 - 15

--- 12-2025-2026/_feladat/backend/feladat_2026_02_24_tablet_bolt/www/index.php
+++ 12-2025-2026/_feladat/backend/feladat_2026_02_24_tablet_bolt/www/index.php
@@ -15,6 +15,47 @@ if (!empty($_GET["page"])) {
     $page = $_GET["page"];
 }
 
-if ($page != "table" && $page != "grid") {
+if ($page !== "table" && $page !== "grid") {
     $page = "404";
 }
+
+$menuItems = [
+    [
+        "text" => "Főoldal",
+        "url" => "index.php",
+        "active" => $page === "home"
+    ],
+    [
+        "text" => "Táblázat",
+        "url" => "index.php?page=table",
+        "active" => $page === "table"
+    ],
+    [
+        "text" => "Rács",
+        "url" => "index.php?page=grid",
+        "active" => $page === "grid"
+    ]
+];
+
+require __DIR__ . "data.php";
+
+switch ($page) {
+    //ár szerint növekvő
+    case "table":
+        usort($tablets, fn($a, $b) //arrown function, simánál return kellene
+            => $a->price <=> $b->price);
+        break;
+
+    //ár szerint csökkenő
+    case "grid":
+        usort($tablets, fn($a, $b)
+            => $b->price <=> $a->price);
+        break;
+
+    case "404":
+        header("HTTP/1.0 404 Not Found");
+        break;
+}
+
+$title = "Tabletek: " . count($tablets) . " darab";
+?>
