2a1d7cc9f8548135ec604861fad0c4be475b6218
VB2007 <admin@vb2007.hu>
2026-02-24 20:25:46 +0100

    completed task 7

---
+++ 12-2025-2026/_feladat/backend/feladat_2026_02_24_tablet_bolt/www/index.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+error_reporting(E_ALL);
+
+require __DIR__ . "/vendor/autoload.php";
+
+$whoops = new \Whoops\Run;
+$whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+$whoops->register();
