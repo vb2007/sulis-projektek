bf575a1146ccae11521a64f9afcbe0f2584008c3
Balázs Vágvölgyi <balazs.vagvolgyi@linistry.com>
2026-02-25 22:51:14 +0100

    created solution & projects

---
+++ 12-2025-2026/_feladat/alkalmazasfejlesztes/feladat_2026_02_24_utasszallito_repulo/Utasszallitok_VB/Utasszallitok_VB_Console/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Utasszallitok_VB_Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ 12-2025-2026/_feladat/alkalmazasfejlesztes/feladat_2026_02_24_utasszallito_repulo/Utasszallitok_VB/Utasszallitok_VB_Lib/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Utasszallitok_VB_Lib
+{
+    public class Class1
+    {
+
+    }
+}
