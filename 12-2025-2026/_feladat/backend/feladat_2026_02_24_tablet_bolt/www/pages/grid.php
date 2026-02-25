55877e80e46264b41ec78e2675160503efc2c518
VB2007 <admin@vb2007.hu>
2026-02-25 13:33:21 +0100

    converted grid html to php + used dynamic title variable

--- 12-2025-2026/_feladat/backend/feladat_2026_02_24_tablet_bolt/www/pages/grid.php
+++ 12-2025-2026/_feladat/backend/feladat_2026_02_24_tablet_bolt/www/pages/grid.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Tabletek 14 darab</title>
+        <title><?= $title ?></title>
         <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
         <link rel="stylesheet" href="./css/tablet.css" />
     </head>
@@ -12,7 +12,7 @@
     <body class="min-h-screen flex flex-col">
         <div class="mx-auto max-w-7xl px-4">
             <h1 class="my-4 text-center text-4xl font-bold text-teal-600">
-                Tabletek 14 darab
+                <?= $title ?>
             </h1>
 
             <div class="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
