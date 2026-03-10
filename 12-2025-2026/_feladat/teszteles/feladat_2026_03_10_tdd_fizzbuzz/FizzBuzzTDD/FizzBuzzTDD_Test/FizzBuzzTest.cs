bbd50f2f06ab0fbbc7d79cdd4ef8577b1375711f
VB2007 <admin@vb2007.hu>
2026-03-10 15:07:25 +0100

    added test cases

---
+++ 12-2025-2026/_feladat/teszteles/feladat_2026_03_10_tdd_fizzbuzz/FizzBuzzTDD/FizzBuzzTDD_Test/FizzBuzzTest.cs
@@ -0,0 +1,33 @@
+﻿namespace FizzBuzzTDD_Test;
+
+public class FizzBuzzTest
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void DivideBy3()
+    {
+        Assert.Pass();
+    }
+    
+    [Test]
+    public void DivideBy2()
+    {
+        Assert.Pass();
+    }
+    
+    [Test]
+    public void DivideBy5()
+    {
+        Assert.Pass();
+    }
+    
+    [Test]
+    public void DivideByBoth()
+    {
+        Assert.Pass();
+    }
+}
