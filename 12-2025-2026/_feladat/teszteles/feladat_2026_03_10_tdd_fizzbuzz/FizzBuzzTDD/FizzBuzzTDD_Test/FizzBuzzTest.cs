bfdc48ca37e262797aa07c122e823d47bb329cc3
VB2007 <admin@vb2007.hu>
2026-03-10 15:21:56 +0100

    fixed test case

--- 12-2025-2026/_feladat/teszteles/feladat_2026_03_10_tdd_fizzbuzz/FizzBuzzTDD/FizzBuzzTDD_Test/FizzBuzzTest.cs
+++ 12-2025-2026/_feladat/teszteles/feladat_2026_03_10_tdd_fizzbuzz/FizzBuzzTDD/FizzBuzzTDD_Test/FizzBuzzTest.cs
@@ -5,17 +5,17 @@ namespace FizzBuzzTDD_Test;
 public class FizzBuzzTest
 {
     [Test]
+    public void DivideBy1()
+    {
+        string result = FizzBuzz.FizzBuzzCheck(1);
+        Assert.That(result, Is.EqualTo("1"), "When the number 1 is used, it should return '1' as a string.");
+    }
+    
+    [Test]
     public void DivideBy3()
     {
         string result = FizzBuzz.FizzBuzzCheck(3);
         Assert.That(result, Is.EqualTo("Fizz"), "When the number 3 is used, it should return 'Fizz'.");
-    }
-    
-    [Test]
-    public void DivideBy2()
-    {
-        string result = FizzBuzz.FizzBuzzCheck(2);
-        Assert.That(result, Is.EqualTo("2"), "When the number 2 is used, it should return '2' as a string.");
     }
     
     [Test]
