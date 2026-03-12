8184da9cc3e296d1ce1e12504e76c50fbb643c71
VB2007 <admin@vb2007.hu>
2026-03-12 13:38:34 +0100

    added missing import & call to main.js

--- 12-2025-2026/_feladat/webprogramozas/feladat_2026_03_10_bicycle_shop/vagvolgyi-balazs-bicycle/web/src/main.js
+++ 12-2025-2026/_feladat/webprogramozas/feladat_2026_03_10_bicycle_shop/vagvolgyi-balazs-bicycle/web/src/main.js
@@ -1,9 +1,12 @@
 import "@assets/app.css";
 import { getBicycles } from "./js/bicycle.js";
-import { generateCards } from "./js/helper.js";
+import { generateCards, setBicyclesRef } from "./js/helper.js";
 
 const bicycles = await getBicycles();
 
 console.log(bicycles);
+
+//tömb -> helper.js
+setBicyclesRef(bicycles);
 
 generateCards(bicycles);
