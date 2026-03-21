8d7bf74724dba917388ab6b7e1214fe8fac8a0da
VB2007 <admin@vb2007.hu>
2026-03-21 22:25:36 +0100

    added php loading to index

--- 12-2025-2026/_feladat/backend/feladat_2026_03_17_villamosok/villamosok-vagvolgyi-balazs/www/index.php
+++ 12-2025-2026/_feladat/backend/feladat_2026_03_17_villamosok/villamosok-vagvolgyi-balazs/www/index.php
@@ -1,3 +1,15 @@
+<?php
+
+error_reporting(E_ALL);
+
+require __DIR__ . "/vendor/autoload.php";
+require __DIR__ . "/data.php";
+
+$whoops = new \Whoops\Run();
+$whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler());
+$whoops->register();
+?>
+
 <!DOCTYPE html>
 <html lang="hu">
 
