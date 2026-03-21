0244eed2e3ea30ae74cb49cae85bfab628496d47
VB2007 <admin@vb2007.hu>
2026-03-21 23:29:01 +0100

    implemented filtering on index page

--- 12-2025-2026/_feladat/backend/feladat_2026_03_17_villamosok/villamosok-vagvolgyi-balazs/www/index.php
+++ 12-2025-2026/_feladat/backend/feladat_2026_03_17_villamosok/villamosok-vagvolgyi-balazs/www/index.php
@@ -5,9 +5,50 @@ error_reporting(E_ALL);
 require __DIR__ . "/vendor/autoload.php";
 require __DIR__ . "/data.php";
 
+// use Budapest\Transport\TramLine;
+
 $whoops = new \Whoops\Run();
 $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler());
 $whoops->register();
+
+$page = $_GET["page"] ?? "grid";
+if ($page !== "table" && $page !== "grid") {
+    $page = "grid";
+}
+
+$filteredLines = $lines;
+
+if (!empty($_GET["terminus"])) {
+    $terminus = $_GET["terminus"];
+    $filteredLines = array_filter($filteredLines, function ($line) use (
+        $terminus,
+    ) {
+        return stripos($line->route, $terminus) !== false;
+    });
+}
+
+if (!empty($_GET["interconnected"])) {
+    $interconnected = $_GET["interconnected"];
+    $filteredLines = array_filter($filteredLines, function ($line) use (
+        $interconnected,
+    ) {
+        return $line->interconnected === $interconnected;
+    });
+}
+if (!empty($_GET["since"])) {
+    $since = (int) $_GET["since"];
+    $filteredLines = array_filter($filteredLines, function ($line) use (
+        $since,
+    ) {
+        return $line->since <= $since;
+    });
+
+    usort($filteredLines, function ($a, $b) {
+        return $a->since <=> $b->since;
+    });
+}
+
+$lines = array_values($filteredLines);
 ?>
 
 <!DOCTYPE html>
