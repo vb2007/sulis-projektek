185a3318e66c475291f7f7fd9227d7a2d4cdbcdd
VB2007 <admin@vb2007.hu>
2026-03-21 23:26:37 +0100

    implemented menu

--- 12-2025-2026/_feladat/backend/feladat_2026_03_17_villamosok/villamosok-vagvolgyi-balazs/www/components/menu.php
+++ 12-2025-2026/_feladat/backend/feladat_2026_03_17_villamosok/villamosok-vagvolgyi-balazs/www/components/menu.php
@@ -6,8 +6,12 @@ $menuItems = [
 <nav class="bg-gray-50 rounded-lg border border-gray-500 py-2 px-3 flex justify-between">
     <span>Viszonylatok</span>
     <ul class="flex items-center gap-4 ">
+        <?php foreach ($menuItems as $key => $value): ?>
             <li>
-                <a class="" href="index.php"></a>
+                <a class="<?= $page === $key
+                    ? "font-bold"
+                    : "" ?>" href="index.php?page=<?= $key ?>"><?= $value ?></a>
             </li>
+        <?php endforeach; ?>
     </ul>
 </nav>
