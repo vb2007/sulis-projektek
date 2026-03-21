ed7620af12bc0ab848ce48fa2656b4eaf5f42a1d
VB2007 <admin@vb2007.hu>
2026-03-21 22:21:10 +0100

    updated formatting

--- 12-2025-2026/_feladat/backend/feladat_2026_03_17_villamosok/villamosok-vagvolgyi-balazs/www/components/menu.php
+++ 12-2025-2026/_feladat/backend/feladat_2026_03_17_villamosok/villamosok-vagvolgyi-balazs/www/components/menu.php
@@ -1,9 +1,8 @@
 <?php
 $menuItems = [
-    'table' => 'Táblázat',
-    'grid' => 'Rács'
-];
-?>
+    "table" => "Táblázat",
+    "grid" => "Rács",
+]; ?>
 <nav class="bg-gray-50 rounded-lg border border-gray-500 py-2 px-3 flex justify-between">
     <span>Viszonylatok</span>
     <ul class="flex items-center gap-4 ">
