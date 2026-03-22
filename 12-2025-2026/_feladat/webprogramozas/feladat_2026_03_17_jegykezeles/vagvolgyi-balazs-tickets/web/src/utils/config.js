e12f4a01e111768c9864e0f64ceb71a04847490c
VB2007 <admin@vb2007.hu>
2026-03-22 19:28:47 +0100

    fixed base url

--- 12-2025-2026/_feladat/webprogramozas/feladat_2026_03_17_jegykezeles/vagvolgyi-balazs-tickets/web/src/utils/config.js
+++ 12-2025-2026/_feladat/webprogramozas/feladat_2026_03_17_jegykezeles/vagvolgyi-balazs-tickets/web/src/utils/config.js
@@ -1,1 +1,2 @@
-export const BASE_URL = "http://localhost:8888/api/";
+//feladatban "http://localhost:8888/" volt, de nincs extra "/api" path az alap json server configban
+export const BASE_URL = "http://localhost:8888/";
