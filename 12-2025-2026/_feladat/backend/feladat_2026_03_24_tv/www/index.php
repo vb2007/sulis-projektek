7dd7b70b3ead29e4c1c698f06566dd94c4b75464
VB2007 <admin@vb2007.hu>
2026-03-24 08:21:25 +0100

    added html blocks

--- 12-2025-2026/_feladat/backend/feladat_2026_03_24_tv/www/index.php
+++ 12-2025-2026/_feladat/backend/feladat_2026_03_24_tv/www/index.php
@@ -8,3 +8,20 @@ require __DIR__ . "/data.php";
 $whoops = new \Whoops\Run();
 $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler());
 $whoops->register();
+?>
+
+<!doctype html>
+<html lang="hu">
+    <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>TV 10 darab</title>
+        <link rel="stylesheet" href="css/tv.css" />
+        <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    </head>
+
+    <body>
+    </body>
+
+</html>
