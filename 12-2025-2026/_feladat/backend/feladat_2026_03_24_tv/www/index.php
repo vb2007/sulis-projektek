e473b5e63ffeb55617171aca565c01cc033557bd
VB2007 <admin@vb2007.hu>
2026-03-24 08:18:22 +0100

    added whoops

--- 12-2025-2026/_feladat/backend/feladat_2026_03_24_tv/www/index.php
+++ 12-2025-2026/_feladat/backend/feladat_2026_03_24_tv/www/index.php
@@ -4,3 +4,7 @@ error_reporting(E_ALL);
 
 require __DIR__ . "/vendor/autoload.php";
 require __DIR__ . "/data.php";
+
+$whoops = new \Whoops\Run();
+$whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler());
+$whoops->register();
