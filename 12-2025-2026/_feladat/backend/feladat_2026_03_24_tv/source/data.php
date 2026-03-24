8758c761d0df687ce95ec219e9d942e921ebf2b2
VB2007 <admin@vb2007.hu>
2026-03-24 07:41:42 +0100

    added source files

---
+++ 12-2025-2026/_feladat/backend/feladat_2026_03_24_tv/source/data.php
@@ -0,0 +1,16 @@
+<?php
+
+use MyShop\Multimedia\Television;
+
+$televisions = [
+    new Television(10,"SAMSUNG QE98QN90AATXXH Neo QLED 4K UHD Smart TV", 1, 98, "4K UHD", "Neo QLED", TRUE, 2599899),
+    new Television(1,"SAMSUNG UE 55 AU7022KXXH 4K UHD Smart LED Televízió, 138 cm", 1, 55, "4K UHD", "LED", TRUE, 137989),
+    new Television(2,"LG 55UR73003LA smart tv, LED TV,LCD 4K TV, Ultra HD TV,uhd TV, HDR,webOS ThinQ AI okos tv, 139 cm", 2, 55, "4K UHD", "LCD", TRUE, 186748),
+    new Television(3,"LG 65UR73003LA smart tv, LED TV, LCD 4K TV, Ultra HD TV,uhd TV, HDR,webOS ThinQ AI okos tv, 164 cm", 2, 65, "4K UHD", "LED", TRUE, 231748),
+    new Television(4,"SAMSUNG QE43Q60CAUXXH QLED 4K UHD Smart TV, 108 cm", 1, 43, "4K UHD", "QLED", TRUE, 144898),
+    new Television(9,"PHILIPS 65OLED718/12 4K Ultra HD Smart OLED Ambilight televízió, 164 cm", 3, 65, "4K UHD", "OLED", TRUE, 599999),
+    new Television(5,"PHILIPS 32PHS5507/12 HD Ready LED televízió, borderless, 80 cm", 3, 32, "HD Ready", "LED", FALSE, 72999),
+    new Television(6,"SAMSUNG UE32T4302AEXXH HD Smart TV, 80 cm", 1, 32, "Full HD", "LED", FALSE, 89899),
+    new Television(7,"SAMSUNG UE32T5302CEXXH Full HD Smart TV, 80 cm", 1, 32, "Full HD", "LED", TRUE, 99899),
+    new Television(8,"SAMSUNG UE50CU7172UXXH 4K Crystal UHD Smart TV, 125 cm", 1, 50, "4K UHD", "LED", TRUE, 144899),
+];
