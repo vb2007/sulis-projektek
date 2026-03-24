bd26f76eecb7fd9d1774b588db5921b0bfb687d3
VB2007 <admin@vb2007.hu>
2026-03-24 08:29:06 +0100

    fixed query paqram handling

--- 12-2025-2026/_feladat/backend/feladat_2026_03_24_tv/www/index.php
+++ 12-2025-2026/_feladat/backend/feladat_2026_03_24_tv/www/index.php
@@ -9,9 +9,12 @@ $whoops = new \Whoops\Run();
 $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler());
 $whoops->register();
 
-$page = $_GET["page"] ?? "products";
-if ($page !== "seller" && $page !== "products") {
-    $page = "404";
+$page = "home";
+if (!empty($_GET["page"])) {
+    $page = $_GET["page"];
+    if ($page !== "seller" && $page !== "products") {
+        $page = "404";
+    }
 }
 ?>
 
@@ -21,7 +24,7 @@ if ($page !== "seller" && $page !== "products") {
         <meta charset="UTF-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>TV 10 darab</title>
+        <title><?= $title ?></title>
         <link rel="stylesheet" href="css/tv.css" />
         <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
     </head>
