a6b625dcf1445ac802e3331c24a01d94a7907af8
Balázs Vágvölgyi <balazs.vagvolgyi@linistry.com>
2026-03-25 23:45:57 +0100

    created solution & console app

---
+++ 12-2025-2026/_feladat/alkalmazasfejlesztes/feladat_2026_03_24_keresztrejtveny/Keresztrejtveny/Keresztrejtveny/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Keresztrejtveny
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
