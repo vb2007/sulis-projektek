e8823f1a0fe731521a64484a0ce278bbb749504a
Balázs Vágvölgyi <balazs.vagvolgyi@linistry.com>
2026-03-26 08:56:32 +0100

    completed backend logic for wpf app

--- 12-2025-2026/_feladat/alkalmazasfejlesztes/feladat_2026_03_26_karakter_dekodolo/KarakterDekodolo/KarakterDekodoloGUI/MainWindow.xaml.cs
+++ 12-2025-2026/_feladat/alkalmazasfejlesztes/feladat_2026_03_26_karakter_dekodolo/KarakterDekodolo/KarakterDekodoloGUI/MainWindow.xaml.cs
@@ -1,13 +1,7 @@
-﻿using System.Text;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace KarakterDekodoloGUI
 {
@@ -16,9 +10,67 @@ namespace KarakterDekodoloGUI
     /// </summary>
     public partial class MainWindow : Window
     {
+        private const int Oszlopok = 4;
+        private const int Sorok = 7;
+
         public MainWindow()
         {
             InitializeComponent();
+            MatrixLetrehozas();
+        }
+
+        private void MatrixLetrehozas()
+        {
+            for (int col = 0; col < Oszlopok; col++)
+            {
+                gridMatrix.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+            }
+
+            for (int row = 0; row < Sorok; row++)
+            {
+                gridMatrix.RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto });
+            }
+
+            for (int row = 0; row < Sorok; row++)
+            {
+                for (int col = 0; col < Oszlopok; col++)
+                {
+                    TextBox tb = new()
+                    {
+                        Text = "0",
+                        Width = 40,
+                        Height = 35,
+                        Margin = new Thickness(2),
+                        HorizontalContentAlignment = HorizontalAlignment.Center,
+                        VerticalContentAlignment = VerticalAlignment.Center,
+                        FontSize = 16,
+                        Background = Brushes.White
+                    };
+
+                    tb.TextChanged += Mezo_TextChanged;
+                    tb.MouseDoubleClick += Mezo_MouseDoubleClick;
+
+                    Grid.SetRow(tb, row);
+                    Grid.SetColumn(tb, col);
+                    gridMatrix.Children.Add(tb);
+                }
+            }
+        }
+
+        private void Mezo_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (sender is TextBox tb)
+            {
+                tb.Background = tb.Text == "1" ? Brushes.LightGray : Brushes.White;
+            }
+        }
+
+        private void Mezo_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (sender is TextBox tb)
+            {
+                tb.Text = tb.Text == "0" ? "1" : "0";
+            }
         }
     }
 }
