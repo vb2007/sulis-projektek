e3a379b25d7231dd608ac91135cb5d288c446379
VB2007 <admin@vb2007.hu>
2026-03-31 14:24:59 +0200

    added ErrorMessage helper, implemented Year2024 case

--- 12-2025-2026/_dolgozat/teszteles/dolgozat_2026_03_31_tdd_szokoev_fuggveny/LeapYearTDD_VB/LeapYearTDD_VB_Test/LeapYearTest.cs
+++ 12-2025-2026/_dolgozat/teszteles/dolgozat_2026_03_31_tdd_szokoev_fuggveny/LeapYearTDD_VB/LeapYearTDD_VB_Test/LeapYearTest.cs
@@ -5,12 +5,19 @@ namespace LeapYearTDD_VB_Test;
 [TestFixture]
 public class LeapYearTest
 {
+    private string ErrorMessage(int year, bool expected, bool actual) => $"Ha az év {year}, a várható eredmény {expected}, de a teszt {actual}-t adott vissza.";
+    
     [Test]
     public void Year2021()
     {
         int year = 2021;
-        Assert.That(LeapYear.IsLeapYear(year), Is.False, $"Ha az év {year}, a várható eredmény False, de a teszt True-t adott vissza.");
+        Assert.That(LeapYear.IsLeapYear(year), Is.False, ErrorMessage(year, false, true));
     }
 
-    
+    [Test]
+    public void Year2024()
+    {
+        int year = 2024;
+        Assert.That(LeapYear.IsLeapYear(year), Is.True, $"Ha az év {year}, a várható eredmény True, de a teszt False-t adott vissza.");
+    }
 }
