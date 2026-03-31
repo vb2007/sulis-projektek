ecf75b95050d6bea275d8bf5c8b5bc1009a5cf4f
VB2007 <admin@vb2007.hu>
2026-03-31 13:51:43 +0200

    created unit test project, added reference

---
+++ 12-2025-2026/_dolgozat/teszteles/dolgozat_2026_03_31_tdd_szokoev_fuggveny/LeapYearTDD_VB/LeapYearTDD_VB_Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace LeapYearTDD_VB_Test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
