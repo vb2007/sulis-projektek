a9ff8a79536aa1b20f926dc6c8c0b4f71b5ed631
VB2007 <admin@vb2007.hu>
2026-04-16 08:48:07 +0200

    implemented all programing practices

--- 12-2025-2026/_feladat/alkalmazasfejlesztes/feladat_2026_04_16_matrixok/Matrixok_VB/Matrixok_VB_Console/Program.cs
+++ 12-2025-2026/_feladat/alkalmazasfejlesztes/feladat_2026_04_16_matrixok/Matrixok_VB/Matrixok_VB_Console/Program.cs
@@ -23,5 +23,38 @@ class Program
 
         int[,] symmetriMatrix = Matrix.CreateSymmetricMatrix();
         Matrix.WriteMatrix("Szimmetrikusmátrix", symmetriMatrix);
+        
+        //Programozási tételek mátrixokra
+
+        int[,] upperTriangularMatrixSample = Matrix.CreateUpperTriangularMatrix();
+        Matrix.WriteMatrix("Teszt felső háromszögmátrix tételekhez", upperTriangularMatrixSample);
+        
+        Console.WriteLine($"Elemeinek összege: {Matrix.MatrixSum(upperTriangularMatrixSample)}");
+        
+        Console.WriteLine($"Elemeinek átlaga: {Matrix.MatrixAverage(upperTriangularMatrixSample)}");
+        
+        Console.WriteLine($"Páros elemek száma: {Matrix.MatrixEvenNumbersCount(upperTriangularMatrixSample)}");
+        
+        (int number, int i, int j) maxNumber = Matrix.MatrixMaxNumber(upperTriangularMatrixSample);
+        Console.WriteLine($"Legnagyobb elem értéke: {maxNumber.number}");
+        Console.WriteLine($"Legnagyobb elem helye: [{maxNumber.i}, {maxNumber.j}]");
+        
+        (int number, int i, int j) minNumber = Matrix.MatrixMinNumber(upperTriangularMatrixSample);
+        Console.WriteLine($"Legkisebb elem értéke: {minNumber.number}");
+        Console.WriteLine($"Legkisebb elem helye: [{minNumber.i}, {minNumber.j}]");
+
+        bool containsDividableBy3 = Matrix.DoesContainNumberDividedBy(upperTriangularMatrix, 3);
+        if (containsDividableBy3)
+        {
+            Console.WriteLine("A mátrix tartalmaz 3-mal osztható számot.");
+            
+            (int number, int i, int j) divideBy3 = Matrix.MatrixDivideByNumber(upperTriangularMatrixSample, 3);
+            Console.WriteLine($"3-mal osztható szám értéke: {divideBy3.number}");
+            Console.WriteLine($"3-mal osztható szám helye: [{divideBy3.i}, {divideBy3.j}]");
+        }
+        else
+        {
+            Console.WriteLine("A mátrix NEM tartalmaz 3-mal osztható számot.");   
+        } 
     }
 }
