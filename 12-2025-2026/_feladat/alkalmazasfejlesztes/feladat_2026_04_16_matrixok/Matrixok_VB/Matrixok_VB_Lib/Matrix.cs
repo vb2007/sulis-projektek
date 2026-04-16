2f70c5912b8c27e042a00882afee46a873b7895d
VB2007 <admin@vb2007.hu>
2026-04-16 08:16:58 +0200

    created WriteMatrix and CreateNullmatrix

--- 12-2025-2026/_feladat/alkalmazasfejlesztes/feladat_2026_04_16_matrixok/Matrixok_VB/Matrixok_VB_Lib/Matrix.cs
+++ 12-2025-2026/_feladat/alkalmazasfejlesztes/feladat_2026_04_16_matrixok/Matrixok_VB/Matrixok_VB_Lib/Matrix.cs
@@ -3,4 +3,29 @@
 public class Matrix
 {
     private static int[,] _matrix = new int[10, 10];
+
+    public static void WriteMatrix(int[,] matrix)
+    {
+        for (int i = 0; i < _matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < _matrix.GetLength(1); j++)
+            {
+                Console.Write(_matrix[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+    
+    public static int[,] CreateNullmatrix()
+    {
+        for (int i = 0; i < _matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < _matrix.GetLength(1); j++)
+            {
+                _matrix[i, j] = 0;
+            }
+        }
+
+        return _matrix;
+    }
 }
