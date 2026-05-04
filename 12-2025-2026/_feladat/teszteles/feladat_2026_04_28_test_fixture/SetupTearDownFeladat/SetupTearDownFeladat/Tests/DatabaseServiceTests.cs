e6fbd7be144f4cc89273e98964e2826ae9319062
VB2007 <admin@vb2007.hu>
2026-05-04 22:13:21 +0200

    added source files

---
+++ 12-2025-2026/_feladat/teszteles/feladat_2026_04_28_test_fixture/SetupTearDownFeladat/SetupTearDownFeladat/Tests/DatabaseServiceTests.cs
@@ -0,0 +1,48 @@
+using NUnit.Framework;
+
+namespace SetupTearDownFeladat.Tests
+{
+    [TestFixture]
+    public class DatabaseServiceTests
+    {
+        protected DatabaseService _service;
+        protected const string TestDbPath = "test_library.db";
+
+        // TODO: Írd meg a SetUp-ot, amely példányosítja a DatabaseService-t a TestDbPath paraméterrel!
+        
+        // TODO: Írd meg a Teardown-t, amely bezárja a kapcsolatot és törli is a fájlt, ha létezik! 
+        
+        // Ellenőrizd, hogy a megfelelő SetUp és Teardown megírása után sikeresen lefutnak-e a tesztek!
+        
+        [Test]
+        public void AddBook_ShouldIncreaseCount()
+        {
+            // Act
+            _service.AddBook("Egri Csillagok");
+            int count = _service.GetBookCount();
+
+            // Assert
+            Assert.That(count, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void Clear_ShouldRemoveAllBooks()
+        {
+            // Arrange
+            _service.AddBook("A Pál utcai fiúk");
+            
+            // Act
+            _service.Clear();
+            
+            // Assert
+            Assert.That(_service.GetBookCount(), Is.EqualTo(0));
+        }
+        
+        [Test]
+        public void Test_Connection_Is_Active()
+        {
+            Assert.That(_service.IsConnected, Is.True);
+        }
+        
+    }
+}
