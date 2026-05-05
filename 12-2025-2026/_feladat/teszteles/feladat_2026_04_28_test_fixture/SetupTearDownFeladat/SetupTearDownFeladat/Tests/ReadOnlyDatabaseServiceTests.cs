6b45c207abe7789c2c739ae09dc183af39bef512
VB2007 <admin@vb2007.hu>
2026-05-05 20:03:59 +0200

    added reasoning to ReadOnlyDatabaseServiceTests

--- 12-2025-2026/_feladat/teszteles/feladat_2026_04_28_test_fixture/SetupTearDownFeladat/SetupTearDownFeladat/Tests/ReadOnlyDatabaseServiceTests.cs
+++ 12-2025-2026/_feladat/teszteles/feladat_2026_04_28_test_fixture/SetupTearDownFeladat/SetupTearDownFeladat/Tests/ReadOnlyDatabaseServiceTests.cs
@@ -5,5 +5,6 @@ namespace SetupTearDownFeladat.Tests;
 [TestFixture]
 public class ReadOnlyDatabaseServiceTests : DatabaseServiceTests
 {
-    
+    // Azért előnyös itt a származtatás, mert a SetUp és TearDown metódusokat újra felhasználhatjuk.
+    // Így elkerülhetjük a kód duplikációt.
 }
