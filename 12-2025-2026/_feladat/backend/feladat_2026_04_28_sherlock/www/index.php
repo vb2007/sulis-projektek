97b2731475e1874161e26e217581249e111064ec
VB2007 <admin@vb2007.hu>
2026-05-10 23:45:06 +0200

    added title & 404 handling

--- 12-2025-2026/_feladat/backend/feladat_2026_04_28_sherlock/www/index.php
+++ 12-2025-2026/_feladat/backend/feladat_2026_04_28_sherlock/www/index.php
@@ -10,10 +10,13 @@ $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler());
 $whoops->register();
 
 $action = "home";
+$title = "Sherlock epizódok";
 if (!empty($_GET["action"])) {
     $action = $_GET["action"];
     if ($action != "create") {
         $action = "404";
+        $title = "404";
+        header("HTTP/1.0 404 Not Found");
     }
 }
 
@@ -38,7 +41,7 @@ $menuItems = [
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>OLDAL CÍME</title>
+    <title><?= $title ?></title>
     <link rel="stylesheet" href="css/tv.css">
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 </head>
