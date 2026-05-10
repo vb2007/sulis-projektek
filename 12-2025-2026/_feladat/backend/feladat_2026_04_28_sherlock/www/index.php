801cd245b16df21aa38723cfef681c1aaf2ae4f5
VB2007 <admin@vb2007.hu>
2026-05-10 23:37:16 +0200

    completed task 2, 3

--- 12-2025-2026/_feladat/backend/feladat_2026_04_28_sherlock/www/index.php
+++ 12-2025-2026/_feladat/backend/feladat_2026_04_28_sherlock/www/index.php
@@ -8,3 +8,11 @@ require __DIR__ . "/data/data.php";
 $whoops = new \Whoops\Run();
 $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler());
 $whoops->register();
+
+$action = "home";
+if (!empty($_GET["action"])) {
+    $action = $_GET["action"];
+    if ($action != "create") {
+        $action = "404";
+    }
+}
