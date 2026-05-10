fe4fe850a1748559505cd4efb77059dddd96fec4
VB2007 <admin@vb2007.hu>
2026-05-10 23:33:56 +0200

    index.php init, whoops config

---
+++ 12-2025-2026/_feladat/backend/feladat_2026_04_28_sherlock/www/index.php
@@ -0,0 +1,10 @@
+<?php
+
+error_reporting(E_ALL);
+
+require __DIR__ . "/vendor/autoload.php";
+require __DIR__ . "/data/data.php";
+
+$whoops = new \Whoops\Run();
+$whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler());
+$whoops->register();
