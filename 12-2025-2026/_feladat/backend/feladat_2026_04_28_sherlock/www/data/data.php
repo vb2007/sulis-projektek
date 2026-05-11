012aa5d84e473f3f71b9ea9cd32fdad4bdd54127
VB2007 <admin@vb2007.hu>
2026-05-11 15:40:20 +0200

    added parsing to data.php

--- 12-2025-2026/_feladat/backend/feladat_2026_04_28_sherlock/www/data/data.php
+++ 12-2025-2026/_feladat/backend/feladat_2026_04_28_sherlock/www/data/data.php
@@ -9,6 +9,6 @@ $lines = explode("\n", $file);
 for ($i = 1; $i < count($lines); $i++) {
     if ("" != $lines[$i]) {
         $split = explode("$", $lines[$i]);
-        array_push($episodes, new Sherlock($split[0], $split[1], $split[2], $split[3], $split[4], $split[5], $split[6]));
+        array_push($episodes, new Sherlock((int) $split[0], $split[1], (int) $split[2], $split[3], $split[4], $split[5], (float) $split[6]));
     }
 }
