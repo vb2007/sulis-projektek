b549722f6eda1ed836a671da258a11901c0865e3
VB2007 <admin@vb2007.hu>
2026-05-11 15:27:59 +0200

    created input template for form

---
+++ 12-2025-2026/_feladat/backend/feladat_2026_04_28_sherlock/www/components/form.php
@@ -0,0 +1,6 @@
+<form method="POST" action="index.php?action=create" class="grid gap-4">
+    <div class="gird grid-cols-1 gap-4">
+        <label for=""></label>
+        <input type="text" id="" name="">
+    </div>
+</form>
