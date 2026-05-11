0c2ec9d0287c86781045a70cd62dc49f090d0c02
VB2007 <admin@vb2007.hu>
2026-05-11 16:41:01 +0200

    finished 404

--- 12-2025-2026/_feladat/backend/feladat_2026_04_28_sherlock/www/pages/404.php
+++ 12-2025-2026/_feladat/backend/feladat_2026_04_28_sherlock/www/pages/404.php
@@ -1,3 +1,5 @@
+<!-- "Alakítsa ki a 404.php fájlt a template.html és a minta alapján." -->
+<!-- Nincsen 404 minta a template-ben, nincsen a 404-es oldalról screenshot -->
 <div class="mx-auto max-w-7xl px-4 py-4 flex-grow create overflow-hidden">
     <h1 class="my-4 text-center text-4xl font-bold text-slate-600">404</h1>
 </div>
