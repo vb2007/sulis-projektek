0fc4190d607bb11eebde8890b8af675b96387c9c
VB2007 <admin@vb2007.hu>
2026-05-11 17:41:22 +0200

    updated 404 page

--- 12-2025-2026/_feladat/backend/feladat_2026_04_28_sherlock/www/pages/404.php
+++ 12-2025-2026/_feladat/backend/feladat_2026_04_28_sherlock/www/pages/404.php
@@ -1,5 +1,4 @@
-<!-- "Alakítsa ki a 404.php fájlt a template.html és a minta alapján." -->
-<!-- Nincsen 404 minta a template-ben, nincsen a 404-es oldalról screenshot -->
-<div class="mx-auto max-w-7xl px-4 py-4 flex-grow create overflow-hidden">
+<div class="mx-auto max-w-7xl px-4 py-4 flex-grow">
     <h1 class="my-4 text-center text-4xl font-bold text-slate-600">404</h1>
+    <p class="text-center text-slate-600">Az oldal nem található.</p>
 </div>
