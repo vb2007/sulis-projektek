6383bba60f22f5a916ae581c0016b11e16a494c8
VB2007 <admin@vb2007.hu>
2026-05-24 22:40:56 +0200

    added fromCsvLine function to concert class

--- 12-2025-2026/_feladat/backend/feladat_2026_05_14_koncertek/vagvolgyi_balazs_koncertek/src/Event/Party/Concert.php
+++ 12-2025-2026/_feladat/backend/feladat_2026_05_14_koncertek/vagvolgyi_balazs_koncertek/src/Event/Party/Concert.php
@@ -48,4 +48,9 @@ class Concert implements Stringable {
     public static function getTypes(): array {
         return self::$types;
     }
+
+    public static function fromCsvLine(string $line): self {
+        [$id, $name, $location, $type, $date, $price] = str_getcsv($line, ";");
+        return new self((int)$id, $name, $location, $type, $date, (int)$price);
+    }
 }
