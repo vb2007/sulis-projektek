7d644eb94d1ec5c7865b9f4832e602f313f55c5b
VB2007 <admin@vb2007.hu>
2026-05-24 22:39:56 +0200

    added null checks to index page

--- 12-2025-2026/_feladat/backend/feladat_2026_05_14_koncertek/vagvolgyi_balazs_koncertek/index.php
+++ 12-2025-2026/_feladat/backend/feladat_2026_05_14_koncertek/vagvolgyi_balazs_koncertek/index.php
@@ -7,7 +7,7 @@ $whoops->register();
 ob_start();
 
 $title = 'Koncertek';
-$action = $_GET["action"];
+$action = $_GET["action"] ?? null;
 ?>
 
 <!DOCTYPE html>
@@ -29,7 +29,7 @@ $action = $_GET["action"];
         switch ($action) {
             case "index":
                 $title = "Koncertek";
-                $layout = $_GET["layout"];
+                $layout = $_GET["layout"] ?? null;
                 switch ($layout) {
                     case "grid":
                         include __DIR__ . "/pages/grid.php";
@@ -40,33 +40,33 @@ $action = $_GET["action"];
                         break;
 
                     default:
-                        include __DIR__ . "pages/grid.php";
+                        include __DIR__ . "/pages/grid.php";
                         break;
                 }
                 break;
 
             case "show":
                 $title = "--koncert neve--";
-                $id = $_GET["id"];
-                include __DIR__ . "pages/show.php";
+                $id = $_GET["id"] ?? null;
+                include __DIR__ . "/pages/show.php";
                 break;
 
             case "create":
                 $title = "Koncert rögzítése";
-                include __DIR__ . "pages/create.php";
+                include __DIR__ . "/pages/create.php";
                 break;
 
             case "store":
-                include __DIR__ . "pages/table.php";
+                include __DIR__ . "/pages/table.php";
                 break;
 
             case "404":
                 $title = "Koncertek";
-                include __DIR__ . "pages/404.php";
+                include __DIR__ . "/pages/404.php";
                 break;
 
             default:
-                include __DIR__ . "pages/grid.php";
+                include __DIR__ . "/pages/grid.php";
                 break;
         }
         ?>
