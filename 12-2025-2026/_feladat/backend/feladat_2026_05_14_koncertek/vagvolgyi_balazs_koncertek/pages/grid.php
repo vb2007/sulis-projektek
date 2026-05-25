c46211e6f9ee8d30d02882d70bdb10c9a490b79c
VB2007 <admin@vb2007.hu>
2026-05-25 15:11:51 +0200

    added data loading to grid

--- 12-2025-2026/_feladat/backend/feladat_2026_05_14_koncertek/vagvolgyi_balazs_koncertek/pages/grid.php
+++ 12-2025-2026/_feladat/backend/feladat_2026_05_14_koncertek/vagvolgyi_balazs_koncertek/pages/grid.php
@@ -1,14 +1,16 @@
 <a href="index.php?action=create" class="bg-violet-600 text-white text-center p-1 rounded-md inline-block cursor-pointer font-bold">Koncert rögzítése</a>
 
 <div class="grid md:grid-cols-2 lg:grid-cols-4 gap-4 my-4">
+    <?php foreach ($concerts as $concert): ?>
         <div class="bg-violet-100 p-2 rounded-lg">
-            <h2 class="text-xl font-bold mb-2"><!-- Név --></h2>            
+            <h2 class="text-xl font-bold mb-2"><?= $concert->name ?></h2>
             <ul class="mb-2">
-                <li><!-- Típus --></li>
-                <li><!-- Helyszín --></li>
-                <li><!-- Dátum --></li>
-                <li><span class="bg-violet-600 text-white p-1 rounded"><!-- Ár --></span></li>
+                <li><?= $concert->type ?></li>
+                <li><?= $concert->location ?></li>
+                <li><?= $concert->date ?></li>
+                <li><span class="bg-violet-600 text-white p-1 rounded"><?= $concert->price ?></span></li>
             </ul>
             <a class="bg-violet-600 text-white text-center p-1 block rounded-md cursor-pointer font-bold">Tovább</a>
         </div>
+    <?php endforeach; ?>
 </div>
