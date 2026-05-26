314b1b068db892a68e06e7f92ab902b5a965d6c8
Balázs Vágvölgyi <admin@vb2007.hu>
2026-05-26 09:47:47 +0000

    added ob sessions

--- 12-2025-2026/_dolgozat/backend/dolgozat_2026_05_26_irodak/www/index.php
+++ 12-2025-2026/_dolgozat/backend/dolgozat_2026_05_26_irodak/www/index.php
@@ -5,3 +5,10 @@ require __DIR__ . "/vendor/autoload.php";
 $whoops = new \Whoops\Run;
 $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
 $whoops->register();
+
+ob_start();
+
+?>
+
+<?php
+ob_end_flush();
