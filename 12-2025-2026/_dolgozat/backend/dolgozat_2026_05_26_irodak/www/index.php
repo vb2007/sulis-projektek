e4a659b1746e068b7a7b155b0a05be849a971611
Balázs Vágvölgyi <admin@vb2007.hu>
2026-05-26 10:11:10 +0000

    implemented query param checks & menu items

--- 12-2025-2026/_dolgozat/backend/dolgozat_2026_05_26_irodak/www/index.php
+++ 12-2025-2026/_dolgozat/backend/dolgozat_2026_05_26_irodak/www/index.php
@@ -1,7 +1,10 @@
 <?php
 declare(strict_types=1);
+error_reporting(E_ALL);
 
 require __DIR__ . "/vendor/autoload.php";
+
+use RealEstate\Rental\Office;
 
 $whoops = new \Whoops\Run;
 $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
@@ -26,6 +29,46 @@ for ($i = 1; $i < count($lines); $i++) {
     }
 }
 
+$title = "";
+$action = "";
+if(!isset($_GET["action"])) {
+    $title = "Irodák";
+    include __DIR__ . "/pages/table.php";
+}
+else {
+    $action = $_GET["action"];
+
+    switch ($action) {
+        case "show":
+            $id = $_GET["id"];
+            $title = "Irodák"; //TODO: iroda neve
+            include __DIR__ . "/pages/show.php"; //TODO: query param
+            break;
+
+        case "create":
+            $title = "Új iroda";
+            include __DIR__ . "/pages/create.php";
+            break;
+
+        default:
+            $title = "404";
+            include __DIR__ . "/pages/404.php";
+            break;
+    }
+}
+
+$menuItems = [
+    [
+        "text" => "Főoldal",
+        "url" => "index.php",
+        "active" => ""
+    ],
+    [
+        "text" => "Új iroda",
+        "url" => "index.php?action=create",
+        "active" => ""
+    ]
+];
 
 ?>
 
