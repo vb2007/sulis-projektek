0068288ccb2fe8d291b81813528505def0db1902
Balázs Vágvölgyi <admin@vb2007.hu>
2026-05-26 08:14:12 +0000

    completed dog fetching & display, updated db, started post

--- 12-2025-2026/_dolgozat/webprogramozas/dolgozat_2026_05_26_kutyak/webprog-alap/src/assets/js/dogs.js
+++ 12-2025-2026/_dolgozat/webprogramozas/dolgozat_2026_05_26_kutyak/webprog-alap/src/assets/js/dogs.js
@@ -1,5 +1,12 @@
 import { BASE_URL } from "./config.js";
 
+const dogbreeds = [
+    "Tacsk ́o",
+    "N ́emetjuh ́asz",
+    "Golden retriever",
+    "Bulldog",
+    "Beagle"
+];
 let dogs = [];
 
 export const getDogs = async () => {
@@ -7,8 +14,29 @@ export const getDogs = async () => {
         method: "GET",
         headers: { Accept: "application/json" },
     });
-    
+
+    if(!response.ok) {
+        throw new Error(`A szerver ${response.status} kóddal válaszolt a(z) ${response.url} útvonalon`);
+    }
+
     const results = await response.json();
 
     return results.data || results;
+}
+
+export const createDog = async (dog) => {
+    const response = await fetch(`${BASE_URL}/dogs`, {
+        method: "POST",
+        body: JSON.stringify(dog),
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+    });
+
+    if (!response.ok) {
+        throw new Error(`A szerver ${response.status} kóddal válaszolt a(z) ${response.url} útvonalon`)
+    }
+
+
 }
--- 12-2025-2026/_dolgozat/webprogramozas/dolgozat_2026_05_26_kutyak/webprog-alap/src/main.js
+++ 12-2025-2026/_dolgozat/webprogramozas/dolgozat_2026_05_26_kutyak/webprog-alap/src/main.js
@@ -1,1 +1,32 @@
 import "@assets/app.css";
+import { getDogs } from "@assets/js/dogs.js";
+
+const templateEl = document.getElementById("dog-card");
+const dogsContainerEl = document.getElementById("dogs-container");
+
+const createCard = (dog) => {
+    const clone = templateEl.content.cloneNode(true);
+
+    const h3 = clone.querySelector(".name");
+    h3.textContent = dog.name;
+
+    const span = clone.querySelector(".breed");
+    span.textContent = dog.breed;
+
+    const age = clone.querySelector(".age");
+    age.textContent = dog.age;
+
+    const owner = clone.querySelector(".owner");
+    owner.textContent = dog.owner;
+
+    return clone;
+}
+
+const displayCards = (dogs) => {
+    for (let i = 0; i < dogs.length; i++) {
+        const card = createCard(dogs[i]);
+        dogsContainerEl.append(card);
+    }
+}
+
+displayCards(await getDogs());
