b244d94840ff3e6c502b119f0af71502e13fb498
Balázs Vágvölgyi <admin@vb2007.hu>
2026-05-26 10:12:48 +0000

    imported show & table from template

---
+++ 12-2025-2026/_dolgozat/backend/dolgozat_2026_05_26_irodak/www/pages/show.php
@@ -0,0 +1,20 @@
+<h1 class="text-3xl font-bold text-center my-4"><?= $title ?></h1>
+
+<div class="grid md:grid-cols-2 lg:grid-cols-[300px_300px_1fr_1fr] gap-4 w-11/12 max-w-400 mx-auto p-4 mb-4 rounded-lg bg-lime-50 drop-shadow-md">
+    <div class="md:col-span-2 md:row-span-2 flex justify-center">
+        <img src="???" alt="???" class="w-full max-w-[400px] rounded-lg">
+    </div>
+    <div class="md:col-span-2 bg-lime-100 rounded-lg overflow-hidden shadow">
+        <h2 class="bg-lime-400 py-1 px-2 font-bold text-xl">Cím</h2>
+        <p class="py-1 px-2 text-xl"><!-- Cím --></p>
+    </div>
+    <div class="bg-lime-100 rounded-lg overflow-hidden shadow">
+        <h2 class="bg-lime-400 py-1 px-2 font-bold text-xl" >Szobák száma</h2>
+        <p class="py-1 px-2 text-xl"><!-- Szobák száma --></p>
+    </div>
+    <div class="bg-lime-100 rounded-lg overflow-hidden shadow">
+        <h2 class="bg-lime-400 py-1 px-2 font-bold text-xl">Ár</h2>
+        <p class="py-1 px-2 text-xl"><!-- Ár --></p>
+    </div>
+    <a href="index.php" class="text-blue-800 hover:underline">Vissza a főoldalra</a>
+</div>
--- 12-2025-2026/_dolgozat/backend/dolgozat_2026_05_26_irodak/www/pages/table.php
+++ 12-2025-2026/_dolgozat/backend/dolgozat_2026_05_26_irodak/www/pages/table.php
@@ -1,1 +1,19 @@
-<?php
+<h1 class="text-3xl font-bold text-center my-4"><?= $title ?></h1>
+
+<table class="w-11/12 max-w-250 mx-auto drop-shadow-md mb-4">
+    <thead>
+        <tr class="bg-lime-400">
+            <th class="p-1 rounded-tl-lg">Név</th>
+            <th class="p-1">Cím</th>
+            <th class="p-1">Szobák száma</th>
+            <th class="p-1 rounded-tr-lg">Ár</th>
+        </tr>
+    </thead>
+            <tr class="text-center [&:last-child>td:first-child]:rounded-bl-lg [&:last-child>td:last-child]:rounded-br-lg odd:bg-lime-50 even:bg-lime-100">
+                <td class="p-1"><a class="text-blue-800 hover:underline" href=""><!-- Név --></a></td>
+                <td class="p-1"><!-- Cím --></td>
+                <td class="p-1"><!-- Szobák száma --></td>
+                <td class="p-1"><!-- Ár --></td>
+            </tr>
+    </tbody>
+</table>
