cdad2fbe462f3477bd0a66c000f6e95446306f8b
Balázs Vágvölgyi <admin@vb2007.hu>
2026-05-26 10:23:52 +0000

    imported tailwind

--- 12-2025-2026/_dolgozat/backend/dolgozat_2026_05_26_irodak/www/index.php
+++ 12-2025-2026/_dolgozat/backend/dolgozat_2026_05_26_irodak/www/index.php
@@ -79,6 +79,7 @@ $menuItems = [
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
     <title><?= $title ?></title>
 </head>
 <body>
