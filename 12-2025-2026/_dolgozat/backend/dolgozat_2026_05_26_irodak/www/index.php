a2f12d3b9feb1bcac7203dd2f446cb1452c09817
Balázs Vágvölgyi <admin@vb2007.hu>
2026-05-26 09:56:54 +0000

    implemented file reading

--- 12-2025-2026/_dolgozat/backend/dolgozat_2026_05_26_irodak/www/index.php
+++ 12-2025-2026/_dolgozat/backend/dolgozat_2026_05_26_irodak/www/index.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 
 require __DIR__ . "/vendor/autoload.php";
 
@@ -7,6 +8,24 @@ $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
 $whoops->register();
 
 ob_start();
+
+$offices = [];
+$file = file_get_contents(__DIR__ . "/data.csv");
+$lines = explode("\n", $file);
+
+for ($i = 1; $i < count($lines); $i++) {
+    if ("" != $lines[$i]) {
+        $split = explode(";", $lines[$i]);
+        array_push($offices, new Office(
+            (int) $split[0],
+            $split[1],
+            $split[2],
+            (int) $split[3],
+            (float) $split[4]
+        ));
+    }
+}
+
 
 ?>
 
