2d786f54c851819803e40e076c712474126956dc
Balázs Vágvölgyi <admin@vb2007.hu>
2026-05-27 09:39:48 +0000

    called 4th class in program.cs

--- 12-2025-2026/_feladat/alkalmazasfejlesztes/feladat_2026_05_27_alapfilmek/Alapfilmek_VB/Alapfilmek_VB_Console/Program.cs
+++ 12-2025-2026/_feladat/alkalmazasfejlesztes/feladat_2026_05_27_alapfilmek/Alapfilmek_VB/Alapfilmek_VB_Console/Program.cs
@@ -1,1 +1,14 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Alapfilmek_VB_Lib;
+
+namespace Alapfilmek_VB_Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataStore.Init();
+
+            Console.WriteLine($"4. feladat: {DataStore.Instance!.HuszadikSzazadiAlkotokSzama} db alkotó született a 20. században.");
+        }
+    }
+}
