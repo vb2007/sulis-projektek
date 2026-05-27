db57ccb20214951037d624453ede67cf2f7f19d5
Balázs Vágvölgyi <admin@vb2007.hu>
2026-05-27 09:39:41 +0000

    created datastore & logic for 4th task

---
+++ 12-2025-2026/_feladat/alkalmazasfejlesztes/feladat_2026_05_27_alapfilmek/Alapfilmek_VB/Alapfilmek_VB_Lib/DataStore.cs
@@ -0,0 +1,53 @@
+﻿using Alapfilmek_VB_Lib.Tables;
+
+namespace Alapfilmek_VB_Lib
+{
+    public class DataStore
+    {
+        private readonly List<Alkotok> _alkotok;
+        private readonly List<Filmek> _filmek;
+        private readonly List<Filmstab> _filmstab;
+        private readonly List<Munkakor> _munkakor;
+
+        private DataStore()
+        {
+            _alkotok = File.ReadAllLines("Input\\alkotok.txt")
+                .Skip(1)
+                .Select(x => new Alkotok(x))
+                .ToList();
+
+            _filmek = File.ReadAllLines("Input\\filmek.txt")
+                .Skip(1)
+                .Select(x => new Filmek(x))
+                .ToList();
+
+            _filmstab = File.ReadAllLines("Input\\filmstab.txt")
+                .Skip(1)
+                .Select(x => new Filmstab(x))
+                .ToList();
+
+            _munkakor = File.ReadAllLines("Input\\munkakor.txt")
+                .Skip(1)
+                .Select(x => new Munkakor(x))
+                .ToList();
+        }
+
+        public static DataStore? Instance { get; private set; }
+
+        public static void Init()
+        {
+            if (Instance is not null)
+            {
+                throw new InvalidOperationException("Már inicializálva van.");
+            }
+
+            Instance = new DataStore();
+        }
+
+        public int HuszadikSzazadiAlkotokSzama => _alkotok
+            .Count(x =>
+                x.Szuletett.HasValue &&
+                x.Szuletett.Value.Year >= 1900
+                && x.Szuletett.Value.Year < 2000);
+    }
+}
