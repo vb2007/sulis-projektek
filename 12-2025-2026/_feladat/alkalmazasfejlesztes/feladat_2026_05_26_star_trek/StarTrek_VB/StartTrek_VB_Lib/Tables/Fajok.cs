722b3fa45458a0c404040f80dc09880abaff1038
VB2007 <admin@vb2007.hu>
2026-05-29 16:59:43 +0200

    renamed solution folder

---
+++ 12-2025-2026/_feladat/alkalmazasfejlesztes/feladat_2026_05_26_star_trek/StarTrek_VB/StartTrek_VB_Lib/Tables/Fajok.cs
@@ -0,0 +1,15 @@
+﻿namespace StartTrek_VB_Lib.Tables;
+
+internal class Fajok
+{
+    public int FajId { get; init; }
+    public string FajNev { get; init; }
+
+    internal Fajok(string dataLines)
+    {
+        string[] split = dataLines.Split(';');
+
+        FajId = int.Parse(split[0]);
+        FajNev = split[1];
+    }
+}
---
+++ 12-2025-2026/_feladat/alkalmazasfejlesztes/feladat_2026_05_26_star_trek/StarTrek_VB/StartTrek_VB_Lib/Tables/HajoOsztalyok.cs
@@ -0,0 +1,17 @@
+﻿namespace StartTrek_VB_Lib.Tables;
+
+internal class HajoOsztalyok
+{
+    public int OsztalyId { get; init; }
+    public string OsztalyNev { get; init; }
+    public int SzerepId { get; init; }
+
+    internal HajoOsztalyok(string dataLines)
+    {
+        string[] split = dataLines.Split(';');
+
+        OsztalyId = int.Parse(split[0]);
+        OsztalyNev = split[1];
+        SzerepId = int.Parse(split[2]);
+    }
+}
