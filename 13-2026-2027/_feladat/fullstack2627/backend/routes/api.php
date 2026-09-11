cd49caa2ddb130aed3ff37ea7e75a6c68162cd18
VB2007 <admin@vb2007.hu>
2026-09-11 15:50:21 +0200

    finished api

--- 13-2026-2027/_feladat/fullstack2627/backend/routes/api.php
+++ 13-2026-2027/_feladat/fullstack2627/backend/routes/api.php
@@ -6,3 +6,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get("/cars", [CarController::class, "index"]);
