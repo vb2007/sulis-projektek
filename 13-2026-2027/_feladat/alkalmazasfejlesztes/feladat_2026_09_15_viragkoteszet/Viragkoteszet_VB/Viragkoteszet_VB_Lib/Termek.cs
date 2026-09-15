9b06591b1947927d06bd1ab6be93e3c0440418d5
VB2007 <admin@vb2007.hu>
2026-09-15 10:14:20 +0200

    created oop termek class

--- 13-2026-2027/_feladat/alkalmazasfejlesztes/feladat_2026_09_15_viragkoteszet/Viragkoteszet_VB/Viragkoteszet_VB_Lib/Termek.cs
+++ 13-2026-2027/_feladat/alkalmazasfejlesztes/feladat_2026_09_15_viragkoteszet/Viragkoteszet_VB/Viragkoteszet_VB_Lib/Termek.cs
@@ -1,7 +1,20 @@
 ﻿namespace Viragkoteszet_VB_Lib
 {
-    internal class Termek
+    internal class Termek : ITermek
     {
+        public int Id { get; set; }
+        public string Tipus { get; init; }
+        public string Megnevezes { get; init; }
+        public int ElkeszitesiIdo { get; set; }
+        public int Ar { get; set; }
 
+        public Termek(int id, string tipus, string megnevezes, int elkeszitesiIdo, int ar)
+        {
+            Id = id;
+            Tipus = tipus;
+            Megnevezes = megnevezes;
+            ElkeszitesiIdo = elkeszitesiIdo;
+            Ar = ar;
+        }
     }
 }
