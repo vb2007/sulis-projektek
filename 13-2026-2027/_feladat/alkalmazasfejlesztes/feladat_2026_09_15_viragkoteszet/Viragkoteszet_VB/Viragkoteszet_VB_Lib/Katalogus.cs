81aab2171acf9d8420966fac8efd6c0ea9119d2c
VB2007 <admin@vb2007.hu>
2026-09-15 09:31:55 +0200

    finished Katalogus class

--- 13-2026-2027/_feladat/alkalmazasfejlesztes/feladat_2026_09_15_viragkoteszet/Viragkoteszet_VB/Viragkoteszet_VB_Lib/Katalogus.cs
+++ 13-2026-2027/_feladat/alkalmazasfejlesztes/feladat_2026_09_15_viragkoteszet/Viragkoteszet_VB/Viragkoteszet_VB_Lib/Katalogus.cs
@@ -2,6 +2,22 @@
 {
     internal class Katalogus
     {
+        private List<Alapanyag> _alapanyagok { get; } = new List<Alapanyag>();
 
+        public Katalogus(IEnumerable<Alapanyag> alapanyagok)
+        {
+            foreach (var alapanyag in alapanyagok)
+            {
+                _alapanyagok.Add(alapanyag);
+            }
+        }
+
+        public Alapanyag this[string azonosito]
+        {
+            get
+            {
+                return _alapanyagok.FirstOrDefault(a => a.Azonosito == azonosito)!;
+            }
+        }
     }
 }
