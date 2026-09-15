9e768e82ed264886c2d2240fe6b35403df9d4c67
VB2007 <admin@vb2007.hu>
2026-09-15 12:38:27 +0200

    created test class file

---
+++ 13-2026-2027/_feladat/teszteles/feladat_2026_09_15_tictactoe/TicTacToe_VB/TicTacToe_VB_Test/TicTacToeTests.cs
@@ -0,0 +1,19 @@
+﻿using TicTacToe_VB_Lib;
+
+namespace TicTacToe_VB_Test
+{
+    internal class TicTacToeTests
+    {
+        [Test]
+        public void UresTablaXelKezdodik()
+        {
+            TicTacToeGame game = new TicTacToeGame();
+
+            Assert.That(game.CurrentPlayer, Is.EqualTo('X'));
+            Assert.That(game.GetCell(0, 0), Is.EqualTo(' '));
+            Assert.That(game.GetCell(1, 1), Is.EqualTo(' '));
+            Assert.That(game.GetCell(2, 2), Is.EqualTo(' '));
+            Assert.That(game.IsGameOver, Is.False);
+        }
+    }
+}
