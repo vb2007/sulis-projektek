1cfcfe6107c96820927d2561308fcc30eba051aa
VB2007 <admin@vb2007.hu>
2026-09-15 10:34:20 +0200

    finished termekek oop development

--- 13-2026-2027/_feladat/alkalmazasfejlesztes/feladat_2026_09_15_viragkoteszet/Viragkoteszet_VB/Viragkoteszet_VB_Lib/Termek.cs
+++ 13-2026-2027/_feladat/alkalmazasfejlesztes/feladat_2026_09_15_viragkoteszet/Viragkoteszet_VB/Viragkoteszet_VB_Lib/Termek.cs
@@ -2,19 +2,25 @@
 {
     internal class Termek : ITermek
     {
-        public int Id { get; set; }
+        public int Id { get; init; }
         public string Tipus { get; init; }
         public string Megnevezes { get; init; }
-        public int ElkeszitesiIdo { get; set; }
-        public int Ar { get; set; }
+        public int ElkeszitesiIdo { get; init; }
+        public int Ar { get; init; }
+        public List<string> Alapanyagok { get; init; }
+        public List<int> AlapanyagMennyisegek { get; init; }
+        public List<Katalogus> AlapanyagKatalogus { get; init; }
 
-        public Termek(int id, string tipus, string megnevezes, int elkeszitesiIdo, int ar)
+        public Termek(int id, string tipus, string megnevezes, int elkeszitesiIdo, int ar, List<string> alapanyagok, List<int> alapanyagMennyisegek, List<Katalogus> alapanyagKatalogus)
         {
             Id = id;
             Tipus = tipus;
             Megnevezes = megnevezes;
             ElkeszitesiIdo = elkeszitesiIdo;
             Ar = ar;
+            Alapanyagok = alapanyagok;
+            AlapanyagMennyisegek = alapanyagMennyisegek;
+            AlapanyagKatalogus = alapanyagKatalogus;
         }
     }
 }
