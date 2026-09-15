f3302fa7b51c9183f505ea71ad45630fbc5337da
VB2007 <admin@vb2007.hu>
2026-09-15 12:28:48 +0200

    created solution & console

---
+++ 13-2026-2027/_feladat/teszteles/feladat_2026_09_15_tictactoe/TicTacToe_VB/TicTacToe_VB_Console/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TicTacToe_VB_Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
