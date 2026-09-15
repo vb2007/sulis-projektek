64a5a81a176e11723e0e52f9ea84a9ce6313de08
VB2007 <admin@vb2007.hu>
2026-09-15 09:02:53 +0200

    finished alapanyag class

--- 13-2026-2027/_feladat/alkalmazasfejlesztes/feladat_2026_09_15_viragkoteszet/Viragkoteszet_VB/Viragkoteszet_VB_Lib/Alapanyag.cs
+++ 13-2026-2027/_feladat/alkalmazasfejlesztes/feladat_2026_09_15_viragkoteszet/Viragkoteszet_VB/Viragkoteszet_VB_Lib/Alapanyag.cs
@@ -2,6 +2,19 @@
 {
     internal class Alapanyag
     {
+        public string Azonosito { get; set; }
+        public string Nev { get; set; }
+        public int Ar { get; set; }
+        public int ElkeszitesiIdo { get; set; }
 
+        public Alapanyag(string adatSor)
+        {
+            string[] adatok = adatSor.Split(';');
+
+            Azonosito = adatok[0];
+            Nev = adatok[1];
+            Ar = int.Parse(adatok[2]);
+            ElkeszitesiIdo = int.Parse(adatok[3]);
+        }
     }
 }
