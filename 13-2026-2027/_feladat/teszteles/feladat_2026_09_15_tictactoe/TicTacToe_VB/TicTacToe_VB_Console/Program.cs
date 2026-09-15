d4dc40f4b9648cf5ad04b982b8a945d12a3563be
VB2007 <admin@vb2007.hu>
2026-09-15 13:09:52 +0200

    fixed program.cs

--- 13-2026-2027/_feladat/teszteles/feladat_2026_09_15_tictactoe/TicTacToe_VB/TicTacToe_VB_Console/Program.cs
+++ 13-2026-2027/_feladat/teszteles/feladat_2026_09_15_tictactoe/TicTacToe_VB/TicTacToe_VB_Console/Program.cs
@@ -10,11 +10,13 @@ namespace TicTacToe_VB_Console
 
             while (!jatek.JatekVege)
             {
-                TablaIras(jatek);
+                Console.WriteLine();
+                Console.Write(TablaMegjelenito.Kirajzol(jatek));
+                Console.WriteLine();
                 Console.Write($"Játékos {jatek.AktivJatekos} [Oszlop-Sor (1-3)]: ");
                 string input = Console.ReadLine()!;
 
-                if (!LepesFeldolgozas(input, out var sor, out var oszlop))
+                if (!LepesParser.LepesFeldolgozas(input, out var sor, out var oszlop))
                 {
                     Console.WriteLine("Érvénytelen lépés");
                     continue;
@@ -26,46 +28,10 @@ namespace TicTacToe_VB_Console
                 }
             }
 
-            TablaIras(jatek);
+            Console.WriteLine();
+            Console.Write(TablaMegjelenito.Kirajzol(jatek));
+            Console.WriteLine();
             Console.WriteLine(jatek.Nyertes == ' ' ? "Döntetlen" : $"Játékos {jatek.Nyertes} nyert");
-        }
-
-        private static bool LepesFeldolgozas(string? bemenet, out int sor, out int oszlop)
-        {
-            sor = -1;
-            oszlop = -1;
-
-            if (string.IsNullOrWhiteSpace(bemenet))
-            {
-                return false;
-            }
-
-            string[] darabolt = bemenet.Split('-', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
-            if (darabolt.Length != 2
-                || !int.TryParse(darabolt[0], out var rowInput)
-                || !int.TryParse(darabolt[1], out var columnInput))
-            {
-                return false;
-            }
-
-            sor = rowInput - 1;
-            oszlop = columnInput - 1;
-            return sor is >= 0 and < 3 && oszlop is >= 0 and < 3;
-        }
-
-        private static void TablaIras(Matrix jatek)
-        {
-            Console.WriteLine();
-            for (int sor = 0; sor < 3; sor++)
-            {
-                Console.WriteLine($" {jatek.Cella(sor, 0)} | {jatek.Cella(sor, 1)} | {jatek.Cella(sor, 2)} ");
-                if (sor < 2)
-                {
-                    Console.WriteLine("---+---+---");
-                }
-            }
-
-            Console.WriteLine();
         }
     }
 }
