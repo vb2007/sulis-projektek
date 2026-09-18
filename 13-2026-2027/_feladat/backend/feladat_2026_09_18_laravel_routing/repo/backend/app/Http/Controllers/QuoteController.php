694a7317fe84aa9da7e00820a9293984d152a0fe
VB2007 <admin@vb2007.hu>
2026-09-18 10:38:18 +0200

    added modern family quote

--- 13-2026-2027/_feladat/backend/feladat_2026_09_18_laravel_routing/repo/backend/app/Http/Controllers/QuoteController.php
+++ 13-2026-2027/_feladat/backend/feladat_2026_09_18_laravel_routing/repo/backend/app/Http/Controllers/QuoteController.php
@@ -7,6 +7,14 @@ use Illuminate\Http\Request;
 class QuoteController extends Controller
 {
     public function house() {
-        return "nigger";
+        return ["data" => ["title" => "House",
+                          "quote" => "Nemcsak az emberek megalázásával lehet a gőzt kiereszteni; mondják, hogy a bowling jobb még ennél is.",
+                          "name" => "Dr. House"]];
+    }
+
+    public function modernFamily() {
+        return ["data" =>  ["title" => "Modern Család",
+                             "quote" => "A siker mindig 1 százalék ihlet, plusz 98 százalék verejték, végül pedig 2 százalék odafigyelés.",
+                             "name" => "Phil Dunphy"]];
     }
 }
--- 13-2026-2027/_feladat/backend/feladat_2026_09_18_laravel_routing/repo/backend/routes/api.php
+++ 13-2026-2027/_feladat/backend/feladat_2026_09_18_laravel_routing/repo/backend/routes/api.php
@@ -7,3 +7,6 @@ use Illuminate\Support\Facades\Route;
 Route::get("idezetek/house",
     [QuoteController::class, "house"])
         ->name("quote.house");
+
+Route::get("idezetek/modern-family", [QuoteController::class, "modernFamily"])
+        ->name("quote.modernFamily");
