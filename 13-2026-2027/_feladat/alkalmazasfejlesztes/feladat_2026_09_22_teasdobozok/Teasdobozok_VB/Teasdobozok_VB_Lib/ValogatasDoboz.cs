f9a5d015c21273cc38e5d8c563aae90a3477d78b
VB2007 <admin@vb2007.hu>
2026-09-22 11:11:17 +0200

    finished valogatasdoboz class

--- 13-2026-2027/_feladat/alkalmazasfejlesztes/feladat_2026_09_22_teasdobozok/Teasdobozok_VB/Teasdobozok_VB_Lib/ValogatasDoboz.cs
+++ 13-2026-2027/_feladat/alkalmazasfejlesztes/feladat_2026_09_22_teasdobozok/Teasdobozok_VB/Teasdobozok_VB_Lib/ValogatasDoboz.cs
@@ -1,12 +1,48 @@
 ﻿namespace Teasdobozok_VB_Lib
 {
-    internal class ValogatasDoboz : TeasDoboz
+    public sealed class ValogatasDoboz : TeasDoboz
     {
+        private readonly Filterek _filterek;
+        private readonly List<string> _filterAzonositok;
 
+        public override int Ar => _filterAzonositok.Sum(filterAzonosito => _filterek[filterAzonosito].Ar) + 100;
 
-        public ValogatasDoboz(int darabSzam, Filterek filterek) : base(darabSzam)
+        public string FilterTipusokString => string.Join(", ",
+            _filterAzonositok
+                .Select(filterAzonosito => _filterek[filterAzonosito].Tipus)
+                .Distinct());
+
+        public override string Nev =>
+            string.IsNullOrWhiteSpace(FilterTipusokString)
+                ? "Válogatás tea"
+                : $"Válogatás tea - {FilterTipusokString}";
+
+        public ValogatasDoboz(int darabSzam, Filterek filterek)
+            : this(darabSzam, filterek, Enumerable.Empty<string>())
         {
+        }
 
+        private ValogatasDoboz(int darabSzam, Filterek filterek, IEnumerable<string> filterAzonositok)
+            : base(darabSzam)
+        {
+            _filterek = filterek;
+            _filterAzonositok = new List<string>();
+
+            foreach (string filterAzonosito in filterAzonositok)
+            {
+                _ = filterek[filterAzonosito];
+                _filterAzonositok.Add(filterAzonosito);
+            }
+        }
+
+        public static ValogatasDoboz operator +(ValogatasDoboz doboz, string filterAzonosito)
+        {
+            List<string> ujFilterAzonositok = new(doboz._filterAzonositok)
+            {
+                filterAzonosito
+            };
+
+            return new ValogatasDoboz(doboz.DarabSzam, doboz._filterek, ujFilterAzonositok);
         }
     }
 }
