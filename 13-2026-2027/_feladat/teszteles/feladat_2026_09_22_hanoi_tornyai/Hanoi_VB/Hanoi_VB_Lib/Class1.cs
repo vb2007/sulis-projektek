0f9a20ca0936f8c24e335c919668daaf7378e4d3
VB2007 <admin@vb2007.hu>
2026-09-22 12:33:46 +0200

    added rest of projects & added references

---
+++ 13-2026-2027/_feladat/teszteles/feladat_2026_09_22_hanoi_tornyai/Hanoi_VB/Hanoi_VB_Lib/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Hanoi_VB_Lib
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ 13-2026-2027/_feladat/teszteles/feladat_2026_09_22_hanoi_tornyai/Hanoi_VB/Hanoi_VB_Test/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿namespace Hanoi_VB_Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
