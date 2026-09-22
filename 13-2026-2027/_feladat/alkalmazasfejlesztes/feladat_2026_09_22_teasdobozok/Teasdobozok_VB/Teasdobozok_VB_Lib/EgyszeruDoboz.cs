44b263730d09b7e7d190f5d98ec5b346966f1d09
VB2007 <admin@vb2007.hu>
2026-09-22 10:22:58 +0200

    half completed 2 more classes

---
+++ 13-2026-2027/_feladat/alkalmazasfejlesztes/feladat_2026_09_22_teasdobozok/Teasdobozok_VB/Teasdobozok_VB_Lib/EgyszeruDoboz.cs
@@ -0,0 +1,18 @@
+﻿namespace Teasdobozok_VB_Lib
+{
+    internal sealed class EgyszeruDoboz : TeasDoboz
+    {
+        internal Filter Filter { get; init; }
+        internal int DarabSzam { get; set; }
+        internal int Ar =>
+            Filter.Ar * DarabSzam + 100;
+        internal string Nev =>
+            $"{Filter.Tipus} tea";
+
+        public EgyszeruDoboz(int darabSzam, string filterId, Filterek filterek) : base(darabSzam)
+        {
+            DarabSzam = darabSzam;
+            Filter = filterek[filterId];
+        }
+    }
+}
---
+++ 13-2026-2027/_feladat/alkalmazasfejlesztes/feladat_2026_09_22_teasdobozok/Teasdobozok_VB/Teasdobozok_VB_Lib/ValogatasDoboz.cs
@@ -0,0 +1,12 @@
+﻿namespace Teasdobozok_VB_Lib
+{
+    internal class ValogatasDoboz : TeasDoboz
+    {
+
+
+        public ValogatasDoboz(int darabSzam, Filterek filterek) : base(darabSzam)
+        {
+
+        }
+    }
+}
