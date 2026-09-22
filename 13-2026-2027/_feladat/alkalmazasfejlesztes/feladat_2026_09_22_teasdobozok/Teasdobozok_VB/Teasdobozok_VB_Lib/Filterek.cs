d6c65cffaf36f240001c4bb2d53a2ba9eee69efd
VB2007 <admin@vb2007.hu>
2026-09-22 08:55:01 +0200

    started implementing Filterek class

--- 13-2026-2027/_feladat/alkalmazasfejlesztes/feladat_2026_09_22_teasdobozok/Teasdobozok_VB/Teasdobozok_VB_Lib/Filterek.cs
+++ 13-2026-2027/_feladat/alkalmazasfejlesztes/feladat_2026_09_22_teasdobozok/Teasdobozok_VB/Teasdobozok_VB_Lib/Filterek.cs
@@ -2,6 +2,19 @@
 {
     internal class Filterek
     {
+        private protected List<Filter> _filterek = new List<Filter>();
 
+        public Filterek(IEnumerable<string> adatSorok)
+        {
+            foreach (string adatSor in adatSorok)
+            {
+                string[] adatok = adatSor.Split(";");
+                _filterek.Add(new (
+                    adatok[0],
+                    adatok[1],
+                    int.Parse(adatok[2])
+                 ));
+            }
+        }
     }
 }
