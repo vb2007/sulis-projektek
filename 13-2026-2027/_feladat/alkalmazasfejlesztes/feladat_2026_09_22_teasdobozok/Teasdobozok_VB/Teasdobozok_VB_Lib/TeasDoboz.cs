ae6e544a5c06591295aa55b1268a02880103e1d5
VB2007 <admin@vb2007.hu>
2026-09-22 09:08:39 +0200

    half implemented TeasDoboz

---
+++ 13-2026-2027/_feladat/alkalmazasfejlesztes/feladat_2026_09_22_teasdobozok/Teasdobozok_VB/Teasdobozok_VB_Lib/TeasDoboz.cs
@@ -0,0 +1,15 @@
+﻿namespace Teasdobozok_VB_Lib
+{
+    internal class TeasDoboz : IDoboz
+    {
+        public int DarabSzam { get; set; }
+        public List<Filterek> Filterek { get; set; }
+        public string Nev { get; set; }
+        public int Ar { get; set; }
+
+        public TeasDoboz(int darabSzam)
+        {
+            DarabSzam = darabSzam;
+        }
+    }
+}
