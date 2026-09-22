f253e66ccbe627a5bf6629f256ba23e777516bcb
VB2007 <admin@vb2007.hu>
2026-09-22 12:47:57 +0200

    implemented CanMoveWithValidSteps

--- 13-2026-2027/_feladat/teszteles/feladat_2026_09_22_hanoi_tornyai/Hanoi_VB/Hanoi_VB_Test/MoveTests.cs
+++ 13-2026-2027/_feladat/teszteles/feladat_2026_09_22_hanoi_tornyai/Hanoi_VB/Hanoi_VB_Test/MoveTests.cs
@@ -8,7 +8,13 @@ namespace Hanoi_VB_Test
         [Description("Mozoghat-e érvényes lépésekkkel?")]
         public void CanMoveWithValidSteps()
         {
+            HanoiGame game = new HanoiGame(3);
 
+            int fromRod = 0;
+            int toRod = 0;
+            bool outcome = game.CanMove(fromRod, toRod);
+
+            Assert.That(outcome, Is.EqualTo(false));
         }
     }
 }
