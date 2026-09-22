f65ad2d9588896fc47e582dd0e7e168163c0c0d3
VB2007 <admin@vb2007.hu>
2026-09-22 10:55:59 +0200

    fixed and polished filterek classs

--- 13-2026-2027/_feladat/alkalmazasfejlesztes/feladat_2026_09_22_teasdobozok/Teasdobozok_VB/Teasdobozok_VB_Lib/Filterek.cs
+++ 13-2026-2027/_feladat/alkalmazasfejlesztes/feladat_2026_09_22_teasdobozok/Teasdobozok_VB/Teasdobozok_VB_Lib/Filterek.cs
@@ -1,29 +1,47 @@
 ﻿namespace Teasdobozok_VB_Lib
 {
-    internal class Filterek
+    public class Filterek
     {
-        private protected List<Filter> _filterek = new List<Filter>();
+        private readonly List<Filter> _filterek = new();
 
         public Filterek(IEnumerable<string> adatSorok)
         {
             foreach (string adatSor in adatSorok)
             {
-                string[] adatok = adatSor.Split(";");
-                _filterek.Add(new (
+                if (string.IsNullOrWhiteSpace(adatSor) || adatSor.StartsWith("ID;"))
+                {
+                    continue;
+                }
+
+                string[] adatok = adatSor.Split(';');
+                _filterek.Add(new Filter(
                     adatok[0],
                     adatok[1],
-                    int.Parse(adatok[2])
-                 ));
+                    int.Parse(adatok[2])));
             }
         }
 
-        public Filter this[string id] =>
-            _filterek.FirstOrDefault(x => x.Id == id)!;
+        public Filter this[string id]
+        {
+            get
+            {
+                Filter? filter = _filterek.FirstOrDefault(x => x.Id == id);
+                if (filter is null)
+                {
+                    throw new HibasAzonositoException();
+                }
 
-        public List<string> GyogynovenyFilterek =>
-            _filterek
+                return filter;
+            }
+        }
+
+        public List<string> GyogynovenyFilterek()
+        {
+            return _filterek
+                .Where(x => x.Gyogytea)
                 .Select(x => x.Tipus)
                 .OrderBy(x => x)
                 .ToList();
+        }
     }
 }
