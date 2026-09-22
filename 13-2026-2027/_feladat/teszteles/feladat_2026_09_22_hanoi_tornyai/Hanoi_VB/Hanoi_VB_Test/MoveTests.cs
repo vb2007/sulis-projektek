63a1b61ecd549019fe52c541e06260dacb324845
VB2007 <admin@vb2007.hu>
2026-09-22 12:55:14 +0200

    updated namespace

--- 13-2026-2027/_feladat/teszteles/feladat_2026_09_22_hanoi_tornyai/Hanoi_VB/Hanoi_VB_Test/MoveTests.cs
+++ 13-2026-2027/_feladat/teszteles/feladat_2026_09_22_hanoi_tornyai/Hanoi_VB/Hanoi_VB_Test/MoveTests.cs
@@ -1,22 +1,19 @@
 ﻿using Hanoi_VB_Lib;
 
-namespace Hanoi_VB_Test
+namespace Hanoi_VB_Test;
+
+internal class MoveTests
 {
-    internal class MoveTests
+    [Test]
+    [Description("Mozoghat-e érvényes lépésekkkel?")]
+    public void CanMoveWithValidSteps()
     {
-        [Test]
-        [Description("Mozoghat-e érvényes lépésekkkel?")]
-        public void CanMoveWithValidSteps()
-        {
-            HanoiGame game = new HanoiGame(3);
+        HanoiGame game = new HanoiGame(3);
 
-            int fromRod = 0;
-            int toRod = 0;
-            bool outcome = game.CanMove(fromRod, toRod);
+        int fromRod = 0;
+        int toRod = 0;
+        bool outcome = game.CanMove(fromRod, toRod);
 
-            Assert.That(outcome, Is.EqualTo(false));
-        }
-
-
+        Assert.That(outcome, Is.EqualTo(false));
     }
 }
