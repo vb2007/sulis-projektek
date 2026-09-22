591f32662d7b12b8a61144ac5e3701688f358188
VB2007 <admin@vb2007.hu>
2026-09-22 13:02:25 +0200

    added error logging and 2 state testing to MoveTests

--- 13-2026-2027/_feladat/teszteles/feladat_2026_09_22_hanoi_tornyai/Hanoi_VB/Hanoi_VB_Test/MoveTests.cs
+++ 13-2026-2027/_feladat/teszteles/feladat_2026_09_22_hanoi_tornyai/Hanoi_VB/Hanoi_VB_Test/MoveTests.cs
@@ -3,17 +3,28 @@
 namespace Hanoi_VB_Test;
 
 internal class MoveTests
-{
+{   
+    HanoiGame _game = new HanoiGame(3);
+
     [Test]
-    [Description("Mozoghat-e érvényes lépésekkkel?")]
+    [Description("Mozoghat-e érvényes lépéssel?")]
     public void CanMoveWithValidSteps()
     {
-        HanoiGame game = new HanoiGame(3);
+        int fromRod = 0;
+        int toRod = 1;
+        bool outcome = _game.CanMove(fromRod, toRod);
 
+        Assert.That(outcome, Is.EqualTo(true), "Érvényes lépésekkel engedélyeznie kéne a műveletet.");
+    }
+
+    [Test]
+    [Description("Mozgathat-e érvénytelen lépéssel?")]
+    public void CanMoveWithInvalidSteps()
+    {
         int fromRod = 0;
         int toRod = 0;
-        bool outcome = game.CanMove(fromRod, toRod);
+        bool outcome = _game.CanMove(fromRod, toRod);
 
-        Assert.That(outcome, Is.EqualTo(false));
+        Assert.That(outcome, Is.EqualTo(false), "Érvényes lépésekkel nem kéne engedélyeznie a műveletet.");
     }
 }
