0d10331508ff9c25d2163b98561c6fc07a7a08dc
VB2007 <admin@vb2007.hu>
2026-09-22 09:15:13 +0200

    implemented HibasAzonositoException

---
+++ 13-2026-2027/_feladat/alkalmazasfejlesztes/feladat_2026_09_22_teasdobozok/Teasdobozok_VB/Teasdobozok_VB_Lib/HibasAzonositoException.cs
@@ -0,0 +1,7 @@
+﻿namespace Teasdobozok_VB_Lib
+{
+    internal class HibasAzonositoException : Exception
+    {
+        public HibasAzonositoException() : base("A megadott filter azonosító nem létezik.") { }
+    }
+}
