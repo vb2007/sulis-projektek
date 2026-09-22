1f7079188e2cca783124ec14a9eac3e2aec1f8e7
VB2007 <admin@vb2007.hu>
2026-09-22 13:10:08 +0200

    fixed asserts in CantMoveFrom0ToMinus1 and CantMoveFromMinus1To0

--- 13-2026-2027/_feladat/teszteles/feladat_2026_09_22_hanoi_tornyai/Hanoi_VB/Hanoi_VB_Test/MoveTests.cs
+++ 13-2026-2027/_feladat/teszteles/feladat_2026_09_22_hanoi_tornyai/Hanoi_VB/Hanoi_VB_Test/MoveTests.cs
@@ -56,9 +56,10 @@ internal class MoveTests
     {
         int fromRod = 0;
         int toRod = -1;
-        bool outcome = _game.CanMove(fromRod, toRod);
-
-        Assert.That(outcome, Is.EqualTo(false), "Érvényes lépésekkel nem kéne engedélyeznie a műveletet.");
+        Assert.Throws<ArgumentOutOfRangeException>(() =>
+            _game.CanMove(fromRod, toRod),
+            "Érvénytelen rúd index esetén kivételt kell dobnia."
+        );
     }
 
     [Test]
@@ -67,8 +68,9 @@ internal class MoveTests
     {
         int fromRod = -1;
         int toRod = 0;
-        bool outcome = _game.CanMove(fromRod, toRod);
-
-        Assert.That(outcome, Is.SameAs(false), "Érvényes lépésekkel nem kéne engedélyeznie a műveletet.");
+        Assert.Throws<ArgumentOutOfRangeException>(() =>
+            _game.CanMove(fromRod, toRod),
+            "Érvénytelen rúd index esetén kivételt kell dobnia."
+        );
     }
 }
